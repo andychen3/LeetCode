5dbc4b6fbcb681ae093e210b5cd192ac99642b21
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-10-24 14:41:50 -0400

    Time: 1290 ms (94.10%), Space: 26.2 MB (15.63%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,17 +1,33 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        max_average = float(-inf)
-        window_start = 0
-        total = 0
+        length = len(nums)
+    
+        max_sum = sum(nums[:k])
+        temp_sum = max_sum
         
-        for window_end in range(len(nums)):
-            total += nums[window_end]
+        for index in range(length-k):
+            
+            temp_sum -= nums[index]
+            temp_sum += nums[k+index]
+            if max_sum < temp_sum:
+                max_sum = temp_sum
+                
+        return max_sum/k
         
-            while window_end-window_start+1 > k:
-                total -= nums[window_start]
-                window_start += 1
+        
+        
+#         max_average = float(-inf)
+#         window_start = 0
+#         total = 0
+        
+#         for window_end in range(len(nums)):
+#             total += nums[window_end]
+        
+#             while window_end-window_start+1 > k:
+#                 total -= nums[window_start]
+#                 window_start += 1
             
-            if window_end-window_start + 1 == k:
-                max_average = max(max_average, total/k)
+#             if window_end-window_start + 1 == k:
+#                 max_average = max(max_average, total/k)
             
-        return max_average
+#         return max_average
