49f84c02342065b838087c86aaa91a18f80d04b9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-10-26 10:39:00 -0400

    Time: 192 ms (60.77%), Space: 21.9 MB (64.65%) - LeetHub

--- 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
+++ 0244-shortest-word-distance-ii/0244-shortest-word-distance-ii.py
@@ -11,10 +11,21 @@ class WordDistance:
     def shortest(self, word1: str, word2: str) -> int:
         min_val = float('inf')
         
+        index1 = self.wordHash[word1]
+        index2 = self.wordHash[word2]
+        
+        ptr1 = ptr2 = 0
+        
+        while ptr1 < len(index1) and ptr2 < len(index2):
+            min_val = min(min_val, abs(index1[ptr1]-index2[ptr2]))
+            if index2[ptr2]> index1[ptr1]:
+                ptr1 += 1
+            else:
+                ptr2 += 1
 
-        for index1 in self.wordHash[word1]:
-            for index2 in self.wordHash[word2]:
-                min_val = min(min_val, abs(index1-index2))
+        # for index1 in self.wordHash[word1]:
+        #     for index2 in self.wordHash[word2]:
+        #         min_val = min(min_val, abs(index1-index2))
         return min_val
 
         
