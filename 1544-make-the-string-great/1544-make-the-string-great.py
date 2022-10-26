57e157bcd674129f58605419eccefcaeb6e2bfed
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-10-26 16:36:18 -0400

    Time: 37 ms (94.50%), Space: 13.9 MB (62.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,19 +1,28 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        if not s:
-            return ""
-        
         stack = []
         
         for char in s:
-            if stack:
-                if char == char.upper() and stack[-1] == char.lower():
-                    stack.pop()
-                    continue
-                elif char == char.lower() and stack[-1] == char.upper():
-                    stack.pop()
-                    continue
-            stack.append(char)
-        
+            if stack and char == stack[-1].swapcase():
+                stack.pop()
+            else:
+                stack.append(char)
         return "".join(stack)
+        
+#         if not s:
+#             return ""
+        
+#         stack = []
+        
+#         for char in s:
+#             if stack:
+#                 if char == char.upper() and stack[-1] == char.lower():
+#                     stack.pop()
+#                     continue
+#                 elif char == char.lower() and stack[-1] == char.upper():
+#                     stack.pop()
+#                     continue
+#             stack.append(char)
+        
+#         return "".join(stack)
         
