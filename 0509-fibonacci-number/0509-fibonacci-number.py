0d264fd65fba319ed0e614ed5f636dede91c85d6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-11-20 09:22:48 -0500

    Time: 36 ms (90.39%), Space: 13.9 MB (55.17%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def dp(n):
+            if n == 0:
+                return 0
+            
+            if n == 1:
+                return 1
+            
+            if n in memo:
+                return memo[n]
+            
+            memo[n] = dp(n-1) + dp(n-2)
+            return memo[n]
+        
+        memo = {}
+        return dp(n)
