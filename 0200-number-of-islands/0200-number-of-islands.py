c27dcf42f2a4103ea571b828f851938351903c91
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-11-22 11:20:34 -0500

    Time: 732 ms (56.66%), Space: 16.7 MB (42.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,66 +2,35 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_islands = 0
-        rows, cols = len(grid), len(grid[0])
-        stack = []
-    
+        rows = len(grid)
+        cols = len(grid[0])
+        ans = 0
         
-        def dfs():
-            while stack:
-                x, y = stack.pop()
-                grid[x][y] = '0'
-                for new_x, new_y in ((x-1,y), (x+1,y), (x,y+1), (x,y-1)):
-                    if 0 <= new_x < rows and 0 <= new_y < cols and grid[new_x][new_y] == '1':
-                        stack.append((new_x,new_y))
-                        grid[new_x][new_y] == '0'
-                
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == '1':
-                    stack.append((row,col))
-                    dfs()
-                    num_islands += 1
-        
-        return num_islands
-                    
-        
+        def dfs(x, y):
+            if 0 > x  or x >= rows or 0 > y or y >= cols:
+                return
+            if grid[x][y] == '0':
+                return
             
-        
-        
-        
+            grid[x][y] = '0'
             
+#             dfs(x+1, y)
+#             dfs(x-1, y)
+#             dfs(x, y+1)
+#             dfs(x, y-1)
+            
+            [dfs(x + x1, y + y1) for x1, y1 in ((1,0), (-1,0), (0,1), (0,-1))]
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    ans += 1
+                    dfs(r, c)
+        return ans
         
         
         
-        
-        
-        
-        
-#         def search(d):
-#             while d:
-#                 r, c = d.popleft()
-#                 grid[r][c] = '0'
-                
-#                 for new_x, new_y in (r-1,c), (r+1,c), (r,c-1), (r,c+1):
-#                     if 0 <= new_x < rows and 0 <= new_y < cols and grid[new_x][new_y] == '1':
-#                         grid[new_x][new_y] = '0'
-#                         d.append((new_x,new_y))
-        
-#         num_islands = 0
-#         d = deque()
-        
-#         rows = len(grid)
-#         cols = len(grid[0])
-        
-#         for row in range(rows):
-#             for col in range(cols):
-#                 if grid[row][col] == '1':
-#                     d.append((row,col))
-#                     search(d)
-#                     num_islands += 1
-        
-#         return num_islands
+
             
             
         
