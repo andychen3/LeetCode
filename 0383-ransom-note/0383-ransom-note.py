39b4ee3d154254664f4847d715e1c3d894083d2d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-11-23 17:35:09 -0500

    Time: 101 ms (59.67%), Space: 14.2 MB (19.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,3 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_dict = Counter(ransomNote)
-        magazine_dict = Counter(magazine)
-              
-        ransom_dict.subtract(magazine_dict)
-        print(ransom_dict)
-        
-        for x in ransom_dict.values():
-            if x > 0:
-                return False
-            
-        return True
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:  
+        return Counter(ransomNote) <= Counter(magazine)
