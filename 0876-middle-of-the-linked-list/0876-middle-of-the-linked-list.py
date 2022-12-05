33ce47b9ec18f1db6472f818affd5dd5e7330994
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-12-05 16:30:06 -0500

    Time: 55 ms (54.31%), Space: 13.9 MB (11.63%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -10,9 +10,9 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            
-        return slow
         
+        return slow
+
 
             
             
