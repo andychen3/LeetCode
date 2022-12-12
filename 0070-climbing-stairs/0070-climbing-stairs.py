eb7636e18ffbc3f75abae13a51e6bf3590fc2a84
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-12-12 11:35:39 -0500

    Time: 36 ms (84.45%), Space: 13.8 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,21 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
+        memo = defaultdict(int)
+        def dfs(steps):
+            if steps <= 2:
+                return steps
+            
+            if steps in memo:
+                return memo[steps]
+            
+            memo[steps] = dfs(steps-1)+dfs(steps-2)
+            return memo[steps]
         
-        def dp(i):
-            if i == 0:
-                return 0
-            if i == 1:
-                return 1
-            if i == 2:
-                return 2
-            
-            if i in memo:
-                return memo[i]
-            
-            memo[i] = dp(i-1) + dp(i-2) 
-            return memo[i]
+        return dfs(n)
         
-        memo = {}
-        return dp(n)
-            
