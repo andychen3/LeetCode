d0f0e2f109c820de0283253a905ae9be32d348a6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-12-16 19:28:59 -0500

    Time: 70 ms (91.90%), Space: 14.2 MB (96.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,39 +2,22 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         
-        for token in tokens:
-            if token in '+*-/':
-                if token == '+':
-                    second_num = stack.pop()
-                    first_num = stack.pop()
-                    stack.append(first_num+second_num)
-                elif token == '*':
-                    second_num = stack.pop()
-                    first_num = stack.pop()
-                    stack.append(first_num*second_num)
-                elif token == '-':
-                    second_num = stack.pop()
-                    first_num = stack.pop()
-                    stack.append(first_num-second_num)
-                elif token =='/':
-                    second_num = stack.pop()
-                    first_num = stack.pop()
-                    stack.append(int(first_num/second_num))
+        for char in tokens:
+            if char in '+-*/':
+                second = stack.pop()
+                first = stack.pop()
+                if char is '+':
+                    stack.append(first+second)
+                elif char is '-':
+                    stack.append(first-second)
+                elif char is '*':
+                    stack.append(first*second)
+                elif char is '/':
+                    stack.append(int(first/second))
             else:
-                stack.append(int(token))
-        return stack.pop()
-        
-        
-        
-#         stack = []
-#         operations ={'+': lambda a,b: a+b, '*':lambda a, b: a*b, '-':lambda a,b: a-b, '/':lambda a,b: int(a/b)}
-        
-#         for num in tokens:
-#             if num in operations:
-#                 operation = operations[num]
-#                 first = stack.pop()
-#                 second = stack.pop()
-#                 stack.append(operation(second,first))
-#             else:
-#                 stack.append(int(num))
-#         return stack.pop()
+                stack.append(int(char))
+                
+        return stack[-1]
+                
+                
+            
