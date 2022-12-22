6e8c452e1da94deac7466c6f1b133b28767abe07
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-12-21 20:40:56 -0500

    Time: 316 ms (95.92%), Space: 14.9 MB (97.05%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
@@ -1,15 +1,20 @@
 import heapq
-
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        cost = 0
+        if len(sticks) < 2:
+            return 0
+        
         heapq.heapify(sticks)
+        ans = 0
         
         while len(sticks) > 1:
-            stick_1 = heapq.heappop(sticks)
-            stick_2 = heapq.heappop(sticks)
-            combined_cost = stick_1+stick_2
-            cost += combined_cost
-            heapq.heappush(sticks, combined_cost)
-        return cost
-        
+            first_stick = heapq.heappop(sticks)
+            second_stick = heapq.heappop(sticks)
+            combined_stick = first_stick+second_stick
+            ans += combined_stick
+            heapq.heappush(sticks, combined_stick)
+            
+        return ans
+            
+            
+            
