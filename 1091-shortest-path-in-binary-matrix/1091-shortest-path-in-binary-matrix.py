247e71ccb33b8df914077886d4cfa502fcdf5511
Andy Chen <81591593+andychen3@users.noreply.github.com>
2022-12-23 10:14:08 -0500

    Time: 1264 ms (61.12%), Space: 15.4 MB (40.30%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -9,24 +9,29 @@ class Solution:
         if grid[0][0] or grid[N-1][N-1]:
             return -1
         
-        q = deque([(0,0,1)])
+        q = deque([(0,0)])
         seen = {(0,0)}
         directions = ((0,1), (0,-1), (1,0), (-1,0), (1,1), (-1,-1), (-1,1), (1,-1))
+        steps = 1
         
         while q:
-            x, y, steps = q.popleft()
+            q_len = len(q)
             
-            if (x, y) == (N-1, N-1):
-                    return steps
-            
-            for x1, y1 in directions:
-                new_x = x + x1
-                new_y = y + y1
+            for _ in range(q_len):
+                x, y = q.popleft()
 
-                if valid(new_x, new_y) and (new_x, new_y) not in seen:
-                    seen.add((new_x, new_y))
-                    q.append((new_x, new_y, steps+1))
-                
+                if (x, y) == (N-1, N-1):
+                        return steps
+
+                for x1, y1 in directions:
+                    new_x = x + x1
+                    new_y = y + y1
+
+                    if valid(new_x, new_y) and (new_x, new_y) not in seen:
+                        seen.add((new_x, new_y))
+                        q.append((new_x, new_y))
+            steps += 1
+
         return -1
             
             
