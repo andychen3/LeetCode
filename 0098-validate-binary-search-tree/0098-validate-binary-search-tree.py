e8a8cc7d565f1a000d3e27892e7b811fd66d7152
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-01-07 20:33:55 -0500

    Time: 46 ms (86.00%), Space: 16.5 MB (78.38%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -12,12 +12,11 @@ class Solution:
             
             if node.val <= lo or node.val >= hi:
                 return False
-            
+
             left = dfs(node.left, lo, node.val)
             right = dfs(node.right, node.val, hi)
             
             return left and right
-            
-            
+ 
         return dfs(root, float('-inf'), float('inf'))
         
