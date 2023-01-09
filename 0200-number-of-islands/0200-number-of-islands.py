ad92854602835dd62a2586dd3c7cfe7a4d6fda8e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-01-09 10:50:01 -0500

    Time: 331 ms (77.94%), Space: 25.1 MB (11.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
-        seen = set()
-        row = len(grid)
-        col = len(grid[0])
+        def valid(x, y):
+            return x >= 0 and x < rows and y >= 0 and y < cols and grid[x][y] == '1'
         
-        def dfs(node):
-            x, y = node
-            
-            for x1, y1 in ((0,1), (0,-1), (1,0), (-1,0)):
+        
+        def dfs(x, y):
+            for x1, y1 in directions:
                 new_x = x + x1
                 new_y = y + y1
-                
-                if new_x < row and new_x >= 0 and new_y < col and new_y >= 0 and (new_x, new_y) not in seen:
-                    if grid[new_x][new_y] == '1':
-                        seen.add((new_x, new_y))
-                        dfs((new_x, new_y))
+                if valid(new_x, new_y) and (new_x, new_y) not in seen:
+                    seen.add((new_x, new_y))
+                    dfs(new_x, new_y)
         
-        for rows in range(len(grid)):
-            for cols in range(len(grid[0])):
-                if grid[rows][cols] == '1' and (rows, cols) not in seen:
-                    seen.add((rows, cols))
-                    dfs((rows, cols))
-                    islands += 1
+        rows = len(grid)
+        cols = len(grid[0])
+        seen = set()
+        directions = ((0,1), (1,0), (0,-1), (-1,0))
+        ans = 0
         
-        return islands
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in seen:
+                    ans += 1
+                    dfs(r,c)
+                    
+        return ans
+                    
         
