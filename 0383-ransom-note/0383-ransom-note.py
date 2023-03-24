57cf10939e0a087de25d8ce4eb34bac3ddeda77f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-24 15:44:53 -0400

    Time: 54 ms (66.62%), Space: 14 MB (89.61%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,3 +1,15 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:  
-        return Counter(ransomNote) <= Counter(magazine)
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+        
+        magazine_hash = collections.Counter(magazine)
+        
+        for letters in ransomNote:
+            if letters not in magazine_hash:
+                return False
+            magazine_hash[letters] -= 1
+            if magazine_hash[letters] == 0:
+                del magazine_hash[letters]
+        
+        return True
