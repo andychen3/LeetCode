f7280bab633315e58190dc2b0ccf838d41ef5f76
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-24 14:32:39 -0400

    Time: 1033 ms (22.98%), Space: 42.9 MB (9.85%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -2,19 +2,33 @@
 from collections import defaultdict
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        ans = len(cards)+1
-        hash_map = defaultdict(int)
-        left = 0
+        hash_cards = defaultdict(list)
         
-        for right, card in enumerate(cards):
-            hash_map[card] += 1
-            while hash_map[card] == 2:
-                ans = min(right - left + 1, ans)
-                hash_map[cards[left]] -= 1
-                if hash_map[cards[left]] == 0:
-                    del hash_map[cards[left]]
-                left += 1
+        for index, card in enumerate(cards):
+            hash_cards[card].append(index)
+            
+        ans = len(cards)+1
+        for pairs in hash_cards.values():
+            n = len(pairs)
+            for i in range(n-1):
+                ans = min(ans, pairs[i+1] - pairs[i] + 1)
         return ans if ans <= len(cards) else -1
+        
+        
+        
+#         ans = len(cards)+1
+#         hash_map = defaultdict(int)
+#         left = 0
+        
+#         for right, card in enumerate(cards):
+#             hash_map[card] += 1
+#             while hash_map[card] == 2:
+#                 ans = min(right - left + 1, ans)
+#                 hash_map[cards[left]] -= 1
+#                 if hash_map[cards[left]] == 0:
+#                     del hash_map[cards[left]]
+#                 left += 1
+#         return ans if ans <= len(cards) else -1
         
         
         
