ce6ecaa9fee27fb0b4a4850480499210801f56c3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-24 15:46:16 -0400

    Time: 39 ms, Memory: 14.2 MB - LeetHub

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -1,26 +1,15 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:        
-        ransom_dict = Counter(ransomNote)
-        magazine_dict = Counter(magazine)
-              
-        ransom_dict.subtract(magazine_dict)
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
         
-        for x in ransom_dict.values():
-            if x > 0:
+        magazine_hash = collections.Counter(magazine)
+        
+        for letters in ransomNote:
+            if letters not in magazine_hash:
                 return False
-            
-        return True
-
-    
-#         char_frequency = {}
+            magazine_hash[letters] -= 1
+            if magazine_hash[letters] == 0:
+                del magazine_hash[letters]
         
-#         for char in magazine:
-#             char_frequency[char] = char_frequency.get(char, 0) + 1
-            
-#         for elements in ransomNote:
-#             if elements in char_frequency and char_frequency[elements] != 0:
-#                 char_frequency[elements] -= 1
-#             else:
-#                 return False
-            
-#         return True
+        return True
