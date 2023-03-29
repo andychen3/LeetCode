dbc12c88dc76c229fb14beeb4a8cb88fc8535ff0
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-29 10:32:49 -0400

    Time: 716 ms (37.39%), Space: 15.4 MB (42.67%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -4,30 +4,30 @@ class Solution:
         if grid[0][0] == 1:
             return -1
         
+        queue = deque([(0, 0, 1)])
+        directions = ((0,1), (1,0), (-1,0), (0,-1), (1,1), (-1,-1), (-1,1), (1,-1))
+        row = len(grid)
+        col = len(grid[0])
+        seen = {(0,0)}
+        
         def valid(r, c):
             return 0 <= r < row and 0 <= c < col and grid[r][c] == 0
         
         
-        row = len(grid)
-        col = len(grid[0])
-        queue = deque([(0,0,1)])
-        directions = ((0,1), (1,0), (0,-1), (-1,0), (1,1), (1,-1), (-1,1), (-1,-1))
-        seen = set()
-        
         while queue:
-            x, y, steps = queue.popleft()
-
-            if (x, y) == (row-1, col-1):
-                return steps
-
-            for dx, dy in directions:
-                new_x, new_y = dx + x, dy + y
-                if valid(new_x, new_y) and (new_x, new_y) not in seen:
-                    seen.add((new_x, new_y))
-                    queue.append((new_x, new_y, steps+1))
+            queue_len = len(queue)
+            
+            for _ in range(queue_len):
+                x, y, steps = queue.popleft()
+                
+                if (x,y) == (row-1, col-1):
+                    return steps
+                
+                for dx, dy in directions:
+                    new_x, new_y = dx + x, dy + y
+                    if valid(new_x, new_y) and (new_x, new_y) not in seen:
+                        seen.add((new_x, new_y))
+                        queue.append((new_x, new_y, steps + 1))
                         
         return -1
-                        
-        
-        
-        
+                
