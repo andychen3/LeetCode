806d515877250309f4b2d59713f28c2f24c0a6f1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-30 11:51:15 -0400

    Time: 161 ms (54.12%), Space: 14.5 MB (21.88%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,17 +1,13 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         by_units = sorted(boxTypes, key=lambda x:-x[1])
-        size = 0
+        size = truckSize
         total = 0
         
         for num_boxes, units in by_units:
-            if truckSize - size - num_boxes >= 0:
-                size += num_boxes
-                total += num_boxes*units
-            elif truckSize - size < num_boxes:
-                remainder = truckSize - size
-                if remainder <= num_boxes:
-                    total += remainder*units
-                    break
+            boxes = min(size, num_boxes)
+            total += boxes*units
+            size -= boxes
+            
         return total
             
