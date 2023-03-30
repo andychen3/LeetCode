817a61f242d57defa98a91df7f0029cdbcd2a640
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-30 16:19:48 -0400

    Time: 102 ms (87.24%), Space: 14.2 MB (71.19%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,17 +1,17 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        def binary_search(arr, target):
-            left = 0
-            right = len(arr)-1
-            while left <= right:
-                mid = (left+right) // 2
-                if arr[mid] == target:
-                    return mid + 1
-                if arr[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            return left
+        # def binary_search(arr, target):
+        #     left = 0
+        #     right = len(arr)-1
+        #     while left <= right:
+        #         mid = (left+right) // 2
+        #         if arr[mid] == target:
+        #             return mid + 1
+        #         if arr[mid] > target:
+        #             right = mid - 1
+        #         else:
+        #             left = mid + 1
+        #     return left
         
         
         temp = sorted(nums)
@@ -22,6 +22,6 @@ class Solution:
             prefix.append(prefix[-1] + temp[i+1])
         
         for num in queries:
-            ans.append(binary_search(prefix, num))
+            ans.append(bisect.bisect_right(prefix, num))
             
         return ans
