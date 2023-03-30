8699dbc051e58bfe39b7f90fbd43aae33a24eb11
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-29 21:00:20 -0400

    Time: 137 ms, Memory: 17.9 MB - LeetHub

--- 0max-area-of-island/0max-area-of-island.py
+++ 0max-area-of-island/0max-area-of-island.py
@@ -6,12 +6,12 @@ class Solution:
         
         
         def dfs(x, y):
-            ans = 0
+            ans = 1
             for dx, dy in ((0,1), (1,0), (-1,0), (0,-1)):
                 new_x, new_y = dx + x, dy + y
                 if valid(new_x, new_y) and (new_x, new_y) not in seen:
                     seen.add((new_x, new_y))
-                    ans += dfs(new_x, new_y)+1
+                    ans += dfs(new_x, new_y)
             return ans
         
         
@@ -24,6 +24,6 @@ class Solution:
             for c in range(col):
                 if grid[r][c] and (r,c) not in seen:
                     seen.add((r,c))
-                    ans = max(ans, dfs(r, c)+1)
+                    ans = max(ans, dfs(r, c))
         return ans
                     
