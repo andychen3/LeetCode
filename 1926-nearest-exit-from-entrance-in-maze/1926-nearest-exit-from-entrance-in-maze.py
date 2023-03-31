846c4364e48553a5e12455a9a0d605d8a58aca7a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-31 09:49:44 -0400

    Time: 806 ms (47.95%), Space: 15.5 MB (25.48%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,22 +1,21 @@
 from collections import deque
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        
-        x, y = entrance
-        row = len(maze)
-        col = len(maze[0])
-        queue = deque([(x, y, 0)])
-        seen = {(x, y)}
-        directions = ((0,1), (1,0), (-1,0), (0,-1))
-        
         def valid(r, c):
             return 0 <= r < row and 0 <= c < col and maze[r][c] == '.'
+        
+        row = len(maze)
+        col = len(maze[0])
+        directions = ((0,1), (1,0), (-1,0), (0,-1))
+        start_x, start_y = entrance
+        queue = deque([(start_x, start_y, 0)])
+        seen = {(start_x, start_y)}
         
         while queue:
             x, y, steps = queue.popleft()
             
             if [x,y] != entrance:
-                if x == 0 or y == 0 or x == row - 1 or y == col -1:
+                if x == 0 or x == row-1 or y == 0 or y == col-1:
                     return steps
             
             for dx, dy in directions:
@@ -24,5 +23,6 @@ class Solution:
                 if valid(new_x, new_y) and (new_x, new_y) not in seen:
                     seen.add((new_x, new_y))
                     queue.append((new_x, new_y, steps + 1))
+                    
         return -1
-            
+        
