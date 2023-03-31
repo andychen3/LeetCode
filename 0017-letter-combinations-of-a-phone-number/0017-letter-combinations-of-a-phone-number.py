a24f3fb07daa88685f04282d3673458d0badb110
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-31 13:02:35 -0400

    Time: 33 ms (53.54%), Space: 13.9 MB (66.17%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,32 +1,22 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if digits == "":
             return []
         
-        digit_hash = {'2':['a','b','c'],
-                      '3':['d','e','f'],
-                      '4':['g','h','i'],
-                      '5':['j','k','l'],
-                      '6':['m','n','o'],
-                      '7':['p','q','r','s'],
-                      '8':['t','u','v'],
-                      '9':['w','x','y','z']}
+        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
+                   "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         
         def backtrack(curr, i):
             if len(curr) == len(digits):
-                string = "".join(curr)
-                ans.append(string)
+                ans.append("".join(curr[:]))
                 return
-
-            for index in range(i, len(digits)):
-                for letters in digit_hash[digits[index]]:
-                    curr.append(letters)
-                    backtrack(curr, i+1)
-                    curr.pop()
-                break
-        
+            
+            curr_digits = letters[digits[i]]
+            for c in curr_digits:
+                curr.append(c)                
+                backtrack(curr, i + 1)
+                curr.pop()
+                
         ans = []
         backtrack([], 0)
         return ans
-        
-        
