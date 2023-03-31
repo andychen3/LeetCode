d5f9881142f072bb06c4b0a1ff62bb0c49f96a7a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-03-31 08:31:16 -0400

    Time: 204 ms (86.81%), Space: 15.3 MB (54.42%) - LeetHub

---
+++ 1231-divide-chocolate/1231-divide-chocolate.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        def check(chunks):
+            curr_sweetness = 0
+            num_people = 0
+            
+            for sweets in sweetness:
+                curr_sweetness += sweets
+                if curr_sweetness > chunks:
+                    curr_sweetness = 0
+                    num_people += 1
+            
+            return num_people < k+1
+        
+        left = min(sweetness)
+        right = sum(sweetness) // (k+1)
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
