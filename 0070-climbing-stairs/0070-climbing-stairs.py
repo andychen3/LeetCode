7f6bf10298bd8cc100cda4969c905623b8429cfc
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-01 10:27:24 -0400

    Time: 35 ms (32.84%), Space: 13.9 MB (44.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,8 +2,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         @cache
         def dp(i):
-            if i <= 1:
+            if i == 1:
                 return 1
+            if i == 2:
+                return 2
             
             return dp(i-1) + dp(i-2)
         return dp(n)
