174b0754e07687c9b1f44efa00a9b0fa8f146acf
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-01 08:21:49 -0400

    Time: 91 ms (57.44%), Space: 14.2 MB (23.64%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,3 +1,4 @@
+
 from collections import defaultdict, deque
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
@@ -7,21 +8,21 @@ class Solution:
         graph = defaultdict(lambda: defaultdict(list))
         for x, y in redEdges:
             graph[RED][x].append(y)
-            
         for x,y in blueEdges:
             graph[BLUE][x].append(y)
             
+        ans = [float("inf")] * n
         queue = deque([(0, RED, 0), (0, BLUE, 0)])
-        seen = {(0,RED), (0,BLUE)}
-        ans = [float("inf")]*n
+        seen = {(0, RED), (0, BLUE)}
         
         while queue:
             node, color, steps = queue.popleft()
-            ans[node] = min(ans[node], steps)
+            ans[node] = min(steps, ans[node])
             
             for neighbors in graph[color][node]:
                 if (neighbors, 1 - color) not in seen:
                     seen.add((neighbors, 1 - color))
                     queue.append((neighbors, 1 - color, steps + 1))
+        return [nums if nums != float("inf") else - 1 for nums in ans]
         
-        return [num if num != float("inf") else -1 for num in ans]
+        
