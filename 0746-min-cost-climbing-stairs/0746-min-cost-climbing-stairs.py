1999fd2389048058d3be840f8b373ce8d01d8e10
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-02 10:22:49 -0400

    Time: 66 ms (27.73%), Space: 16.7 MB (5.98%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -6,6 +6,6 @@ class Solution:
                 return 0
             
             return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
-            
         
         return dp(len(cost))
+        
