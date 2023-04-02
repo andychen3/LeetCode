61f83b6da9e7088d1ee72ec5da23db8d725a3968
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-02 09:25:35 -0400

    Time: 1038 ms (79.70%), Space: 38.7 MB (73.60%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,20 +1,19 @@
 import heapq
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        heap = []
         projects = sorted(zip(capital, profits))
-        picked = 0
+        heap = []
         i = 0
         
         for _ in range(k):
-            while picked != k and i < len(projects) and projects[i][0] <= w:
+            while i < len(projects) and projects[i][0] <= w:
                 heapq.heappush(heap, -projects[i][1])
                 i += 1
-
+                
             if len(heap) == 0:
                 return w
-
+            
             w -= heapq.heappop(heap)
-            picked += 1
-                
+            
+            
         return w
