ce79cc790736961dc7db197022f495c1c3a7ac0f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-01 20:17:30 -0400

    Time: 85 ms (79.34%), Space: 14 MB (26.26%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -3,14 +3,8 @@ class Solution:
         row = len(matrix)
         col = len(matrix[0])
         
-        for r in range(row):
-            for c in range(col):
-                # check top left
-                if 0 <= r-1 and 0 <= c-1: 
-                    if matrix[r-1][c-1] != matrix[r][c]:
-                        return False
-                if r+1 < row and c+1 < col: 
-                    if matrix[r+1][c+1] != matrix[r][c]:
-                        return False
+        for r in range(row-1):
+            for c in range(col-1):
+                if matrix[r][c] != matrix[r +1][c +1]:
+                    return False
         return True
-        
