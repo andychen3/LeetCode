8e336a4dc92584b3e280b7e8e45d5c09b931dbd4
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-04 19:14:17 -0400

    Time: 34 ms (69.77%), Space: 13.9 MB (40.63%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,15 +1,15 @@
+from collections import defaultdict
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        balloon_hash = collections.defaultdict(int)
+        if len(text) < 7:
+            return 0
+        hash_map = defaultdict(int)
         
-        for char in text:
-            if char in 'balloon':
-                balloon_hash[char] += 1
-        balloon_hash['l'] //= 2
-        balloon_hash['o'] //= 2
-        return min(balloon_hash.values()) if len(balloon_hash) == 5 else 0
+        for vals in text:
+            if vals in "balon":
+                hash_map[vals] += 1
         
+        hash_map['l'] = hash_map['l'] // 2
+        hash_map['o'] = hash_map['o'] // 2
         
-                
-        
-        
+        return min(hash_map.values()) if len(hash_map) == 5 else 0
