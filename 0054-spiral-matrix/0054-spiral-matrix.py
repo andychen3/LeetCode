f93df20ff3d5476ef54482fa24bbbd83db13d3b4
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-07 10:09:54 -0400

    Time: 29 ms (79.03%), Space: 14 MB (21.65%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,24 +4,31 @@ class Solution:
         res = []
         
         while left < right and top < bottom:
+            # going left to right
             for i in range(left, right):
                 res.append(matrix[top][i])
             top += 1
             
+            # top to bottom
+            
             for i in range(top, bottom):
                 res.append(matrix[i][right-1])
-            
+                
             right -= 1
             
-            if not (left < right and top < bottom):
+            if not(left < right and top < bottom):
                 break
                 
+            #right to left
+            
             for i in range(right - 1, left - 1, -1):
                 res.append(matrix[bottom-1][i])
-            bottom -= 1    
-                
-            for i in range(bottom - 1, top -1, -1):
+            bottom -= 1
+            
+            # bottom to top
+            for i in range(bottom-1, top-1, -1):
                 res.append(matrix[i][left])
+            
             left += 1
         return res
                 
