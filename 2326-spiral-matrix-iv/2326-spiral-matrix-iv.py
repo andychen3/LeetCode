0ecdcc78f5980743b3d0a725b673299ed094cbcc
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-07 13:46:02 -0400

    Time: 1533 ms (43.48%), Space: 64.9 MB (68.12%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -4,18 +4,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:        
-        res = [[-1]*n for _ in range(m)]
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        matrix = [[-1]*n for _ in range(m)]
         
-        # if the next point is out of bounds or is on a previous number we have to change directions
         def valid(r, c):
-            return 0 > r or r >= m or 0 > c or c >= n or res[r][c] != -1
+            return r < 0 or r >= m or c < 0 or c >= n or matrix[r][c] != -1
         
         x, y = 0, 0
         dx, dy = 0, 1
         
         while head:
-            res[x][y] = head.val
+            matrix[x][y] = head.val
             
             if valid(x+dx, y+dy):
                 dx, dy = dy, -dx
@@ -23,5 +22,4 @@ class Solution:
             x += dx
             y += dy
             head = head.next
-        
-        return res
+        return matrix
