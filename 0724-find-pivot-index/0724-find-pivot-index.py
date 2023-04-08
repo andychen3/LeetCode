6630d541fe3c1d03c4ec5903224d131e353d0a11
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-08 07:32:06 -0400

    Time: 156 ms (55.60%), Space: 15.3 MB (35.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,7 +1,6 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         total = sum(nums)
-        left = 0
         left_total = 0
         
         
@@ -9,7 +8,6 @@ class Solution:
             total -= val
             if left_total == total:
                 return index
-            left += 1
             left_total += val
         return -1
             
