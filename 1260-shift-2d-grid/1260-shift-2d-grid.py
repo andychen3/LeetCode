4fe93b77b07b7d6294d8eefdca43edb66b60840b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-09 10:21:01 -0400

    Time: 165 ms (58.04%), Space: 14.3 MB (57.30%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -3,10 +3,10 @@ class Solution:
         row = len(grid)
         col = len(grid[0])
         
-        mat = [[0]*col for _ in range(row)]
+        ans = [[0]*col for _ in range(row)]
         
         for i in range(row*col):
-            index = (i+k) % (row*col)
-            mat[index//col][index%col] = grid[i//col][i%col]
+            index = (i + k) % (row*col)
+            ans[index // col][index % col] = grid[i // col][i % col]
+        return ans
         
-        return mat
