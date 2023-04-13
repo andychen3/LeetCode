bb9bcdc543dc06cc05c3d5b89dd6105db39ef79b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-13 08:15:54 -0400

    Time: 1358 ms (78.27%), Space: 28.5 MB (66.64%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,16 +3,9 @@ class Solution:
         stack = []
         ans = [0]*len(temperatures)
         
-        for index, temps in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < temps:
+        for index, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
                 idx = stack.pop()
                 ans[idx] = index-idx
-                
             stack.append(index)
         return ans
-            
-            
-        
-
-                    
-            
