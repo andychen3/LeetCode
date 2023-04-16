b168245e9ecfa94c3a2c626e7611611b44fcd5a1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-16 10:43:27 -0400

    Time: 44 ms (52.01%), Space: 14.3 MB (39.24%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,28 +1,16 @@
+from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_hash = collections.defaultdict(int)
-        t_hash = collections.defaultdict(int)
+        s_map = defaultdict(int)
+        t_map = defaultdict(int)
         
         for index, s_char in enumerate(s):
             t_char = t[index]
-            if s_char not in s_hash and t_char not in t_hash:
-                s_hash[s_char] = t_char
-                t_hash[t_char] = s_char
-            else:
-                if s_char in s_hash and s_hash[s_char] != t_char or t_char in t_hash and t_hash[t_char] != s_char:
-                    return False
+            if s_char in s_map and s_map[s_char] != t_char:
+                return False
+            if t_char in t_map and t_map[t_char] != s_char:
+                return False
+            s_map[s_char] = t_char
+            t_map[t_char] = s_char
+                
         return True
-                
-        
-
-            
-        
-        
-        
-
-
-                
-                
-            
-            
-        
