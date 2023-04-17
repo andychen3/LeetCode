b5ed5565c680f149096733302c36ed897dc1e89f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-17 08:40:03 -0400

    Time: 91 ms (62.39%), Space: 24.1 MB (36.06%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -9,26 +9,29 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         if not head:
-            return 
-        # use fast and slow pointer and then start reversing mid way
+            return
         
         slow = fast = head
         
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            
+        
         curr = slow
         prev = None
         
         while curr:
             curr.next, curr, prev = prev, curr.next, curr
+        
+        # merge the list
+          
+        first_ptr = head
+        second_ptr = prev
+        
+        while second_ptr and second_ptr.next:
+            first_ptr.next, first_ptr = second_ptr, first_ptr.next
+            second_ptr.next, second_ptr = first_ptr, second_ptr.next
+        
+        return head
+        
             
-        ptr1 = head
-        
-        while prev.next:
-            ptr1.next, ptr1 = prev, ptr1.next
-            prev.next, prev = ptr1, prev.next
-        
-        
-        # start merging the two lists together
