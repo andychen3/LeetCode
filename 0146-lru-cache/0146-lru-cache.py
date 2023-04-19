949fae42c129b7923b9bf0dc525b743b24820538
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-19 09:10:41 -0400

    Time: 878 ms (57.17%), Space: 76.6 MB (21.15%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -18,10 +18,9 @@ class LRUCache:
         
     def _add(self, node):
         prev = self.tail.prev
-        nxt = self.tail
         prev.next = node
-        nxt.prev = node
-        node.next = nxt
+        self.tail.prev = node
+        node.next = self.tail
         node.prev = prev
         
         
@@ -47,19 +46,15 @@ class LRUCache:
         if key in self.cache:
             node = self.cache[key]
             self._remove(node)
-            newNode = Node(key, value)
-            self.cache[key] = newNode
-            self._add(newNode)
-        else:
+        newNode = Node(key, value)
+        self.cache[key] = newNode
+        self._add(newNode)
 
-            self.cache[key] = Node(key, value)
-            node = self.cache[key]
-            self._add(node)
-            if len(self.cache) > self.capacity:
-                lru = self.head.next
-                self._remove(lru)
-                del self.cache[lru.key]
-            
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
+
 
 
 # Your LRUCache object will be instantiated and called as such:
