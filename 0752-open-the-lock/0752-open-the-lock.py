0f88075d596e8fe6aaf27bd741fe174a82a90290
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-27 07:46:15 -0400

    Time: 599 ms (76.89%), Space: 17.8 MB (5.72%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,36 +1,31 @@
 from collections import deque
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        def change_lock(node):
+        def check(node):
             ans = []
             for i in range(4):
                 num = int(node[i])
                 for x in [-1, 1]:
-                    change = (num+x) % 10
-                    ans.append(node[:i] + str(change) + node[i+1:])
+                    change = (x + num) % 10
+                    ans.append(slots[:i] + str(change) + slots[i+1:])
             return ans
             
+        
         
         if '0000' in deadends:
             return -1
         
         seen = set(deadends)
-        seen.add("0000")
+        seen.add('0000')
         d = deque([('0000', 0)])
         
         while d:
-            for _ in range(len(d)):
-                node, steps = d.popleft()
-                if node == target:
-                    return steps
-                
-                for neighbor in change_lock(node):
-                    if neighbor not in seen:
-                        seen.add(neighbor)
-                        d.append((neighbor, steps+1))
+            slots, moves = d.popleft()
+            if slots == target:
+                return moves
+            
+            for neighbors in check(slots):
+                if neighbors not in seen:
+                    seen.add(neighbors)
+                    d.append((neighbors, moves + 1))
         return -1
-            
-            
-        
-        
-        
