ddf31c4a73fdb301a2feb178cb3a91f8814cf218
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-29 08:31:16 -0400

    Time: 284 ms (50.57%), Space: 18.1 MB (5.30%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,31 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights:
-            return []
-
+        pacific_set = set()
+        atlantic_set = set()
+        directions = ((0,1), (1,0), (-1,0), (0,-1))
+        rows = len(heights)
+        cols = len(heights[0])
+        
         def valid(r, c):
             return 0 <= r < rows and 0 <= c < cols
         
         
-        p_visited = set()
-        a_visited = set()
-        rows, cols = len(heights), len(heights[0])
-        directions = ((0,1), (1,0), (-1,0), (0,-1))
-        
-        def traverse(x, y, visited):
-            visited.add((x, y))
+        def dfs(r, c, visited):
+            if (r,c) in visited:
+                return
+            
+            visited.add((r,c))
+            
             for dx, dy in directions:
-                new_x, new_y = dx + x, dy + y
-                if valid(new_x, new_y) and (new_x, new_y) not in visited:
-                    
-                    if heights[new_x][new_y] >= heights[x][y]:
-                        traverse(new_x, new_y, visited)
+                new_dx, new_dy = dx + r, dy + c
+                if valid(new_dx, new_dy):
+                    if heights[new_dx][new_dy] >= heights[r][c]:
+                        dfs(new_dx, new_dy, visited)
         
-        for row in range(rows):
-            traverse(row, 0, p_visited)
-            traverse(row, cols-1, a_visited)
-        for col in range(cols):
-            traverse(0, col, p_visited)
-            traverse(rows-1, col, a_visited)
         
-        return list(p_visited & a_visited)
+        # iterate through rows and cols to get the borders
+        for r in range(rows):
+            dfs(r, 0, pacific_set)
+            dfs(r, cols-1, atlantic_set)
+        for c in range(cols):
+            dfs(0, c, pacific_set)
+            dfs(rows-1, c, atlantic_set)
+        
+        return list(pacific_set & atlantic_set)
