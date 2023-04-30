e5725cf92ad2afa71623518f055cf874f67202bd
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-04-30 13:00:25 -0400

    Time: 292 ms (39.03%), Space: 18.2 MB (5.95%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,34 +1,29 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific_set = set()
-        atlantic_set = set()
-        directions = ((0,1), (1,0), (-1,0), (0,-1))
-        rows = len(heights)
-        cols = len(heights[0])
         
         def valid(r, c):
             return 0 <= r < rows and 0 <= c < cols
         
+        pacific_set = set()
+        atlantic_set = set()
+        rows = len(heights)
+        cols = len(heights[0])
+        directions = ((0,1), (1,0), (-1,0), (0,-1))
         
-        def dfs(r, c, visited):
-            if (r,c) in visited:
-                return
-            
-            visited.add((r,c))
-            
+        def dfs(x, y, visited):
+            visited.add((x,y))
             for dx, dy in directions:
-                new_dx, new_dy = dx + r, dy + c
-                if valid(new_dx, new_dy):
-                    if heights[new_dx][new_dy] >= heights[r][c]:
+                new_dx, new_dy = dx + x, dy + y
+                if valid(new_dx, new_dy) and (new_dx, new_dy) not in visited:
+                    if heights[new_dx][new_dy] >= heights[x][y]:
                         dfs(new_dx, new_dy, visited)
         
-        
-        # iterate through rows and cols to get the borders
         for r in range(rows):
             dfs(r, 0, pacific_set)
             dfs(r, cols-1, atlantic_set)
         for c in range(cols):
             dfs(0, c, pacific_set)
             dfs(rows-1, c, atlantic_set)
-        
+            
         return list(pacific_set & atlantic_set)
+        
