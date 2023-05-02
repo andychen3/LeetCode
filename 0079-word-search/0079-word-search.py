da6902e2361f69588f7a17c15305e495d497e1e6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-05-02 09:16:46 -0400

    Time: 8427 ms (29.39%), Space: 16.4 MB (5.69%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,38 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def valid(r, c):
-            return 0 <= r < rows and 0 <= c < col
-        
+            return 0 <= r < rows and 0 <= c < cols
         
         rows = len(board)
-        col = len(board[0])
+        cols = len(board[0])
+        directions = ((0,1), (1,0), (-1,0), (0,-1))
         
-        def backtrack(i, coord, seen):
-            directions = ((0,1), (1,0), (-1,0), (0,-1))
+        def backtrack(char_set, index, coords):
             
-            if i == len(word):
+            x, y = coords
+            
+            if index == len(word):
                 return True
-            x, y = coord
-            
             for dx, dy in directions:
-                new_x, new_y = dx + x, dy + y
-                if valid(new_x, new_y) and (new_x, new_y) not in seen:
-                    if board[new_x][new_y] == word[i]:
-                        seen.add((new_x, new_y))
-                        if backtrack(i+1, (new_x,new_y), seen):
-                            return True
-                        seen.remove((new_x,new_y))
+                new_dx, new_dy = dx + x, dy + y
+                if valid(new_dx, new_dy) and board[new_dx][new_dy] == word[index] \
+                and (new_dx,new_dy) not in char_set:
+                    char_set.add((new_dx,new_dy))
+                    if backtrack(char_set, index+1, (new_dx, new_dy)):
+                        return True
+                    char_set.remove((new_dx, new_dy))
             return False
-            
+    
+        
         for r in range(rows):
-            for c in range(col):
-                if board[r][c] == word[0] and backtrack(1, (r,c), {(r,c)}):
+            for c in range(cols):
+                if board[r][c] == word[0] and backtrack({(r,c)}, 1, (r, c)):
                     return True
+                    
+                    
         return False
+        
+        
+        
+        
+        
