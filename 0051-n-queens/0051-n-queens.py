d0a3e451a5ee28e40f68c77dbd12db5894f075fc
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-05-04 08:09:36 -0400

    Time: 63 ms (52.66%), Space: 16.7 MB (8.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -15,18 +15,19 @@ class Solution:
                 diagonal = row + col
                 anti_diagonal = row - col
                 
-                if not (diagonal in diag_set or anti_diagonal in anti_diag_set or col in col_set):
-                    col_set.add(col)
-                    diag_set.add(diagonal)
-                    anti_diag_set.add(anti_diagonal)
-                    board[row][col] = 'Q'
-                    
-                    backtrack(row + 1)
-                    
-                    col_set.remove(col)
-                    diag_set.remove(diagonal)
-                    anti_diag_set.remove(anti_diagonal)
-                    board[row][col] = "."
+                if (diagonal in diag_set or anti_diagonal in anti_diag_set or col in col_set):
+                    continue
+                col_set.add(col)
+                diag_set.add(diagonal)
+                anti_diag_set.add(anti_diagonal)
+                board[row][col] = 'Q'
+
+                backtrack(row + 1)
+
+                col_set.remove(col)
+                diag_set.remove(diagonal)
+                anti_diag_set.remove(anti_diagonal)
+                board[row][col] = "."
                     
         backtrack(0)
         return ans
