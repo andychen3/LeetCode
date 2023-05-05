076111e04a83affd4bd5129b6088ddca3e1c2af3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-05-05 07:57:02 -0400

    Time: 67 ms (48.91%), Space: 16.8 MB (5.07%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,6 +1,6 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [["."]*n for _ in range(n)]
+        empty_board = [["."]*n for _ in range(n)]
         col_set = set()
         diag_set = set()
         anti_diag_set = set()
@@ -8,29 +8,29 @@ class Solution:
         
         def backtrack(row):
             if row == n:
-                ans.append(["".join(row) for row in board])
+                ans.append(["".join(empty_board[r]) for r in range(row)])
                 return
-            
-            for col in range(n):
-                diagonal = row + col
-                anti_diagonal = row - col
                 
-                if (diagonal in diag_set or anti_diagonal in anti_diag_set or col in col_set):
+            for col in range(n):
+                if (col in col_set or (row-col) in diag_set or
+                    (row + col) in anti_diag_set):    
                     continue
+                
                 col_set.add(col)
-                diag_set.add(diagonal)
-                anti_diag_set.add(anti_diagonal)
-                board[row][col] = 'Q'
-
+                diag_set.add(row-col)
+                anti_diag_set.add(row+col)
+                empty_board[row][col] = 'Q'
+                
                 backtrack(row + 1)
-
+                
                 col_set.remove(col)
-                diag_set.remove(diagonal)
-                anti_diag_set.remove(anti_diagonal)
-                board[row][col] = "."
-                    
-        backtrack(0)
+                diag_set.remove(row-col)
+                anti_diag_set.remove(row+col)
+                empty_board[row][col] = '.'
+        
+        backtrack(0)     
         return ans
-            
+                    
+                    
             
             
