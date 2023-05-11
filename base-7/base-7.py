71aebcb2b2ac65c6a0332197f113c80a5887d269
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-05-11 08:10:27 -0400

    Time: 44 ms, Memory: 16.3 MB - LeetHub

--- base-7/base-7.py
+++ base-7/base-7.py
@@ -2,18 +2,18 @@ class Solution:
     def convertToBase7(self, num: int) -> str:
         if num == 0:
             return "0"
-        res = []
-        negative = False
-        if num < 0:
-            negative = True
-            num = abs(num)
         
+        res = []
+        signed = False
+        if num < 0:
+            signed = True
+        num = abs(num)
+            
         while num:
-            quotient, remainder = divmod(num, 7)
+            num, remainder = divmod(num, 7)
             res.append(str(remainder))
-            num = quotient
-
-        if negative:
+        
+        if signed:
             res.append("-")
-        ans = "".join(reversed(res))
-        return ans
+        
+        return "".join(reversed(res))
