186df1a3c9e84c4d2e0f2cb8c0abf879da93d009
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-06-12 09:41:35 -0400

    Time: 1445 ms (65.78%), Space: 20.4 MB (72.55%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        def two_ptrs(left, right, third):
+        res = []
+        for index, num in enumerate(nums):
+            if index > 0 and num == nums[index-1]:
+                continue
+            left = index+1
+            right = len(nums) - 1
             while left < right:
-                ans = nums[left] + nums[right] + third
-                if ans == 0:
-                    res.append([nums[left], nums[right], third])
+                current_sum = num + nums[left] + nums[right]
+                if current_sum == 0:
+                    res.append([num, nums[left], nums[right]])
                     left += 1
                     right -= 1
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
-                elif ans > 0:
+                elif current_sum > 0:
                     right -= 1
                 else:
                     left += 1
-        
-        res = []
-        right = len(nums)-1
-        
-        for index, num in enumerate(nums):
-            if index > 0 and num == nums[index-1]:    
-                continue
-            two_ptrs(index+1, right, num)
         return res
-        
-        
-        
-        
-                    
