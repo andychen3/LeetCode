9cd40d05c31cfd376d2a571291e864f317fd540a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-06-13 07:55:00 -0400

    Time: 1338 ms (52.77%), Space: 16.4 MB (68.58%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -1,18 +1,18 @@
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
         nums.sort()
-        res = 0
+        ans = 0
         N = len(nums)
         
         for index, num in enumerate(nums):
             left = index + 1
             right = N - 1
             while left < right:
-                curr_sum = num + nums[left] + nums[right]
-                if curr_sum < target:
-                    res += right - left
+                _sum = num + nums[left] + nums[right]
+                if _sum < target:
+                    ans += right - left
                     left += 1
                 else:
                     right -= 1
-        return res
-                
+        return ans
+        
