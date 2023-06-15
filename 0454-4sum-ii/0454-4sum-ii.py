0244d30f2751568e309e60dd59d4a66ee7345035
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-06-15 07:44:41 -0400

    Time: 566 ms (87.57%), Space: 16.5 MB (46.10%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,17 +1,15 @@
+from collections import defaultdict
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        counts = {}
+        counts = defaultdict(int)
         ans = 0
         
         for a in nums1:
             for b in nums2:
-                if (a+b) not in counts:
-                    counts[a+b] = 0
                 counts[a+b] += 1
-                
+        
         
         for c in nums3:
             for d in nums4:
-                if -c-d in counts:
-                    ans += counts[-(c+d)] 
+                ans += counts[-(c+d)]
         return ans
