921b6fbee9ae6d0dda4a73faf968b60802963dc0
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-08-21 07:01:09 -0400

    Time: 1515 ms (86.75%), Space: 56 MB (47.63%) - LeetHub

--- 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
@@ -12,16 +12,17 @@ class Solution:
             count = 1
             curr = tail.next
 
-            # Finding the number of nodes in the next group
+            # find number of nodes within group
+            # It is curr.next because you have to find out if the next element exists
             while curr.next and count < group:
-                curr = curr.next
                 count += 1
+                curr = curr.next
 
-            prev = tail
-            curr = tail.next
+            # Set variables to begin reversal
+            prev, curr = tail, tail.next
 
             if count % 2 == 0:
-                # reverse the nodes in the group
+                # cookie cutter traversal
                 while count and curr:
                     next_node = curr.next
                     curr.next = prev
@@ -29,11 +30,16 @@ class Solution:
                     prev = curr
                     curr = next_node
                     count -= 1
+                # Find start of current group
                 first = tail.next
+                # Set the current group's first node to connect to next group's first node
                 first.next = curr
+                # Set last group's end node to the current group's end node
                 tail.next = prev
+                # Move tail to end node of current group
                 tail = first
             else:
+                # No reversal
                 while count and curr:
                     prev = curr
                     curr = curr.next
@@ -41,6 +47,4 @@ class Solution:
                 tail = prev
             
             group += 1
-        
         return head
-        
