740b2ba5e2ba2f61a5fb4f7901ec2b6b69b3113c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-08-22 08:22:06 -0400

    Time: 99 ms (84.42%), Space: 17.6 MB (45.53%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,14 +3,20 @@ class Solution:
         stack = []
         
         for asteroid in asteroids:
+            # Check if there are asteroids in stack and if top asteroid is positive
+            # while the current asteroid is negative
             while stack and stack[-1] > 0 and asteroid < 0:
+                # Check if the top asteroid is smaller than the left moving asteroid
                 if stack[-1] + asteroid < 0:
                     stack.pop()
+                # If the top asteroid is bigger than the left moving skip
                 elif stack[-1] + asteroid > 0:
                     break
+                # both are the same size so pop top asteroid
                 else:
                     stack.pop()
                     break
+            # Else add the asteroid since they are all facing the same direction
             else:
                 stack.append(asteroid)
         return stack
