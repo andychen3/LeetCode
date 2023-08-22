1012df6760d6506c1475ea9e0deec1d1c33801af
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-08-22 08:23:06 -0400

    Time: 91 ms (94.49%), Space: 17.5 MB (45.53%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,23 +1,24 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
-        
+    
         for asteroid in asteroids:
-            # Check if there are asteroids in stack and if top asteroid is positive
-            # while the current asteroid is negative
-            while stack and stack[-1] > 0 and asteroid < 0:
-                # Check if the top asteroid is smaller than the left moving asteroid
-                if stack[-1] + asteroid < 0:
-                    stack.pop()
-                # If the top asteroid is bigger than the left moving skip
-                elif stack[-1] + asteroid > 0:
-                    break
-                # both are the same size so pop top asteroid
-                else:
-                    stack.pop()
-                    break
-            # Else add the asteroid since they are all facing the same direction
-            else:
+            # Check if asteroid is moving to the right or stack is empty
+            if not stack or asteroid > 0:
                 stack.append(asteroid)
+            else:
+                # Handle collisions with asteroids moving to the left
+                while stack and stack[-1] > 0:
+                    top_asteroid = stack.pop()
+                    if abs(top_asteroid) == abs(asteroid):
+                        # Both asteroids explode
+                        break
+                    elif abs(top_asteroid) > abs(asteroid):
+                        # Top asteroid survives and current asteroid explodes
+                        stack.append(top_asteroid)
+                        break
+                else:
+                    # The current asteroid survives and is moving to the left
+                    stack.append(asteroid)
+        
         return stack
-
