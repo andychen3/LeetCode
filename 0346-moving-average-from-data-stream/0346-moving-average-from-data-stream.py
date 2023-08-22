43410a0b8b454603c845c6312bb9c74aa2dd0d6b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-08-22 08:56:58 -0400

    Time: 94 ms (36.69%), Space: 19.6 MB (33.91%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,26 +1,20 @@
+from collections import deque
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.window = collections.deque()
-        self.curr_sum = 0
-        self.window_size = 0
         self.size = size
+        self.d = deque()
+        
 
     def next(self, val: int) -> float:
-        if self.window_size < self.size:
-            self.window.append(val)
-            self.curr_sum += val
-            self.window_size += 1
-            return self.curr_sum / self.window_size
+        if len(self.d) < self.size:
+            self.d.append(val)
+            return sum(self.d) / len(self.d)
         else:
-            deleted_element = self.window.popleft()
-            self.curr_sum -= deleted_element
-            self.window_size -= 1
-            self.window.append(val)
-            self.curr_sum += val
-            self.window_size += 1
-            return self.curr_sum / self.window_size
-
+            self.d.popleft()
+            self.d.append(val)
+            return sum(self.d) / self.size
+        
 
 
 # Your MovingAverage object will be instantiated and called as such:
