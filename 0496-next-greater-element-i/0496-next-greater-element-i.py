e7eb93c44552c41b5e302bf1f8929ddef4b09dad
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-08-23 06:45:53 -0400

    Time: 63 ms (55.82%), Space: 16.4 MB (93.93%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,15 +2,16 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
         map = {}
-    
+
+        # build the hashmap with numbers:greater number
         for num in nums2:
-            while stack and num > stack[-1]:
+            while stack and stack[-1] < num:
                 map[stack.pop()] = num
             stack.append(num)
-    
-        while stack:
-            map[stack.pop()] = -1
-    
-        res = [map[num] for num in nums1]
-                                            
-        return res
+
+        # check if stack is empty
+        for num in stack:
+            map[num] = -1
+        
+        return [map[nums] for nums in nums1]
+
