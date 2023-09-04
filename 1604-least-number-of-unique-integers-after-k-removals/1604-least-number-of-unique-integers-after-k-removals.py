7824d69e63cddad08805fc62f1009f2c9ad58ce7
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-04 19:25:22 -0400

    Time: 353 ms (95.08%), Space: 33.4 MB (58.36%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.py
@@ -1,19 +1,16 @@
 from collections import Counter
-import heapq
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         counts = Counter(arr)
-        heap = []
+        freq = sorted(counts.values(), reverse=True) # Sort all the frequency values in reverse order
 
-        # The reason this solution is slower because I am adding all n elements into the heap
-        # Then i am iterating k times and performing log(n) operations.
-        for key, freq in counts.items():
-            heapq.heappush(heap, (freq, key))
-        
-        for _ in range(k):
-            freq, key = heapq.heappop(heap)
-            if freq - 1 != 0:
-                heapq.heappush(heap, (freq-1, key))
-        
-        return len(heap)
+        while k:
+            num = freq[-1] # We want the last element since it is the least frequency
+            # If the frequency is enough to decrement k we do it and then pop it
+            if num <= k:
+                k -= num
+                freq.pop()
+            else:
+                break
+        return len(freq)
 
