e5edf92683fafee06ae85f3bda5c8108ba0a5001
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-04 11:29:45 -0400

    Time: 752 ms (86.36%), Space: 31.1 MB (63.16%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
@@ -2,9 +2,15 @@ class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
         nums.sort()
         left = 0
-        ans = 1
+        # We start ans to 1 because we always have 1 element and since k can be 0
+        # and an element minus itself will always be 0 so there will always be one
+        # subsequence
+        ans = 1 
 
         for right, num in enumerate(nums):
+            # We do it this way so we don't need that second while loop and continue statement
+            # we only increment answer when we start a new subsequence and set the next min val to the
+            # start of the next subsequence
             if num - nums[left] > k:
                 ans += 1    
                 left = right 
