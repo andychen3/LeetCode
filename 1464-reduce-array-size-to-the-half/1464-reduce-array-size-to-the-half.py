283861f46d1140d13f08f8bd2b9bc5a642882a2c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-05 06:01:48 -0400

    Time: 465 ms (96.69%), Space: 33.3 MB (79.71%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        # it can potentially be greedy because it asks for min size.
+        # Also because you can kind of tell that the most optimal solution will probably
+        # come from deleting the number that has the most occurences
+        n = len(arr) // 2
+        counts = Counter(arr)
+        freq = sorted(counts.values())
+        ans = 0
+        
+        while freq:
+            if n <= 0:
+                break
+            val = freq[-1]
+            n -= val
+            freq.pop()
+            ans += 1
+
+        return ans
+            
