990e258873634a37a3570e0db2b0f83e060e1c1d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-06 08:14:55 -0400

    Time: 44 ms (28.36%), Space: 16.4 MB (13.94%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,21 +2,19 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        hash_map = {'2':'abc', '3':'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9':'wxyz'}
-        def backtrack(arr, i):
-            if i >= len(digits):
-                ans.append("".join(arr[:]))
+        num_char = {'2': "abc", '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', 
+                    '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        def backtrack(index, string):
+            if index == len(digits):
+                ans.append("".join(string[:]))
                 return
+            
+            digit = num_char[digits[index]]
+            for c in digit:
+                string.append(c)
+                backtrack(index + 1, string)
+                string.pop()
 
-            
-            mapping = hash_map[digits[i]]
-            for char in mapping:
-                arr.append(char)
-                backtrack(arr, i+1)
-                arr.pop()
-            
-        
-        
         ans = []
-        backtrack([], 0)
+        backtrack(0, [])
         return ans
