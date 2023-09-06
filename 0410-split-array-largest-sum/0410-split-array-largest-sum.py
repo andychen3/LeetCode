951119fa77e2dfbcf82b88b62f22366a9b2c5460
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-06 07:52:11 -0400

    Time: 57 ms (28.5%), Space: 16.5 MB (51.94%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -10,7 +10,19 @@ class Solution:
                 else:
                     splits += 1
                     curr_sum = num
+            # Splits have to add 1 because if in the instance the curr_sum doesn't exactly match
+            # max_sum and that is the last element. You have to account for the last element which adds to
+            # the split
             return splits+1 <= k
+        
+        # The left bound is not 1 because 
+        # We need the max nums because if worse case we have to split the array and it has to be the total
+        # max number to be able to split it that is the lowest possible number to be able to split
+        # This example is portrayed when arr = [1,4,4] and k = 3. 1 Would not work because if it was 1 you
+        # would be able to split the first element but then you can't split the other 2. So the min number
+        # to allow the splitting to work is the maximum element of the array
+
+        # We also used this template because we are looking for the min largest sum.
         left = max(nums)
         right = sum(nums)
         while left <= right:
@@ -19,7 +31,5 @@ class Solution:
                 right = mid - 1
             else:
                 left = mid + 1
-        
-        print(right)
-        print(mid)
+
         return left
