d95bf8b7c57a32ec3a6d9ba2d304693fdfd348d8
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-07 10:36:03 -0400

    Time: 42 ms (52.46%), Space: 16.2 MB (93.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,19 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # Check if only one element then return that one house
+        # To avoid out of bounds error from setting base case
         if len(nums) == 1:
             return nums[0]
-
-        n = len(nums)
-        # We only multiply n instead of n + 1 because the first index you always rob
-        # if one house.
-        # it's different from fibonacci because the 0th index you have to have it as 0.
-        # while here it is not. You can rewrite it to have 0 as the index if you want.
-        arr = [0] * n
-        arr[0] = nums[0]
-        arr[1] = max(nums[0], nums[1])
-
-        for i in range(2, n):
-            arr[i] = max(arr[i - 2] + nums[i], arr[i-1])
         
-        return arr[n-1]
+        n = len(nums)
+
+        # Base cases
+        back_two = nums[0]
+        back_one = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            # back_two becomes back_one, and back_one gets updated
+            # We save back_one to temp because we want this value to be the new back_two value
+            # We perform the recurrence relation on back_one because back_one is the value that is movign 
+            # forward. 
+            # That is why we return back_one because at the end of the for loop it holds the answer at the 
+            # last index.
+            temp = back_one
+            back_one = max(back_one, back_two + nums[i])
+            back_two = temp
+
+            # back_one, back_two = max(back_one, back_two + nums[i]), back_one
+
+        return back_one
