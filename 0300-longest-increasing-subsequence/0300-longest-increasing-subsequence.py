3847ae49aa23a73324be7e38798d1f9153c5f639
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-07 19:48:16 -0400

    Time: 2428 ms (54.01%), Space: 16.8 MB (9.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,20 +1,16 @@
-from functools import cache
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        @cache
-        def dp(i):
-            # We have ans = 1 because all numbers have a length of 1 that can be added
-            # to the subsequence
-            ans = 1
+        n = len(nums)
+        # We set all the values to 1 because all numbers have a length of 1
+        dp = [1] * n
 
-            # We use j as the pointer to go back from [0, i) and compare to the value of nums[i]
-            # We only try to find the max length if nums[j] is less than nums of i
+        for i in range(n):
+            # We iterate to the ith number because we are using j to iterate from the start to i.
+            # This allows us to check if there are any numbers before i that are less than i
             for j in range(i):
                 if nums[i] > nums[j]:
-                    # The recurrence relationship is either the current ans is the longest subsequence or
-                    # we add 1 to the longest subsequence we have found thus far before i
-                    ans = max(ans, dp(j) + 1)
-            return ans
-
-        # We find the max subsequence from the recucrsion call from iterating from i to n
-        return max(dp(i) for i in range(len(nums)))
+                    # We start building the answer by finding the max of the current ith number
+                    # or we add the current ith number which is 1 to the subsequence we were able to build
+                    # before i
+                    dp[i] = max(dp[j] + 1, dp[i])
+        return max(dp)
