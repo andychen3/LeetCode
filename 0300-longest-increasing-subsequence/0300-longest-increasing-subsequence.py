1d1a24ed7d39aead88c321e7dc36c77f7913629d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-07 19:44:48 -0400

    Time: 2528 ms (33.47%), Space: 18.4 MB (6.26%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,11 +1,20 @@
+from functools import cache
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
+        @cache
+        def dp(i):
+            # We have ans = 1 because all numbers have a length of 1 that can be added
+            # to the subsequence
+            ans = 1
 
-        for i in range(n):
+            # We use j as the pointer to go back from [0, i) and compare to the value of nums[i]
+            # We only try to find the max length if nums[j] is less than nums of i
             for j in range(i):
                 if nums[i] > nums[j]:
-                    dp[i] = max(dp[j] + 1, dp[i])
-        return max(dp)
-        
+                    # The recurrence relationship is either the current ans is the longest subsequence or
+                    # we add 1 to the longest subsequence we have found thus far before i
+                    ans = max(ans, dp(j) + 1)
+            return ans
+
+        # We find the max subsequence from the recucrsion call from iterating from i to n
+        return max(dp(i) for i in range(len(nums)))
