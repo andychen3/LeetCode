65d39c80b447e3f8e68eeeffefee6ad37f3a25a9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-07 06:01:37 -0400

    Time: 52 ms (68.98%), Space: 16.4 MB (22.1%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,33 +1,40 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        board = [["."]*n for _ in range(n)]
-        col_set = set()
-        diag_set = set()
-        anti_diag_set = set()
-        res = 0
-        
-        def backtrack(row):
-            nonlocal res
+        # The trick is you can make sure a queen is not attacked by checking if
+        # They are in the same row, col, diagonal, or anti diagonal
+        # A way to check diagonal is row - col and to check anti diagonal is row + col
+        # Backtrack because of time complexity
+
+        def backtrack(row, column, diagonal, anti_diagonal):
             if row == n:
-                res += 1
-                return
-            
+                return 1
+
+            ans = 0
             for col in range(n):
-                if (col in col_set or (row-col) in diag_set
-                   or (row + col) in anti_diag_set):
+                curr_diagonal = row - col
+                curr_anti_diagonal = row + col
+
+                # Check if they are in the same row, col, diagonal, or anti_diagonal
+                if (col in column or curr_diagonal in diagonal or 
+                    curr_anti_diagonal in anti_diagonal):
                     continue
                 
-                col_set.add(col)
-                diag_set.add(row-col)
-                anti_diag_set.add(row+col)
-                board[row][col] = 'Q'
-                
-                backtrack(row+1)
-                
-                col_set.remove(col)
-                diag_set.remove(row-col)
-                anti_diag_set.remove(row+col)
-                board[row][col] = '.'
-                
-        backtrack(0)
-        return res
+                # Add them all to set so easy O(1) check to see if next queen placement
+                # is valid
+                column.add(col)
+                diagonal.add(curr_diagonal)
+                anti_diagonal.add(curr_anti_diagonal)
+
+                # Call backtrack and add to the answer
+                ans += backtrack(row + 1, column, diagonal, anti_diagonal)
+
+                # Remove state when backtrack comes back to check other possibilities
+                column.remove(col)
+                diagonal.remove(curr_diagonal)
+                anti_diagonal.remove(curr_anti_diagonal)
+            
+            return ans
+
+        return backtrack(0, set(), set(), set())
+
+
