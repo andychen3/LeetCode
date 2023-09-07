e1739086f18518fe98571270ef2656eab14e1463
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-07 10:01:47 -0400

    Time: 43 ms (44.08%), Space: 16.4 MB (35.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,14 +3,15 @@ class Solution:
         def dp(i):
             if i == 0:
                 return nums[0]
+            
             if i == 1:
-                return max(nums[i], nums[i-1])
+                return max(nums[0], nums[1])
             
             if i in memo:
                 return memo[i]
             
-            memo[i] = max(dp(i-1), dp(i-2) + nums[i])
-            return memo[i]    
-            
+            memo[i] = max(dp(i-2) + nums[i], dp(i-1))
+            return memo[i]
+
         memo = {}
         return dp(len(nums)-1)
