3985044013b3dbe022526d19d82330038860967c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 07:11:29 -0400

    Time: 44 ms (30.54%), Space: 16 MB (99.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,15 @@
-from functools import cache
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @cache
-        def dp(i):
-            if i == 0:
-                return 0
-            
-            if i == 1:
-                return 1
-            
-            if i == 2:
-                return 2
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
 
-            return dp(i - 1) + dp(i - 2)
-        return dp(n)
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[-1]
+
+
