973a6996b4e40d3bd82ba0cfb1f8fd69193ef8e7
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 06:44:51 -0400

    Time: 1398 ms (74.98%), Space: 62.6 MB (62.43%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,19 +1,22 @@
-from functools import cache
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        @cache
-        def dp(i):
-            # This is the base case because if the length is greater than the array then there is
-            # no more questions to solve
-            if i >= len(questions):
-                return 0
+        # # Reason why you do n + 1 instead of n because at the first choice
+        # # if you skip you have 0. You don't automatically take the question
+        # # because it affects future decisions. 
+        # # This will prevent out of bounds
+        n = len(questions)
+        dp = [0] * n
+        dp[-1] = questions[-1][0]
 
-            # How to skip a question?
+        for i in range(n - 2, -1, -1): # n - 1 To stay in bounds
+            # how to skip a question?
+            
             j = i + questions[i][1] + 1
+            if j < n:
+                dp[i] += dp[j]
+            # To take or not take? And you want to maximize it
+            # we do min because j could be out of bounds.
+            dp[i] = max(dp[i + 1], questions[i][0] + dp[i])
 
-            # Why is it dp of j? It is dp of j because j represents the next question you can solve
-            # after you skip brainpower. Also, if you solve a question you get x points
-            return max(questions[i][0] + dp(j), dp(i + 1))
-        
-        # Why do you iterate from 0? Because you are starting from the beginning.
-        return dp(0)
+        print(dp)
+        return dp[0]
