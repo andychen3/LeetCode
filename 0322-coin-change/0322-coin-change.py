9205582922b4ee1a3363d2834e4c303c945e3e6d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 09:06:29 -0400

    Time: 1002 ms (59.96%), Space: 36.8 MB (16.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,12 +3,18 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         @cache
         def dp(curr):
+            # We set curr to == 0 because it takes 0 coins to make a total of 0
             if curr == 0:
                 return 0
 
+            # we set min coins to create amount to a huge number
             min_coins = float("inf")
             for c in coins:
+                # We check if this can be a valid solution if we take a coin
                 if curr - c >= 0:
+                    # If so we find the minimum amount of coins it takes to create the amount
+                    # we add 1 to dp because everytime you take a coin you add 1 to the min amount
+                    # of coins.
                     min_coins = min(min_coins, 1 + dp(curr - c))
             return min_coins
         
