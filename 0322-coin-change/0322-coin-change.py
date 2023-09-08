dffd52386a4dfba487867f69bf3711c1f689fd13
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 08:49:38 -0400

    Time: 903 ms (84.78%), Space: 38.6 MB (13.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,17 +4,29 @@ class Solution:
         
         @cache
         def dp(amount):
+            # We return float("inf") because if an amount is less than 0
+            # that means the amount is invalid so we return a huge number
+            # so when doing dp it will allow finding hte min answer to work
             if amount < 0:
                 return float("inf")
+
+            # amount == 0 return 0 because when the amount is 0 we don't need any coins
+            # to form 0
             if amount == 0:
                 return 0
             
+            # We set the starting number of coints to an arbitrary large number
             min_coins = float('inf')
             for coin in coins:
+                # This recurrence relationship tries to solve the minimum number of coins to
+                # create the amount.
+                # we add 1 + dp(amount-coin) because when a total can be made it takes 1 coin.
                 min_coins = min(min_coins, 1 + dp(amount - coin))
             
             return min_coins 
         
+        # We do this part because the answer can be float(inf) that is returned which means
+        # there is no answer thus it should be changed to -1
         result = dp(amount)
         return result if result != float("inf") else -1
 
