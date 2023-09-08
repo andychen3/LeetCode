ab4ce829915301a990ec96bc8fd0a6291fccedde
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 08:46:52 -0400

    Time: 920 ms (82.48%), Space: 38.7 MB (13.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,21 @@
+from functools import cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def dp(remaining):
-            if remaining == 0:
-                return 0
-            ans = float("inf")
-            for c in coins:
-                if remaining - c >= 0:
-                    ans = min(ans, dp(remaining - c)+1)
-                    
-            return ans
         
-        res = dp(amount) 
-        return res if res != float("inf") else -1
+        @cache
+        def dp(amount):
+            if amount < 0:
+                return float("inf")
+            if amount == 0:
+                return 0
             
+            min_coins = float('inf')
+            for coin in coins:
+                min_coins = min(min_coins, 1 + dp(amount - coin))
             
+            return min_coins 
+        
+        result = dp(amount)
+        return result if result != float("inf") else -1
+
+
