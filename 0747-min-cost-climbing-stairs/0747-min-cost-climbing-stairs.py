1123bfe772478929bae43864ea627e987eb5f07a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 07:51:12 -0400

    Time: 64 ms (54.43%), Space: 16.5 MB (32.83%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
         dp = [0] * (n + 1) # n + 1 because the answer is built from previous two.
-        dp[2] = cost[0]
+        # dp[2] = cost[0] we do not need this because the recurrence relation will build it
 
         for i in range(2, n + 1):
             dp[i] = min(dp[i - 1] + cost[i-1], dp[i-2] + cost[i-2])
