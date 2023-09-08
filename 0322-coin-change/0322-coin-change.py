7169309a1f5e620fea913a7343307da9d9cce28d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 12:48:02 -0400

    Time: 847 ms (89.38%), Space: 16.6 MB (75.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,6 +3,9 @@ class Solution:
         dp = [float("inf")] * (amount + 1)
         dp[0] = 0 # 0 coins to make 0 value
 
+        # We should start from iterating through the coins list because we can only use those coins
+        # instead of starting from iterating through the range of 1 to amount + 1 inclusive.
+        # This saves unnecessary iterations.
         for coin in coins:
             for i in range(coin, amount + 1):
                 dp[i] = min(dp[i], dp[i-coin] + 1)
