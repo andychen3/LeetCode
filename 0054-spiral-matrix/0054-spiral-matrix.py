52e9bd3902b5b98cc8d750d6ffe02b28d38faf61
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 15:53:46 -0400

    Time: 32 ms (92.88%), Space: 16.3 MB (77.09%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,34 +1,21 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top, bottom, left, right = 0, len(matrix), 0, len(matrix[0])
-        res = []
+        rows, cols = len(matrix), len(matrix[0])  # Get the number of rows and columns
+        direction = 1  # 1 for right, -1 for left (initially start moving right)
+        row, col = 0, -1  # Initialize starting position (0-based index)
+        result = []  # List to store the elements in spiral order
         
-        while left < right and top < bottom:
-            # going left to right
-            for i in range(left, right):
-                res.append(matrix[top][i])
-            top += 1
+        while rows * cols > 0:  # Continue until there are elements to process
+            for _ in range(cols):  # Move horizontally
+                col += direction
+                result.append(matrix[row][col])
+            rows -= 1  # Reduce the number of rows to process
             
-            # top to bottom
+            for _ in range(rows):  # Move vertically
+                row += direction
+                result.append(matrix[row][col])
+            cols -= 1  # Reduce the number of columns to process
             
-            for i in range(top, bottom):
-                res.append(matrix[i][right-1])
-                
-            right -= 1
-            
-            if not(left < right and top < bottom):
-                break
-                
-            #right to left
-            
-            for i in range(right - 1, left - 1, -1):
-                res.append(matrix[bottom-1][i])
-            bottom -= 1
-            
-            # bottom to top
-            for i in range(bottom-1, top-1, -1):
-                res.append(matrix[i][left])
-            
-            left += 1
-        return res
-                
+            direction *= -1  # Flip the direction for the next iteration
+        
+        return result
