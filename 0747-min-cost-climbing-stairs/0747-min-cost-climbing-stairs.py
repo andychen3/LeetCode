3740876822370e854630fcfb19019f4a8c2945a6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 07:38:12 -0400

    Time: 62 ms (65.01%), Space: 18.9 MB (5.8%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,9 +1,15 @@
+from functools import cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        arr = [0] * (n + 1)
+        @cache
+        def dp(i):
+            if i == 0:
+                return 0
+            if i == 1:
+                return 0
 
-        for i in range(2, n + 1):
-            arr[i] = min(arr[i-1] + cost[i-1], arr[i-2] + cost[i-2])
-        
-        return arr[n]
+            # Recurrence relationship
+            # Min of taking one step and two steps.
+            # When you take either step you incur the cost of that step
+            return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
+        return dp(len(cost))
