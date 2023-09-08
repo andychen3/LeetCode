9866e679dc32cfea2da9fd74d4c2fa12150f62a6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 09:04:57 -0400

    Time: 1006 ms (59.08%), Space: 36.7 MB (16.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,28 +1,16 @@
 from functools import cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
         @cache
-        def dp(amount):
-            # amount == 0 return 0 because when the amount is 0 we don't need any coins
-            # to form 0
-            if amount == 0:
+        def dp(curr):
+            if curr == 0:
                 return 0
-            
-            # We set the starting number of coints to an arbitrary large number
-            min_coins = float('inf')
-            for coin in coins:
-                # This recurrence relationship tries to solve the minimum number of coins to
-                # create the amount.
-                # we add 1 + dp(amount-coin) because when a total can be made it takes 1 coin.
-                if amount - coin >= 0:
-                    min_coins = min(min_coins, 1 + dp(amount - coin))
-            
-            return min_coins 
+
+            min_coins = float("inf")
+            for c in coins:
+                if curr - c >= 0:
+                    min_coins = min(min_coins, 1 + dp(curr - c))
+            return min_coins
         
-        # We do this part because the answer can be float(inf) that is returned which means
-        # there is no answer thus it should be changed to -1
         result = dp(amount)
         return result if result != float("inf") else -1
-
-
