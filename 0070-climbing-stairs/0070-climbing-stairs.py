190b9612ff6259d6d7657b0fbad280377833a2d9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 07:31:18 -0400

    Time: 40 ms (54.68%), Space: 16.3 MB (65.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,15 @@
+from functools import cache
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # This covers base cases that if n = 1 you take 1 step and if n =2 you take 2 steps total
-        if n <= 2:
-            return n
-
-        dp = [0] * (n + 1) # We do n + 1 because we want the answer at the end
-        dp[0] = 0 # On the floor
-        dp[1] = 1 # if there is only one step
-        dp[2] = 2 # If there are two steps. You can do 1 + 1 or 2 which totals to 2
-
-        for i in range(3, n + 1): # Start from 3 because we defined our base cases
-            # We are adding the total number of ways using 1 step and 2 step
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[n]
+        @cache
+        def dp(i):
+            if i == 0:
+                return 0
+            if i == 1:
+                return 1
+            if i == 2:
+                return 2
+            
+            return dp(i-1) + dp(i-2)
+        
+        return dp(n)
