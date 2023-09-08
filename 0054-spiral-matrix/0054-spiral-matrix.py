118b8d8a0a795ec4e01bbf705bde250ebd2ac29f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-08 16:55:35 -0400

    Time: 41 ms (52.1%), Space: 16.4 MB (45.83%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,21 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        rows, cols = len(matrix), len(matrix[0])  # Get the number of rows and columns
-        direction = 1  # 1 for right, -1 for left (initially start moving right)
-        row, col = 0, -1  # Initialize starting position (0-based index)
-        result = []  # List to store the elements in spiral order
-        
-        while rows * cols > 0:  # Continue until there are elements to process
-            for _ in range(cols):  # Move horizontally
-                col += direction
-                result.append(matrix[row][col])
-            rows -= 1  # Reduce the number of rows to process
+        # rows is for up and down or in short vertical
+        # cols is for left and right or in short horizontal 
+
+        rows, cols = len(matrix), len(matrix[0])
+        directions = 1 # This is the value that we are going to use to iterate our col and row
+        row, col = 0, -1 # we initialize 0 because we are starting horizontally first and that is why -1
+        ans = []
+
+        while rows * cols > 0:
+            # Iterate horizontally
+            for _ in range(cols):
+                col += directions # we add to direction because directions is the iterator
+                ans.append(matrix[row][col])
             
-            for _ in range(rows):  # Move vertically
-                row += direction
-                result.append(matrix[row][col])
-            cols -= 1  # Reduce the number of columns to process
-            
-            direction *= -1  # Flip the direction for the next iteration
+            # We do this and not cols first because after we move horizontally we 
+            # have to go down. and to control how many times you can go down you take from rows
+            rows -= 1 
+
+            # Iterate vertically
+            for _ in range(rows):
+                row += directions
+                ans.append(matrix[row][col])
+            cols -= 1
+
+            # Change direction
+            directions *= -1
         
-        return result
+        return ans
+            
