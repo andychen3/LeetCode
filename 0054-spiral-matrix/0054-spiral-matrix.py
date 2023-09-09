015553505965c53b6073d4a1b3b9f0ae25920fdf
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 05:36:49 -0400

    Time: 34 ms (86.84%), Space: 16.4 MB (45.83%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,31 +1,21 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # rows is for up and down or in short vertical
-        # cols is for left and right or in short horizontal 
-
         rows, cols = len(matrix), len(matrix[0])
-        directions = 1 # This is the value that we are going to use to iterate our col and row
-        row, col = 0, -1 # we initialize 0 because we are starting horizontally first and that is why -1
+        directions = 1
+        row, col = 0, -1
         ans = []
 
         while rows * cols > 0:
-            # Iterate horizontally
             for _ in range(cols):
-                col += directions # we add to direction because directions is the iterator
+                col += directions
                 ans.append(matrix[row][col])
             
-            # We do this and not cols first because after we move horizontally we 
-            # have to go down. and to control how many times you can go down you take from rows
-            rows -= 1 
+            rows -= 1
 
-            # Iterate vertically
             for _ in range(rows):
                 row += directions
                 ans.append(matrix[row][col])
             cols -= 1
 
-            # Change direction
             directions *= -1
-        
         return ans
-            
