b1fc33e235a0f1f85123b5f04eec3ef12ed9447c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 09:02:43 -0400

    Time: 71 ms (79.7%), Space: 17.3 MB (36.15%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.py
+++ 0898-transpose-matrix/0898-transpose-matrix.py
@@ -1,13 +1,15 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        ans = []
-
-        for c in range(cols):
-            levels = []
-            for r in range(rows):
-                levels.append(matrix[r][c])
-            ans.append(levels)
-        return ans
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        # Create a new matrix with dimensions switched
+        transposed = [[0 for _ in range(m)] for _ in range(n)]
+        
+        for i in range(m):
+            for j in range(n):
+                # Switch row and column indices
+                transposed[j][i] = matrix[i][j]
+        
+        return transposed
         
