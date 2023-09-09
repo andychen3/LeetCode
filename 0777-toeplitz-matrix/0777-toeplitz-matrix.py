d5de33497fb07400b3fadbc8a08b1138ebc1488c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 07:31:04 -0400

    Time: 96 ms (21.25%), Space: 16.3 MB (85.91%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -1,16 +1,10 @@
-from collections import defaultdict
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        diagonals = defaultdict(list)
-
         rows = len(matrix)
         cols = len(matrix[0])
 
-        for r in range(rows):
-            for c in range(cols):
-                diagonals[r - c].append(matrix[r][c])
-
-        for _, values in diagonals.items():
-            if not all(x == values[0] for x in values):
-                return False
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][c] != matrix[r-1][c-1]:
+                    return False
         return True
