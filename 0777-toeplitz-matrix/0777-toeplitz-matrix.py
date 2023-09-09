42828d5a4301e8bb4d885f4466a5dca15b41fd86
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 07:32:14 -0400

    Time: 91 ms (39.69%), Space: 16.4 MB (58.03%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -3,6 +3,11 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
+        # We can do either way to start from zero or start from 1. If we start from 0
+        # we have to subtract 1 from rows and cols because in the if we would be adding 1 to row and col
+
+        # This way is more intuitive because we want to compare every diagonal and since we start at 0,0
+        # We are checking the next row and column's diagonal to see if it matches.
         for r in range(1, rows):
             for c in range(1, cols):
                 if matrix[r][c] != matrix[r-1][c-1]:
