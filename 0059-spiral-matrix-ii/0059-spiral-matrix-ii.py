39e02c035e3838771930030905c3206396a9bb1c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 05:56:43 -0400

    Time: 45 ms (32.67%), Space: 16.2 MB (97.31%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,32 +1,26 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix = [[0 for _ in range(n)] for _ in range(n)]
-        top = 0
-        bottom = n
-        right = n
-        left = 0
+        rows = cols = n
+        directions = 1
+        row, col = 0, -1
+        ans = [[0 for _ in range(n)] for _ in range(n)]
         nums = 1
-        
-        while left < right or top < bottom:
-            # Move left to right
-            for i in range(left, right):
-                matrix[top][i] = nums
+
+        while rows * cols > 0:
+            for _ in range(cols):
+                col += directions
+                ans[row][col] = nums
                 nums += 1
-            top += 1
-            # top to bottom
-            for i in range(top, bottom):
-                matrix[i][right-1] = nums
-                nums += 1
-            right -= 1
-            # Right to left
             
-            for i in range(right - 1, left - 1, -1):
-                matrix[bottom - 1][i] = nums
+            rows -= 1
+            
+            for _ in range(rows):
+                row += directions
+                ans[row][col] = nums
                 nums += 1
-            bottom -= 1
-            # bottom to top
-            for i in range(bottom - 1, top - 1, -1):
-                matrix[i][left] = nums
-                nums += 1
-            left += 1
-        return matrix
+            
+            cols -= 1
+            
+            directions *= -1
+        return ans
+
