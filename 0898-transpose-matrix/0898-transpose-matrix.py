56aca06a5814b94dcf00fbf2f82c238fabb7e947
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 09:06:59 -0400

    Time: 72 ms (75.03%), Space: 17 MB (93.58%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.py
+++ 0898-transpose-matrix/0898-transpose-matrix.py
@@ -1,15 +1,20 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        m = len(matrix)
-        n = len(matrix[0])
+        row = len(matrix)
+        col = len(matrix[0])
+        ans = []
+        # This way is faster with building an array within the for loops because its O(1) declaring an array
+        # O(1) appending to array and appending to ans
+        # The other way is to create a 2D matrix that is transposed and fill it in. But that is slower
+
+        # We iterate with column first because when we transpose a matrix we switch rows with column
+        for c in range(col):
+            level = []
+            for r in range(row):
+                # This makes sense because when we access the r,c in the original matrix it is still
+                # using its original structure.
+                level.append(matrix[r][c])
+            ans.append(level)
+        return ans
         
-        # Create a new matrix with dimensions switched
-        transposed = [[0 for _ in range(m)] for _ in range(n)]
-        
-        for i in range(m):
-            for j in range(n):
-                # Switch row and column indices
-                transposed[j][i] = matrix[i][j]
-        
-        return transposed
-        
+        # Another answer is to do return [ans for ans in list(zip(*matrix))]
