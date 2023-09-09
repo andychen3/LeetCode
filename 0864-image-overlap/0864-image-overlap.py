87dbb2bd1f2f4fbf4395c228842bd35f70b0d4d8
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-09 08:35:57 -0400

    Time: 432 ms (61.22%), Space: 17 MB (20.91%) - LeetHub

---
+++ 0864-image-overlap/0864-image-overlap.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        arr1 = []
+        arr2 = []
+        ans = 0
+        N = len(img1)
+
+        # get the coordinates for each img
+        for r in range(N):
+            for c in range(N):
+                if img1[r][c]:
+                    arr1.append((r,c))
+                if img2[r][c]:
+                    arr2.append((r,c))
+        
+        overlaps = defaultdict(int)
+        for x1, y1 in arr1:
+            for x2, y2 in arr2:
+                # Transformation vector
+                trans_vector = (x2 - x1, y2-y1)
+                overlaps[trans_vector] += 1
+                ans = max(ans, overlaps[trans_vector])
+        return ans
