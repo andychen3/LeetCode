41604ed6ddb52bb12a8e52ae11932d61711f6c6b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-10 07:10:08 -0400

    Time: 809 ms (59.15%), Space: 23.5 MB (98.52%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
@@ -1,10 +1,16 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        # Check if you can even construct the 2d array. The only time it doesnt work
+        # is if mxn != length of original array.
         if len(original) != m*n:
             return []
 
+        # Build the 2D array based on given rows and columns
         ans = [[0]*n for _ in range(m)]
 
+        # Iterate through the length of the original array
         for i in range(len(original)):
+            # i // n will always give you the correct row from the original array
+            # i % n will always give you the correct column.
             ans[i // n][i % n] = original[i]
         return ans
