8aef28ab924c13cb71db4f8a8e0e1bd464797b4b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-10 05:34:45 -0400

    Time: 87 ms (92.86%), Space: 17.6 MB (48.81%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.py
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.py
@@ -1,19 +1,21 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        r, c = rows, cols
-        ans = [[rStart, cStart]]
-        directions = 1
-        steps = 1
+        ans = [[rStart, cStart]] # We add this in because we include our starting position
+        directions = 1 # Direction starts at one because we start horizontally left to right
+        # we have steps because we go out of bounds. so the pattern is 1, 2,2, 3,3,3, ...etc
+        # That is why we don't use row and cols for the for loops compared to the other spiral matrix
+        # problems
+        steps = 1 
 
         while len(ans) < rows * cols:
             for _ in range(steps):
                 cStart += directions
-                if 0 <= cStart < c and 0 <= rStart < r:
+                if 0 <= cStart < cols and 0 <= rStart < rows:
                     ans.append([rStart, cStart])
 
             for _ in range(steps):
                 rStart += directions
-                if 0 <= cStart < c and 0 <= rStart < r:
+                if 0 <= cStart < cols and 0 <= rStart < rows:
                     ans.append([rStart, cStart])
             steps += 1
             directions *= -1
