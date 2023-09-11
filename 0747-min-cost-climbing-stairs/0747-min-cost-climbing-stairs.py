4029cf9dca00d2140e3b0763d288b1dafed9d328
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-11 07:25:44 -0400

    Time: 57 ms (84.33%), Space: 18.7 MB (9.37%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,10 +1,11 @@
+from functools import cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = [0] * (n + 1) # n + 1 because the answer is built from previous two.
-        # dp[2] = cost[0] we do not need this because the recurrence relation will build it
-
-        for i in range(2, n + 1):
-            dp[i] = min(dp[i - 1] + cost[i-1], dp[i-2] + cost[i-2])
-        print(dp)
-        return dp[n]
+        @cache
+        def dp(i):
+            if i == 0 or i == 1:
+                return 0
+            
+            return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
+        
+        return dp(len(cost))
