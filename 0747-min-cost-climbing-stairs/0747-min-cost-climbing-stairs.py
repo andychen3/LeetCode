75f346376dc5cefa5138373f8d9e1f952a0b035e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-11 07:35:07 -0400

    Time: 65 ms (47.18%), Space: 16.5 MB (64.64%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,11 +1,8 @@
-from functools import cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @cache
-        def dp(i):
-            if i == 0 or i == 1:
-                return 0
-            
-            return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
-        
-        return dp(len(cost))
+        n = len(cost)
+        dp = [0] * (n + 1) # Reason is because we start at 0 and 1 index and they are zero
+
+        for i in range(2, n + 1): # We start from 2 because we can start at 0 and 1 and its 0
+            dp[i] = min(dp[i - 1] + cost[i-1], dp[i-2] + cost[i-2])
+        return dp[n]
