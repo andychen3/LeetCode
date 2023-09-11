3fbf83d7c0e7ef350a4ba011f0684c47792770c3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-11 07:14:01 -0400

    Time: 31 ms (93.15%), Space: 16.2 MB (90.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,10 @@
-from functools import cache
 class Solution:
     def climbStairs(self, n: int) -> int:
         # this is a dp problem only because i have seen it. and it is asking how many distinct ways
         # So there are overlapping subproblems
-        @cache
-        def dp(i):
-            if i == 0:
-                return 1
+        dp = [1] * (n + 1)
 
-            if i == 1:
-                return 1
-            
-            return dp(i-1) + dp(i-2)
-        return dp(n)
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
