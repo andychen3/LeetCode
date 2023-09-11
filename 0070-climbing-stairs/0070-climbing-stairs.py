bcaddf8815cf4a6fbc9462bf9568a46da278db87
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-11 07:10:02 -0400

    Time: 38 ms (61.75%), Space: 16.4 MB (26.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,15 @@
 from functools import cache
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # this is a dp problem only because i have seen it. and it is asking how many distinct ways
+        # So there are overlapping subproblems
         @cache
         def dp(i):
-            # The previous solutions are wrong with i == 0 == 0.
-            # i == 0 = 1 because we have to account for when n == 2. So if 
-            # dp(i-2) represents taking 2 steps. When n == 2. It should equal 1. 
-            # if i == 0 = 0, that wouldn't make sense because if n == 2 and you take 2 steps. it will 
-            # be one distinct way to get there not 0.
-
-            # in order to figure out base cases you start with recurrence relation and then figure out
-            # what is true. The thing that threw me off was starting from the bottom which is 0 step.
-            # but that was something i created. It wasn't said in the problem. It just asked you how many
-            # distinct ways to reach to the top if given n steps and that you are allowed to take 1 or 2
             if i == 0:
                 return 1
-            
+
             if i == 1:
                 return 1
             
-
-            return dp(i - 1) + dp(i - 2)
+            return dp(i-1) + dp(i-2)
         return dp(n)
