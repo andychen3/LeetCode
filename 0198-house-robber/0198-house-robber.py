23b7014f073d6eadb3f438a44f0d93d2c2b8b2f3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 15:50:43 -0400

    Time: 45 ms (29.08%), Space: 16.4 MB (35.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,16 @@
-from functools import cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @cache
-        def dp(i):
-            # Because of 0 indexing. This is if we are at the first house
-            # We rob it if this is the only house.
-            if i == 0:
-                return nums[0]
-            
-            # This is if we have two houses
-            if i == 1:
-                return max(nums[0], nums[1])
-            
-            return max(dp(i-1), dp(i-2) + nums[i])
+        if len(nums) == 1:
+            return nums[0]
+
+        # We do len(nums) and not len(nums)+1 because we don't start with needing a subproblem of zero
+        # At our first decision we can choose to rob the first house or not if there are only 1 house
+        dp = [0] * len(nums)
+        dp[0] = nums[0] # Base cases that are similar to top down. Here 0 represents the first house
+        dp[1] = max(nums[0], nums[1])
+
+        # We start at 2 because our base cases ended at 1.
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         
-        return dp(len(nums) - 1)
+        return dp[-1] # Answer is at the end. We could also do len(nums)-1
