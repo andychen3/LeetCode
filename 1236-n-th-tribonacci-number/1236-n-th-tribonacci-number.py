1118d1725021bed2e1c2c5998b4919868ec73994
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 18:37:50 -0400

    Time: 43 ms (32.02%), Space: 16.2 MB (65.93%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,14 @@
+from functools import cache
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        @cache
+        def dp(i):
+            if i == 0:
+                return 0
+            
+            if i == 1 or i == 2:
+                return 1
+
+            return dp(i-1) + dp(i-2) + dp(i-3)
+        
+        return dp(n)
