a37f128078a7f798d3cc2b905bad3a7bd104738a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 14:15:10 -0400

    Time: 34 ms (82.73%), Space: 16.4 MB (26.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # this is a dp problem only because i have seen it. and it is asking how many distinct ways
-        # So there are overlapping subproblems
-        # The reason i defined everything as 1 instead of 0 is because we are trying to find
-        # the amouint of distinct ways. At every instance whether or not you take 1 or 2 steps that counts
-        # as a way. The recurrence relation is the adding of these two distinct ways. 
-        # By doing it this way I don't have to do dp[0], dp[1], dp[2] to set to 1.
-        dp = [1] * (n + 1)
+        if n == 1:
+            return 1
+            
+        # An array that represents the answer to the problem for a given state
+        dp = [0] * (n + 1)
+        dp[1] = 1 # Base cases
+        dp[2] = 2 # Base cases
+        
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] # Recurrence relation
 
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-
+        
         return dp[n]
