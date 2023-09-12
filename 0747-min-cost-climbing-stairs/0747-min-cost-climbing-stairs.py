114822b04b90de172b96e465134379840ad159c9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 16:07:26 -0400

    Time: 52 ms (95.02%), Space: 16.5 MB (64.35%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,14 +1,9 @@
-from functools import cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @cache
-        def dp(i):
-            # Because we can start at the 0th or 1st index. The initial starting cost is 0
-            if i == 0:
-                return 0
+        n = len(cost)
+        dp = [0] * (n+1) # Because we need to include the top floor.
 
-            if i == 1:
-                return 0
+        for i in range(2, n+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
-            return min(dp(i-1) + cost[i-1], dp(i-2) + cost[i-2])
-        return dp(len(cost))
+        return dp[n]
