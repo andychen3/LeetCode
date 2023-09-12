6da58d4286b72c307b687ad4133f07169fad8a19
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 15:46:41 -0400

    Time: 39 ms (63.18%), Space: 16.5 MB (7.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,26 +1,17 @@
+from functools import cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # To avoid out of bounds error from setting base case
-        if len(nums) == 1:
-            return nums[0]
+        @cache
+        def dp(i):
+            # Because of 0 indexing. This is if we are at the first house
+            # We rob it if this is the only house.
+            if i == 0:
+                return nums[0]
+            
+            # This is if we have two houses
+            if i == 1:
+                return max(nums[0], nums[1])
+            
+            return max(dp(i-1), dp(i-2) + nums[i])
         
-        n = len(nums)
-
-        # Base cases
-        back_two = nums[0]
-        back_one = max(nums[0], nums[1])
-        
-        for i in range(2, n):
-            # back_two becomes back_one, and back_one gets updated
-            # We save back_one to temp because we want this value to be the new back_two value
-            # We perform the recurrence relation on back_one because back_one is the value that is movign 
-            # forward. 
-            # That is why we return back_one because at the end of the for loop it holds the answer at the 
-            # last index.
-            temp = back_one
-            back_one = max(back_one, back_two + nums[i])
-            back_two = temp
-
-            # back_one, back_two = max(back_one, back_two + nums[i]), back_one
-
-        return back_one
+        return dp(len(nums) - 1)
