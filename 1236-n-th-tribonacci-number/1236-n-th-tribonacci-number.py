45b660feb68bb08492a654453a65177a6a71ea31
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 18:41:21 -0400

    Time: 32 ms (90.83%), Space: 16.4 MB (27.31%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,14 +1,16 @@
-from functools import cache
 class Solution:
     def tribonacci(self, n: int) -> int:
-        @cache
-        def dp(i):
-            if i == 0:
-                return 0
-            
-            if i == 1 or i == 2:
-                return 1
-
-            return dp(i-1) + dp(i-2) + dp(i-3)
+        if n == 0:
+            return 0
         
-        return dp(n)
+        if n == 1 or n == 2:
+            return 1
+
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        
+        return dp[n]
