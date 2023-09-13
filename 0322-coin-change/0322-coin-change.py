34632507a99f02985b55d35f3720aac61b0106a5
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-13 08:51:53 -0400

    Time: 961 ms (71.88%), Space: 38.5 MB (13.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,20 @@
 from functools import cache
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # what do i want my function to return? I want it to return the fewest number of coins
-        # What is my state variable. curr to track the amount 
-        # What is my recurrence relation? My recurrence relation is.... 
         @cache
-        def dp(curr):
-            # If the curr sum is invalid. We return a huge coin value
-            # Because this answer is not possible
-            if curr < 0:
+        def dp(remain):
+            if remain < 0:
                 return float("inf")
-            # If we get the curr sum to equal amount that means we found a solution.
-            # We return 0 because when the recursion returns it already adds 1 to the possible solution
-            if curr == 0:
+
+            if remain == 0:
                 return 0
 
-            min_coins = float("inf") # We float inf because we want the fewest num of coins
-            for c in coins:
-                min_coins = min(min_coins, 1 + dp(curr - c))
+            min_coins = float("inf")
             
-            return min_coins if min_coins != float("inf") else float("inf")
+            for coin in coins:
+                # Recurrence relation
+                min_coins = min(min_coins, dp(remain - coin) + 1)
+
+            return min_coins
         
-        result = dp(amount)
-        return result if result != float("inf") else -1
+        return dp(amount) if dp(amount) != float("inf") else -1
