4420dd5e7ff9f0b67fc758de4fc018dc47848207
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-12 20:33:43 -0400

    Time: 871 ms (96.47%), Space: 27.9 MB (81.76%) - LeetHub

--- 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.py
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.py
@@ -1,15 +1,14 @@
-from functools import cache
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-        @cache
-        def dp(i, left):
-            if i == m:
-                return 0
-            
-            mult = multipliers[i]
-            right = (n-1) - (i - left)
-            return max(mult * nums[left] + dp(i+1, left+1), mult * nums[right] + dp(i+1, left))
-
         n = len(nums)
         m = len(multipliers)
-        return dp(0, 0)
+
+        dp = [[0] * (m + 1) for _ in range(m+1)]
+
+        for i in range(m - 1, -1, -1):
+            for left in range(i, -1, -1):
+                mult = multipliers[i]
+                right = (n -1) - (i - left)
+                dp[i][left] = max(mult * nums[left] + dp[i+1][left+1], mult * nums[right] + dp[i+1][left])
+        
+        return dp[0][0]
