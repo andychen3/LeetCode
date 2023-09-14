731b9e13fb68edf913ea8f43aa6f3721eb91389c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 07:25:19 -0400

    Time: 46 ms (16.18%), Space: 16.3 MB (74.76%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -1,22 +1,18 @@
-from functools import cache
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        @cache
-        # Return the number of ways
-        def dp(i):
-            # Base cases
-            # If we have 1 post then there is k ways to paint it
-            if i == 1:
-                return k
+        if n == 1:
+            return k
 
-            # If we have 2 posts then there are k * k ways to paint it
-            if i == 2:
-                return k * k
+        if n == 2:
+            return k * k
 
-            # Recurrence relation is
-            # You have two options.
-            # 1. Paint the post the same color but with restriction on third post
-            # 2. Paint the post a different color
-            return (k-1) * (dp(i-1) + dp(i-2))
+        dp = [0] * (n + 1)
+        # We skip dp[0] because when you have zero posts there are 0 ways to paint it
+        dp[1] = k
+        dp[2] = k * k
 
-        return dp(n)
+        for i in range(3, n + 1):
+            dp[i] = (k-1) * (dp[i-1] + dp[i-2])
+        print(dp)
+        return dp[n]       
+ 
