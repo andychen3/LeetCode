2b7561f6d8086a01b08b59bd9ea9022d7fe349fa
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 06:00:41 -0400

    Time: 146 ms (53.44%), Space: 21.1 MB (44.72%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -7,7 +7,6 @@ class Solution:
             for holding in range(2):
                 for remaining in range(1, k + 1): # We skip 0 because that is a base case 
                     skip = dp[i+1][holding][remaining]
-                    ans = 0
 
                     if holding:
                         ans = prices[i] + dp[i+1][0][remaining - 1]
@@ -17,26 +16,3 @@ class Solution:
                     dp[i][holding][remaining] = max(ans, skip)
         
         return dp[0][0][k]
-
-
-
-
-
-        @cache
-        def dp(i, remaining, holding):
-            if i == len(prices) or remaining == 0:
-                return 0
-
-            # do nothing
-            skip = dp(i+1, remaining, holding)
-            ans = 0
-            #recurrence relation
-            # I can buy, sell, or do nothing
-            if holding: # We have a stock
-                ans = prices[i] + dp(i+1, remaining - 1, 0)
-            else:
-                ans = -prices[i] + dp(i+1, remaining, 1)
-            
-            return max(ans, skip)
-
-        return dp(0, 0, k)
