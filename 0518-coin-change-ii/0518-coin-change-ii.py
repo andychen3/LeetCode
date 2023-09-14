1ff6de5a7dd25a44f75346481af31ee97bf9ddcb
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 08:21:19 -0400

    Time: 380 ms (36.6%), Space: 33.7 MB (36.06%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,23 +1,17 @@
-from functools import cache
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def dp(i, remain):
-            if remain < 0:
-                return 0
+        n = len(coins)
+        dp = [[0] * (n+1) for _ in range(amount + 1)]
 
-            if i == len(coins):
-                return 0
+        for i in range(n+1):
+            dp[0][i] = 1
 
-            if remain == 0:
-                return 1
-            # Reason why you don't need a for loop because i is used to go through
-            # the coins array
-
-            # The recurrence relation is if you take the same coin how many times can you take it
-            # If you skip the coin remain stays the same
-            ways = dp(i, remain - coins[i]) + dp(i+ 1, remain)
-            
-            return ways
-
-        return dp(0, amount)
+        for remain in range(amount+1):
+            for i in range(n -1, -1, -1):
+                if remain - coins[i] >= 0:
+                    dp[remain][i] = dp[remain - coins[i]][i] + dp[remain][i+1]
+                else:
+                    dp[remain][i] = dp[remain][i+1]
+        
+        print(dp)
+        return dp[amount][0]
