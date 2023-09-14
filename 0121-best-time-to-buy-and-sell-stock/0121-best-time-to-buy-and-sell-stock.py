138a896847ac1e3bc0353be15547c3481544f46a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 17:42:35 -0400

    Time: 871 ms (57.66%), Space: 27.2 MB (90.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,6 +3,8 @@ class Solution:
         min_prices = float("inf")
         best = 0
         for num in prices:
+            # You try to find the min_price to sell
             min_prices = min(num, min_prices)
+            # You find the best price from wherever you are selling to the min_price
             best = max(best, num - min_prices)
         return best
