f5bf8e5fc7fd52ed3f5e2fb50f05c126c49e4919
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 19:49:52 -0400

    Time: 43 ms (87.77%), Space: 16.5 MB (25.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,22 +1,23 @@
+from functools import cache
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
         @cache
         def dp(row, col):
-            if obstacleGrid[row][col] == 1:
-                return 0
-            
+            # Base cases
             if row + col == 0:
                 return 1
             
-            ans = 0
+            if obstacleGrid[row][col] == 1:
+                return 0
+
+            ways = 0
+            # Recurrence relation
             if row > 0:
-                ans += dp(row-1, col)
-            
+                ways += dp(row-1, col)
             if col > 0:
-                ans += dp(row, col-1)
-            
-            return ans
-        
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        return dp(m-1,n-1)
+                ways += dp(row, col-1)
+            return ways
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        return dp(m-1, n-1)
