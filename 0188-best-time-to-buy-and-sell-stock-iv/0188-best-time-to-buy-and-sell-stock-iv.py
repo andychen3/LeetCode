19cec39f0e1d17f5415e78d34feafb415c486c6e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 05:57:55 -0400

    Time: 145 ms (53.96%), Space: 21.1 MB (44.72%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,6 +1,27 @@
-from functools import cache
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[[0] * (k+1) for _ in range(2)] for _ in range(n+1)]
+
+        for i in range(n - 1, -1, -1):
+            for holding in range(2):
+                for remaining in range(1, k + 1): # We skip 0 because that is a base case 
+                    skip = dp[i+1][holding][remaining]
+                    ans = 0
+
+                    if holding:
+                        ans = prices[i] + dp[i+1][0][remaining - 1]
+                    else:
+                        ans = -prices[i] + dp[i+1][1][remaining]
+                    
+                    dp[i][holding][remaining] = max(ans, skip)
+        
+        return dp[0][0][k]
+
+
+
+
+
         @cache
         def dp(i, remaining, holding):
             if i == len(prices) or remaining == 0:
@@ -18,4 +39,4 @@ class Solution:
             
             return max(ans, skip)
 
-        return dp(0, k, 0)
+        return dp(0, 0, k)
