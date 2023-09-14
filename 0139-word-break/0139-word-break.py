c28d6af5d511e76c02f5400142da274efcd8360f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-13 20:30:06 -0400

    Time: 28 ms (99.61%), Space: 16.2 MB (96.49%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,20 @@
-from functools import cache
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def dp(i):
-            # We check for less than 0 because if the first word matches
-            # we need to check if the prior word also matches a word in wordDict.
-            if i < 0 :
-                return True
+        # False because we are trying to return true or false
+        dp = [False] * len(s)
 
+        for i in range(len(s)):
             for word in wordDict:
-                # We check if the word can pontetially form from s and dp(i-len(word)) checks for if
-                # previous word was formable
-                if i >= len(word) - 1 and dp(i - len(word)):
-                    # Recurrence relation?
-                    # This builds the string from the end - len of word + 1 because of inclusive and then
-                    # builds the length of the word + 1 inclusive again and check if it matches the word
-                    if s[i - len(word) + 1: i + 1] == word:
-                        return True
-            return False
+                # We first chceck if length of s is greater than a length of a word
+                # WE have the condition to check if i is equal to length of a word in wordDict
+                # This is to check for if the first word is built from 0 to i and there is no previous word
+                # because dp[i-len(word)] would be negative and that is out of bounds
+                # After we have found a word dp[i-len(word)] would check if the previous word was true
+                if i >= len(word) - 1 and (i == len(word) - 1 or dp[i - len(word)]):
+                    if s[i - len(word) + 1 : i + 1] == word:
+                        dp[i] = True
+                        break
+
+        return dp[-1]
+
         
-        return dp(len(s) - 1) # so we can index into the word
