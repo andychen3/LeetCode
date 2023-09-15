865d707f2cd9b8c78d10d206ab5bc4477c9892fc
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-15 16:09:33 -0400

    Time: 135 ms (94.69%), Space: 18 MB (45.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,12 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Find the first zero
         zero_ptr = 0
         for i in range(len(nums)):
             if nums[i] == 0:
                 zero_ptr = i
                 break
-
+        # Start the next iteration from where the zero_ptr is + 1 and switch
+        # if num[zero_ptr] is a zero and nums[i] is not
         for i in range(zero_ptr+1, len(nums)):
             if nums[zero_ptr] == 0 and nums[i] != 0:
                 nums[i], nums[zero_ptr] = nums[zero_ptr], nums[i]
