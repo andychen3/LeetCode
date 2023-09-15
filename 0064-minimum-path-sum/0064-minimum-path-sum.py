953011ee0f4187fa051f386efd570f64d57c22ca
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-14 20:37:02 -0400

    Time: 107 ms (32.35%), Space: 18 MB (78.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,22 +1,14 @@
-from functools import cache
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        @cache
-        def dp(row, col):
-            # We return grid[row][col] because at the end when we reach end we return the number there
-            if row + col == 0:
-                return grid[row][col]
-            
-            ans = float("inf")
-            # we want the min of going down
-            if row > 0:
-                ans = min(ans, dp(row-1, col))
-            # we want min of going right
-            if col > 0:
-                ans = min(ans, dp(row, col -1))
-            # We always add whatever the value to the min answer because we are currently there
-            return grid[row][col] + ans
-            
-
         m, n = len(grid), len(grid[0])
-        return dp(m-1, n-1)
+        dp = [[float('inf')] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for row in range(m):
+            for col in range(n):
+                if row > 0:
+                    dp[row][col] = min(dp[row][col], dp[row-1][col] + grid[row][col])
+                if col > 0:
+                    dp[row][col] = min(dp[row][col], dp[row][col-1] + grid[row][col])
+
+        return dp[-1][-1]
