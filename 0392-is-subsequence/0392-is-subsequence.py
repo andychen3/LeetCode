bcfff179e6c4624f7ac9fb58b52b206b4b66afab
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-15 15:54:03 -0400

    Time: 43 ms (40.91%), Space: 16.3 MB (59.01%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,16 +1,12 @@
-from functools import cache
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        @cache
-        def dp(i, j):
-            if i >= len(s):
-                return True
-            
-            if j >= len(t):
-                return False
-            
-            if s[i] == t[j]:
-                return dp(i+1, j+1)
-            else:
-                return dp(i, j+1)
-        return dp(0,0)
+        left, right = 0,0
+
+        while left < len(s) and right < len(t):
+            if s[left] == t[right]:
+                left += 1
+                right += 1
+            elif s[left] != t[right]:
+                right += 1
+        
+        return True if left == len(s) else False
