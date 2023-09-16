1e66ac76dea3c1bed48595c6290afd2ba0b4d118
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-16 11:13:08 -0400

    Time: 2573 ms (5.1%), Space: 35 MB (36.67%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -1,23 +1,14 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
+        prefix = [nums[0]]
+        for i in range(1, len(nums)):
+            prefix.append(nums[i] + prefix[-1])
+        print(prefix)
+
         ans = [-1] * len(nums)
-        n = len(nums)
-
-        # Created prefix sum
-        prefix = [0] * (n + 1)
-        for i in range(n):
-            prefix[i + 1] = prefix[i] + nums[i]
-        
-        # Iterate through the current nums array
-        for index in range(k, n - k):
-            left_bound = index - k
-            right_bound = index + k
-            subarray_sum = prefix[right_bound + 1] - prefix[left_bound]
-            average = (subarray_sum) // (2 * k + 1)
-            ans[index] = average
-
-        return ans
-            
-            
-        
-        
+        n = (k * 2) + 1
+        curr = 0
+        for i in range(k, len(nums)-k):
+            curr = prefix[i + k] - prefix[i - k] + nums[i - k]
+            ans[i] = curr // n
+        return ans 
