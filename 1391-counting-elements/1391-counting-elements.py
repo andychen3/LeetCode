7e128d254e993ca0d018d8376b287ead1feae288
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-16 18:04:44 -0400

    Time: 42 ms (73.3%), Space: 16.3 MB (93.48%) - LeetHub

--- 1391-counting-elements/1391-counting-elements.py
+++ 1391-counting-elements/1391-counting-elements.py
@@ -1,9 +1,9 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         hash_set = set(arr)
-        ans = 0
-        
+        res = 0
+
         for num in arr:
-            if num+1 in hash_set:
-                ans += 1 
-        return ans
+            if num + 1 in hash_set:
+                res += 1
+        return res
