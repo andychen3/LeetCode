50264cc2778d1bbab6a5fc7b6f9c1543f224204a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-16 11:20:05 -0400

    Time: 2425 ms (5.1%), Space: 35 MB (36.67%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -8,6 +8,8 @@ class Solution:
         ans = [-1] * len(nums)
         n = (k * 2) + 1
         for i in range(k, len(nums)-k):
-            curr = prefix[i + k] - prefix[i - k] + nums[i - k]
+            left_bound = prefix[i-k]
+            right_bound = prefix[i+k]
+            curr = right_bound - left_bound + nums[i - k] # We add back the left most element since it was removed when subtracted from right_bound.
             ans[i] = curr // n
         return ans 
