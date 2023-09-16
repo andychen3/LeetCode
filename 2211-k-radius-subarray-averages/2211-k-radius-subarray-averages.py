5a7225488a46e52f38f73827248efdb408d3b578
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-16 11:21:22 -0400

    Time: 1326 ms (66.51%), Space: 34.1 MB (94.68%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -1,13 +1,23 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        prefix = [nums[0]]
-        for i in range(1, len(nums)):
-            prefix.append(nums[i] + prefix[-1])
-        print(prefix)
-
         ans = [-1] * len(nums)
-        n = (k * 2) + 1
-        for i in range(k, len(nums)-k):
-            curr = prefix[i+k] - prefix[i-k] + nums[i - k] # We add back the left most element since it was removed when subtracted from right_bound.
-            ans[i] = curr // n
-        return ans 
+        n = len(nums)
+
+        # Created prefix sum
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+        
+        # Iterate through the current nums array
+        for index in range(k, n - k):
+            left_bound = index - k
+            right_bound = index + k
+            subarray_sum = prefix[right_bound + 1] - prefix[left_bound]
+            average = (subarray_sum) // (2 * k + 1)
+            ans[index] = average
+
+        return ans
+            
+            
+        
+        
