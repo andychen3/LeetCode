b003eee7e2e8ddec4da0f4242340889d8153846e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-22 06:17:25 -0400

    Time: 41 ms (49.15%), Space: 16.1 MB (99.94%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,26 +1,24 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        rows = cols = n
+        ans = [[0] * n for _ in range(n)]
+        rows, cols = n, n
         directions = 1
-        row, col = 0, -1
-        ans = [[0 for _ in range(n)] for _ in range(n)]
-        nums = 1
+        x, y = 0, -1
+        num = 1
 
         while rows * cols > 0:
-            for _ in range(cols):
-                col += directions
-                ans[row][col] = nums
-                nums += 1
+            for c in range(cols):
+                y += directions
+                ans[x][y] = num
+                num += 1
             
             rows -= 1
-            
-            for _ in range(rows):
-                row += directions
-                ans[row][col] = nums
-                nums += 1
-            
+
+            for r in range(rows):
+                x += directions
+                ans[x][y] = num
+                num += 1
             cols -= 1
-            
+
             directions *= -1
         return ans
-
