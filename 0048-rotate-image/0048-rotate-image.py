848fc8894897c70766b4222185ce39a8a6aac7b4
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-21 21:32:41 -0400

    Time: 37 ms (86.83%), Space: 16.2 MB (79.19%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,14 +3,13 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # Each row I switch the col and row and then after I do that I reverse the rows
+        # transpose the matrix
         n = len(matrix)
-        for r in range(n):
-            # we need r + 1 because if not when it does the second iteration. 
-            # It just reverses it back to normal
-            for c in range(r + 1, n): 
-                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        for row in range(n):
+            for col in range(row + 1, n):
+                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
 
+        # reverse the matrix
         for row in matrix:
             row.reverse()
-        return matrix
+
