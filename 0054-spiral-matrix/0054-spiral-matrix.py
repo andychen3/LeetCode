0198372d6dfded162c91ea04798d0534b73f384f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-22 06:14:00 -0400

    Time: 43 ms (25.94%), Space: 16.2 MB (94.86%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -2,7 +2,9 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         rows, cols = len(matrix), len(matrix[0])
         directions = 1 # it starts at one because we are building horizontally first
-        x, y = 0, -1
+        # we start at 0 for x because we go through the first row first
+        # We start y at -1 to make sure we are at the first index when we start
+        x, y = 0, -1 
         ans = []
 
         while rows * cols > 0:
@@ -10,13 +12,13 @@ class Solution:
                 y += directions
                 ans.append(matrix[x][y])
             
-            rows -= 1
+            rows -= 1 # we decrement row because in a spiral after the first go row goes down 1
 
             for r in range(rows):
                 x += directions
                 ans.append(matrix[x][y])
             
-            cols -= 1
+            cols -= 1 # We decrement col after row because we start going horizontal again
 
             directions *= -1
         return ans
