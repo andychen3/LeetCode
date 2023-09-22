2d10ecb0e355d2b927734c88f9d9a3eb9ed60851
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-22 05:52:11 -0400

    Time: 260 ms (52.34%), Space: 20.4 MB (18.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,14 @@
 from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sum_dict = defaultdict(int)
-        sum_dict[0] = 1
-
-        ans = 0
-        curr = 0
+        counts = defaultdict(int)
+        ans = curr = 0
+        counts[0] = 1
 
         for num in nums:
             curr += num
-            ans += sum_dict[curr - k]
-            sum_dict[curr] += 1
+            # curr - k is current sum - k and i am looking to see if that sum has appeared before 
+            ans += counts[curr - k] 
+            counts[curr] += 1
         
         return ans
