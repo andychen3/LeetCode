ddad5226c167d9a0e6366953a1f7fd9e5b51dbca
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-22 08:04:27 -0400

    Time: 96 ms (11.32%), Space: 16.4 MB (52.4%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -1,15 +1,14 @@
+from collections import defaultdict
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
+        m, n = len(matrix), len(matrix[0])
+        counts = defaultdict(set)
 
-        # We can do either way to start from zero or start from 1. If we start from 0
-        # we have to subtract 1 from rows and cols because in the if we would be adding 1 to row and col
-
-        # This way is more intuitive because we want to compare every diagonal and since we start at 0,0
-        # We are checking the next row and column's diagonal to see if it matches.
-        for r in range(1, rows):
-            for c in range(1, cols):
-                if matrix[r][c] != matrix[r-1][c-1]:
-                    return False
+        for row in range(m):
+            for col in range(n):
+                counts[row-col].add(matrix[row][col])
+        
+        for values in counts.values():
+            if len(values) > 1:
+                return False
         return True
