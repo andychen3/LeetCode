1962c2d3cafd63afe4b1e2e36b03e0f3a2764679
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-27 07:35:46 -0400

    Time: 45 ms (17.98%), Space: 16.4 MB (40.81%) - LeetHub

--- 1619-path-crossing/1619-path-crossing.py
+++ 1619-path-crossing/1619-path-crossing.py
@@ -1,21 +1,17 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        # What did I do wrong?
-        # I forgot that if you move in opposite directions right away you end up in same location.
-        # I didn't think to make it a coord and add it to a set and check if the set had the same coord
-        hash_set = {(0,0)}
-        r,c = (0,0)
-        for direction in path:
-            if direction == "N":
-                r -= 1
-            elif direction == "S":
-                r += 1
-            elif direction == "W":
-                c -= 1
-            elif direction == "E":
-                c += 1
-            if (r,c) in hash_set:
+        seen = {(0,0)}
+        row, col = 0, 0
+        for directions in path:
+            if directions == 'N':
+                row -= 1
+            elif directions == 'S':
+                row += 1
+            elif directions == 'W':
+                col += 1
+            else:
+                col -= 1
+            if (row, col) in seen:
                 return True
-            hash_set.add((r,c))
-
+            seen.add((row,col))
         return False
