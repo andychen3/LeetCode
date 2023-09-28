3d12a58094019f2dc9c0a9518a0f6455d6ea6c5f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-28 14:07:38 -0400

    Time: 58 ms (43.26%), Space: 16.3 MB (93.04%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -2,8 +2,12 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
 
+        # We do n // 2 because any string that is more than half won't match.
         for i in range(1, n // 2 + 1):
+            # We check if it's an even string because if odd it won't work
             if n % i == 0:
+                # We build the substring from the start to the current index and multiply it
+                # by how many times to get the length of s
                 substring = s[:i] * (n // i)
                 if substring == s:
                     return True
