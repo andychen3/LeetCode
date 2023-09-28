5091446ea7cd890da782824d7378d5ba2d765c9d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-28 17:52:16 -0400

    Time: 107 ms (43.76%), Space: 16.5 MB (68.67%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
@@ -4,9 +4,11 @@ class Solution:
         ans = 0
         
         for i in range(N):
+            # Represents the diagonal
             ans += mat[i][i]
-
+            # Represents the anti-diagonal from top right
             ans += mat[i][N-1-i]
+        # IF square is odd you have to subtract out the middle
         if N % 2 != 0:
             ans -= mat[N//2][N//2]
         return ans
