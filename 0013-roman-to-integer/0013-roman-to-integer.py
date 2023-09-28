58954c332bacbc970c7c0528f032d830e4334e5a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-09-28 15:44:42 -0400

    Time: 58 ms (24.05%), Space: 16.1 MB (99.15%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,43 +2,13 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         hash_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
         output = 0
-        special = False
+        i = 0
 
-        for index, char in enumerate(s):
-            if special:
-                special = False
-                continue
-
-            if char == 'I' and index + 1 < len(s):
-                if s[index+1] == 'V':
-                    output += 4
-                elif s[index+1] == 'X':
-                    output += 9
-                else:
-                    output += hash_map[char]
-                    continue
-                special = True
-                continue
-            elif char == 'X' and index + 1 < len(s):
-                if s[index+1] == 'L':
-                    output += 40
-                elif s[index+1] == 'C':
-                    output += 90
-                else:
-                    output += hash_map[char]
-                    continue
-                special = True
-                continue
-            elif char == 'C' and index + 1 < len(s):
-                if s[index+1] == 'D':
-                    output += 400
-                elif s[index+1] == 'M':
-                    output += 900
-                else:
-                    output += hash_map[char]
-                    continue
-                special = True
-                continue
+        while i < len(s):
+            if i + 1 < len(s) and hash_map[s[i]] < hash_map[s[i+1]]:
+                output += hash_map[s[i+1]] - hash_map[s[i]]
+                i += 2
             else:
-                output += hash_map[char]
+                output += hash_map[s[i]]
+                i += 1
         return output
