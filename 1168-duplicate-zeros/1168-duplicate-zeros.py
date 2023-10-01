8366bb24038c316c42f0a7abb528e5b282cc979c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-01 17:01:21 -0400

    Time: 61 ms (92.48%), Space: 17.2 MB (54.77%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -6,11 +6,9 @@ class Solution:
         zeros = arr.count(0)
         n = len(arr)
 
-        for i in range(n-1, -1, -1):
-            # If reg element and within bound moves it to the end
+        for i in range(n - 1, -1, -1):
             if i + zeros < n:
                 arr[i + zeros] = arr[i]
-            # If element is zero duplicate it
             if arr[i] == 0:
                 zeros -= 1
                 if i + zeros < n:
