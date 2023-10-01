f546015fc6f146940a9b1548729f95fab1e78256
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-01 17:04:41 -0400

    Time: 62 ms (89.99%), Space: 17.1 MB (83.29%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -6,7 +6,12 @@ class Solution:
         zeros = arr.count(0)
         n = len(arr)
 
+        # We do i + zeros because that is where the last element should go to make space for the
+        # duplicated zero
+        # if we encounter a zero we duplicate it.
         for i in range(n - 1, -1, -1):
+            # We do this if statement first to make sure we copy the val if its not 0 somewhere else
+            # before we overwrite it with the next if statement.
             if i + zeros < n:
                 arr[i + zeros] = arr[i]
             if arr[i] == 0:
