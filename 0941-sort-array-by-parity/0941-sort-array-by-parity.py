a97cb78b976a3ecd392143a541898d028f336d2c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-01 19:32:21 -0400

    Time: 66 ms (96.57%), Space: 16.9 MB (98.62%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.py
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -1,13 +1,8 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         ptr = 0
-        n = len(nums)
-        for i in range(n):
-            # We are looking for the even number to switch with ptr.
-            # We don't care if ptr is an even or odd number because if its even everytime we switch it
-            # with another even we move ptr up until we find an odd number
+        for i in range(len(nums)):
             if nums[i] % 2 == 0:
                 nums[i], nums[ptr] = nums[ptr], nums[i]
                 ptr += 1
         return nums
-        
