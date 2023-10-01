759176e4bfc25445090539533a216cddae008372
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-01 19:00:37 -0400

    Time: 44 ms (98.98%), Space: 16.3 MB (88.12%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,9 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        hash_map = {num:idx for idx, num in enumerate(arr)}
+        hash_map = {val:key for key, val in enumerate(arr)}
         
         for idx, num in enumerate(arr):
-            if num*2 in hash_map and idx != hash_map[num*2]:
+            if num*2 in arr and idx != hash_map[num*2]:
                 return True
-            
         return False
