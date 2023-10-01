3e6d62141e0ac63a54244baf87031ce4fcf48e24
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-01 17:37:44 -0400

    Time: 87 ms (49.22%), Space: 17.9 MB (43.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,11 +3,7 @@ class Solution:
         insert_index = 1
 
         for i in range(1, len(nums)):
-            # We are using 3 pointers. Insert_index is the place to add the correct num
-            # i as the one to check the next nums and i-1 to compare if the previous is a different num
-            
-            if nums[i] != nums[i-1]:
+            if nums[i-1] != nums[i]:
                 nums[insert_index] = nums[i]
                 insert_index += 1
-
         return insert_index
