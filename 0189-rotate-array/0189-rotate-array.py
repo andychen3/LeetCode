4dcff030c1f4533dc73af79433a616a54277318b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-02 10:24:08 -0400

    Time: 180 ms (69.62%), Space: 27.6 MB (86.57%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,28 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        actual_rotation = k % len(nums)
+        n = len(nums)
+        arr = [0] * n
+
+        for i in range(n):
+            arr[(i + k) % n] = nums[i]
         
-        start = 0
-        break_point = (len(nums)-1)-actual_rotation
-        end = len(nums)-1
-        end_break_point = len(nums)-actual_rotation
-        
-        def reverse(s,e):
-            while s<e:
-                nums[s], nums[e] = nums[e], nums[s]
-                s +=1
-                e -=1
-        
-        # Reverse the beginning to the break_point
-        reverse(start,break_point)
-        
-        # Reverse the end_break_point to the end
-        reverse(end_break_point, end)
-        
-        # Reverse the whole list
-        
-        reverse(start,end)
-        
+        nums[:] = arr
         return nums
-        
