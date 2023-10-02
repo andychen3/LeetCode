9c6c3cd6483ff08e8786ca965f2ce23d7c2fbdf1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-02 18:21:01 -0400

    Time: 34 ms (83.24%), Space: 16.3 MB (58.85%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -2,8 +2,9 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         row = [1]
 
-        for i in range(1, rowIndex+1):
+        for i in range(rowIndex):
             new_row = [1]
+
             for j in range(1, len(row)):
                 new_row.append(row[j] + row[j-1])
             new_row.append(1)
