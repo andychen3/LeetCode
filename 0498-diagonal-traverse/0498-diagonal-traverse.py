69f6e3006714a3cd19085f252ba163f6c64e5189
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-02 17:20:39 -0400

    Time: 167 ms (69.85%), Space: 20.2 MB (35.89%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,18 +1,20 @@
 from collections import defaultdict
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        diag = defaultdict(list)
+        anti_diag = defaultdict(list)
         row, col = len(mat), len(mat[0])
 
         for r in range(row):
             for c in range(col):
-                diag[r+c].append(mat[r][c])
-        
+                anti_diag[r+c].append(mat[r][c])
+
         ans = []
 
-        for key, val in diag.items():
+        for key, val in anti_diag.items():
             if key % 2 == 0:
                 ans += val[::-1]
             else:
                 ans += val
         return ans
+            
+    
