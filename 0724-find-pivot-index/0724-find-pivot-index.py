fc1053cbe505a8d5b25111ca4e0b61fa58392b5d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-01 21:00:02 -0400

    Time: 139 ms (58.19%), Space: 17.4 MB (91%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,15 +1,14 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # we get the right and left bounds
-        right_sum = sum(nums) 
-        left_total = 0
+        total = sum(nums)
+        curr = 0
 
         for index, num in enumerate(nums):
-            # We decrement the right bound first and check if it matches
-            right_sum -= num
-            # if match return index
-            if right_sum == left_total:
+            
+            total -= num
+
+            if total == curr:
                 return index
-            # then we increment the left bound
-            left_total += num
+            curr += num
         return -1
+        
