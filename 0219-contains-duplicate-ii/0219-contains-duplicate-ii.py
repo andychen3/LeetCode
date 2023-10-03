ca8e5a7ef537475474e6d5812a30a30742c3d7e3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-03 10:43:11 -0400

    Time: 587 ms (10.16%), Space: 35.9 MB (5.1%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,20 +1,14 @@
+from collections import defaultdict
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        """
-        Find a duplicate and if there is a duplicate the absolute value of the difference of their indexes
-        is less than k.
-        
-        Param: array of size n
-        Param: k = an integer
-        
-        Return Boolean 
-        """
-        hash = collections.defaultdict(int)
-        
-        for index, num in enumerate(nums):
-            if num in hash and index - hash[num] <= k:
-                return True
-            else:
-                hash[num] = index
-        
+        counts = defaultdict(list)
+
+        for idx, num in enumerate(nums):
+            counts[num].append(idx)
+
+        for key, val in counts.items():
+            if len(val) >= 2:
+                for i in range(1, len(val)):
+                    if abs(val[i] - val[i-1]) <= k:
+                        return True
         return False
