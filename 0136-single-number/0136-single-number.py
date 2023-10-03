25648d333ba9edf6517e3e18275d3cdc8046ddd1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-02 20:18:44 -0400

    Time: 116 ms (90%), Space: 18.8 MB (86.68%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,9 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        
-        for n in nums:
-            res ^= n
-        return res
+        counts = Counter(nums)
+
+        for key, val in counts.items():
+            if val == 1:
+                return key
+
