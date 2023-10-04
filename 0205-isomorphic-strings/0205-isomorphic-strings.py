b3ce8a2ab05fc8e9210ceef33efd224ed79091e3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 19:53:50 -0400

    Time: 34 ms (98.07%), Space: 16.5 MB (91.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -4,11 +4,10 @@ class Solution:
         s_map = defaultdict(str)
         t_map = defaultdict(str)
 
-        for s_char, t_char in zip(s,t):
+        for s_char, t_char in zip(s, t):
             if s_char not in s_map and t_char not in t_map:
                 s_map[s_char] = t_char
                 t_map[t_char] = s_char
-
-            if s_map[s_char] != t_char or t_map[t_char] != s_char:
+            elif s_map[s_char] != t_char or t_map[t_char] != s_char:
                 return False
         return True
