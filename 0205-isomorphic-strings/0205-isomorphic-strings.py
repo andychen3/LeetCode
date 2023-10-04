50193a7b4383222e4daade3d851e4cc439ef10b0
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 16:11:13 -0400

    Time: 46 ms (59.73%), Space: 16.6 MB (66.47%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,3 +1,14 @@
+from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
+        s_map = defaultdict(str)
+        t_map = defaultdict(str)
+
+        for s_char, t_char in zip(s,t):
+            if s_char not in s_map and t_char not in t_map:
+                s_map[s_char] = t_char
+                t_map[t_char] = s_char
+
+            if s_map[s_char] != t_char or t_map[t_char] != s_char:
+                return False
+        return True
