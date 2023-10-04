c4c0b02e79cc8403ed3a91f9101768b3aebbd1a5
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 17:52:38 -0400

    Time: 292 ms (71.08%), Space: 21.5 MB (62.48%) - LeetHub

--- 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
+++ 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
@@ -3,20 +3,22 @@ class TwoSum:
 
     def __init__(self):
         self.hash_map = defaultdict(int)
+        
 
     def add(self, number: int) -> None:
         self.hash_map[number] += 1
         
 
     def find(self, value: int) -> bool:
-        for num in self.hash_map.keys():
-            difference = value - num
-            if num != difference:
-                if difference in self.hash_map:
+        for key in self.hash_map.keys():
+            diff = value - key
+            if key != diff:
+                if diff in self.hash_map:
                     return True
-            elif self.hash_map[num] > 1:
+            elif self.hash_map[key] > 1:
                 return True
         return False
+        
 
 
 # Your TwoSum object will be instantiated and called as such:
