fcfa65c52c4c28562e8f3a87634b9a9dc238f67f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 16:23:01 -0400

    Time: 609 ms (13.44%), Space: 16.9 MB (42.45%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,22 +1,16 @@
 from collections import defaultdict
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        intersect = set(list1) & set(list2)
         hash_map = defaultdict(int)
 
         for idx, word in enumerate(list1):
-            if word in intersect:
-                hash_map[word] += idx
-            
-        for idx, word in enumerate(list2):
-            if word in intersect:
-                hash_map[word] += idx
+            for idx2, word2 in enumerate(list2):
+                if word == word2:
+                    hash_map[word] = idx + idx2
         
-        ans = []
         min_val = min(hash_map.values())
-
+        ans = []
         for key, val in hash_map.items():
             if val == min_val:
                 ans.append(key)
         return ans
-        
