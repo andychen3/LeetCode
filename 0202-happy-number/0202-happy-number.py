b839dc45fbc8c6ff8b652ec0bb4669b90ca5cbb9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 16:00:53 -0400

    Time: 47 ms (26.17%), Space: 16.1 MB (83.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,18 +1,8 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        
-        def find_n(n):
-            total = 0
-            while n:
-                n, digits = divmod(n, 10)
-                total += digits **2
-            return total
-
-
         seen = set()
 
         while n != 1 and n not in seen:
             seen.add(n)
-            n = find_n(n)
-        
+            n = sum(int(digits)**2 for digits in str(n))
         return n == 1
