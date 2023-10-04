52b53ee2085a5a099972176cf789dd761533be31
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 07:31:42 -0400

    Time: 213 ms (56.74%), Space: 29.7 MB (44.38%) - LeetHub

---
+++ 0288-unique-word-abbreviation/0288-unique-word-abbreviation.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+class ValidWordAbbr:
+
+    def __init__(self, dictionary: List[str]):
+        self.list = dictionary
+        self.hash_map = defaultdict(set)
+
+        for words in self.list:
+            length = len(words)
+            if length <= 2:
+                self.hash_map[words].add(words)
+            else:
+                self.hash_map[words[0] + str((length-2)) + words[-1]].add(words)
+
+    def convert_word(self, word):
+        converted = word
+        if len(word) > 2:
+            converted = word[0] + str(len(word)-2) + word[-1]
+        return converted
+        
+
+    def isUnique(self, word: str) -> bool:
+        converted = self.convert_word(word)
+        if converted in self.hash_map and len(self.hash_map[converted]) == 1 and word in self.hash_map[converted]:
+            return True
+        elif converted not in self.hash_map:
+            return True
+        else:
+            return False
+        
+
+# Your ValidWordAbbr object will be instantiated and called as such:
+# obj = ValidWordAbbr(dictionary)
+# param_1 = obj.isUnique(word)
