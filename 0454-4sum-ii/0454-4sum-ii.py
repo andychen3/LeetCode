5c819f3f7044cfd94364dad3f0d252eae44aebe7
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-04 07:00:39 -0400

    Time: 422 ms (93.47%), Space: 16.5 MB (76.56%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,16 +1,15 @@
-
+from collections import defaultdict
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        counts = {}
+        hash_map = defaultdict(int)
+
+        for i1 in nums1:
+            for i2 in nums2:
+                hash_map[i1+i2] += 1
+        
         ans = 0
-        
-        for a in nums1:
-            for b in nums2:
-                counts[a+b] = counts.get(a+b, 0) + 1
-        
-        
-        for c in nums3:
-            for d in nums4:
-                if -(c+d) in counts:
-                    ans += counts[-(c+d)]
+        for i3 in nums3:
+            for i4 in nums4:
+                ans += hash_map[-(i3+i4)]
         return ans
+        
