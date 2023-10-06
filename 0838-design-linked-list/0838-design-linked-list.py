5599e899cd880c943762b7df041536fb8f1c9301
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-06 14:07:28 -0400

    Time: 187 ms (27.77%), Space: 17 MB (93.25%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -3,17 +3,16 @@ class ListNode:
         self.val = val
         self.next = next
 
-
 class MyLinkedList:
 
     def __init__(self):
         self.size = 0
         self.head = ListNode()
-        
 
     def get(self, index: int) -> int:
         if index < 0 or index >= self.size:
             return -1
+        
         curr = self.head
         for _ in range(index+1):
             curr = curr.next
@@ -28,13 +27,11 @@ class MyLinkedList:
     def addAtIndex(self, index: int, val: int) -> None:
         if index > self.size:
             return
-        
+
         if index < 0:
             index = 0
-        
-        self.size += 1
-        prev = self.head
 
+        prev = self.head
         for _ in range(index):
             prev = prev.next
         
@@ -42,14 +39,19 @@ class MyLinkedList:
         to_add.next = prev.next
         prev.next = to_add
 
+        self.size += 1
+
     def deleteAtIndex(self, index: int) -> None:
         if index < 0 or index >= self.size:
-            return 
-        self.size -= 1
+            return
+        
         prev = self.head
         for _ in range(index):
             prev = prev.next
-        prev.next = prev.next.next        
+        prev.next = prev.next.next
+        self.size -= 1
+
+        
 
 
 # Your MyLinkedList object will be instantiated and called as such:
