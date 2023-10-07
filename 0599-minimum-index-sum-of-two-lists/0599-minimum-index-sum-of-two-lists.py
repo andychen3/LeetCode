4c82efd016da3224587549b3c46235802091a537
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-07 18:39:59 -0400

    Time: 158 ms (80.26%), Space: 16.8 MB (41.55%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,16 +1,22 @@
-from collections import defaultdict
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        hash_map = defaultdict(int)
-
-        for idx, word in enumerate(list1):
-            for idx2, word2 in enumerate(list2):
-                if word == word2:
-                    hash_map[word] = idx + idx2
+        hash_map = {}
+        same_elem = {}
         
-        min_val = min(hash_map.values())
+        for idx, key in enumerate(list1):
+            hash_map[key] = idx
+        
+        for idx, key in enumerate(list2):
+            if key in hash_map:
+                same_elem[key] = idx
+                same_elem[key] += hash_map[key]
+        
+        min_val = min(same_elem.values())
         ans = []
-        for key, val in hash_map.items():
+
+        for key, val in same_elem.items():
             if val == min_val:
                 ans.append(key)
         return ans
+                
+        
