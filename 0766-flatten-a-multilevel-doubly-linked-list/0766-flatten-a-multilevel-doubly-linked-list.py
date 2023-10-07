0b0a17553b7ff02c21d818fb0614186338421b4d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-07 08:03:45 -0400

    Time: 37 ms (89.03%), Space: 17 MB (69.98%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.py
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.py
@@ -10,25 +10,29 @@ class Node:
 
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
-
+        dummy = ListNode(next=head)
+        curr = head
         def merge(curr):
             child = curr.child
 
+            # Iterate through child list
+            # We want child.next instead of just child because we only want to iterate through the list
+            # if there is an element after child. Because we don't want to set child to null.
             while child.next:
                 child = child.next
             
             if curr.next:
                 child.next = curr.next
                 curr.next.prev = child
+            # Go back to the current element before it entered into child list
             curr.next = curr.child
             curr.child.prev = curr
             curr.child = None
 
-        curr = head
         while curr:
             if curr.child:
                 merge(curr)
             curr = curr.next
-        return head
+        return dummy.next
+
+
