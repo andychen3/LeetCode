e92c50757c437ff10c729d5dd0e3164692d93c11
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-07 06:49:06 -0400

    Time: 59 ms (87.33%), Space: 20.1 MB (18.21%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -11,7 +11,9 @@ class Solution:
         while curr:
             if curr.val == val:
                 prev.next = curr.next
+                # We don't do prev = prev.next because to remove vals we always have to be one behind
             else:
+                # You only move this when the current element isn't the value
                 prev = curr
             curr = curr.next
         return dummy.next 
