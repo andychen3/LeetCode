1a80be47883984580d2a22ef43899b94b6fa3bd1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-07 09:09:45 -0400

    Time: 42 ms (69.28%), Space: 17.3 MB (58.76%) - LeetHub

--- 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.py
+++ 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.py
@@ -9,21 +9,23 @@ class Node:
 class Solution:
     def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
         if not head:
-            curr = Node(insertVal)
-            curr.next = curr
-            return curr
-
-        
-
+            node = Node(insertVal)
+            node.next = node
+            return node
         curr = head
         while curr.next != head:
+            
             if curr.val <= insertVal <= curr.next.val:
                 break
-            if curr.val > curr.next.val and (curr.val <= insertVal or insertVal <= curr.next.val):
+            # curr.val is to find the highest number and where the tail shuld be.
+            # curr.val <= insertval is for the condiiton that you are at the tail and you are trying
+            # to put a bigger number
+            # curr.next.val >= insertVal is for if you reached the tail but the insertval is actually smaller so you have to keep going.
+            # We have the while condition to break out if you go back in the cycle so the second condition 
+            # curr.next.val >= insertVal solves for the 0 case if the tail element was this [3,5,1] and you are trying to put in a 0 which should go [3,5,0,1]. You need to add the curr.next.val >= insertVal
+            if curr.val > curr.next.val and (curr.val <= insertVal or curr.next.val >= insertVal):
                 break
             curr = curr.next
-        
-        node = Node(insertVal, next=curr.next)
+        node = Node(insertVal, curr.next)
         curr.next = node
-        
         return head
