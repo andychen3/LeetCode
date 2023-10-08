041adee5b7b59fa38bb19ee4bedeb3fb5aeb5f67
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-07 21:29:50 -0400

    Time: 42 ms (69.89%), Space: 16.3 MB (82.04%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -3,13 +3,14 @@ class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
 
-        def create_key(word):
+        def get_key(word):
             key = []
             for i in range(len(word)-1):
                 key.append((ord(word[i+1]) - ord(word[i])) % 26)
             return tuple(key)
 
-        for word in strings:
-            key = create_key(word)
-            groups[key].append(word)
+
+        for words in strings:
+            key = get_key(words)
+            groups[key].append(words)
         return list(groups.values())
