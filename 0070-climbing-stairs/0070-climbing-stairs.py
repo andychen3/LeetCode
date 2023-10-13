83cacda49d37b33c41b8e2daa4e2d6c490a139ee
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-13 13:14:34 -0400

    Time: 38 ms (52.07%), Space: 16.3 MB (15.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,14 @@
-from functools import cache
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # This is the correct recursive definition the others are wrong even though they work
-        @cache 
-        def dp(i):
-            # We could so the shorthand of i <= 2 return i but this is more explicit
-
-            # The reason we don't do 0 is because our constraint is n >= 1. So we will never get 0
-            # as a testcase.
-            # This also makes sense since we don't get zero that means the number of ways to reach 1 step
-            # is 1 and to reach 2 is 2. (1+1) and (2)
-            if i == 1:
+        def dp(n):
+            if n == 1:
                 return 1
-
-            if i == 2:
+            if n == 2:
                 return 2
-            
-            return dp(i-1) + dp(i-2)
+            if n in memo:
+                return memo[n]
+            memo[n] = dp(n-1) + dp(n-2)
+            return memo[n]
+
+        memo = {}
         return dp(n)
