9d44ea3f5edc45c75a3de475eef999ad02afa361
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-14 08:23:28 -0400

    Time: 178 ms (33.29%), Space: 16.9 MB (92.47%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -1,20 +1,14 @@
 class ListNode:
-    def __init__(self, val=0, next=None, prev=None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        self.prev = prev
-
 
 class MyLinkedList:
 
     def __init__(self):
         self.size = 0
         self.head = ListNode()
-        self.tail = ListNode()
-        self.head.next = self.tail
-        self.tail.prev = self.head
         
-
     def get(self, index: int) -> int:
         if index < 0 or index >= self.size:
             return -1
@@ -23,7 +17,6 @@ class MyLinkedList:
         for _ in range(index+1):
             curr = curr.next
         return curr.val
-        
 
     def addAtHead(self, val: int) -> None:
         self.addAtIndex(0, val)
@@ -32,35 +25,30 @@ class MyLinkedList:
         self.addAtIndex(self.size, val)
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index > self.size:
-            return
+        if index > self.size: # We don't have equal to size because equal to size means out of bounds
+            return 
         
         if index < 0:
             index = 0
-
-        prev = self.head
+        
+        curr = self.head
         for _ in range(index):
-            prev = prev.next
+            curr = curr.next
         
-        node = ListNode(val, next=prev.next, prev=prev)
-        prev.next = node
-        prev.next.prev = node
-        
+        node_to_add = ListNode(val)
+        node_to_add.next = curr.next
+        curr.next = node_to_add
         self.size += 1
 
     def deleteAtIndex(self, index: int) -> None:
         if index < 0 or index >= self.size:
-            return
+            return 
         
-        prev = self.head
+        curr = self.head
         for _ in range(index):
-            prev = prev.next
-        
-        nxt = prev.next.next
-        prev.next = nxt
-        nxt.prev = prev
+            curr = curr.next
+        curr.next = curr.next.next
         self.size -= 1
-
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
