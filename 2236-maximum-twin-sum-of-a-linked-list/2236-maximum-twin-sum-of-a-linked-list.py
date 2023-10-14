b8959011806fd961117628a22f6470ff494f653e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-14 09:44:33 -0400

    Time: 635 ms (72.85%), Space: 47.4 MB (72.13%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -6,29 +6,22 @@
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         slow = fast = head
+        max_sum = 0
 
-        # find the middle
-        while fast:
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        # slow should be at the middle.
-        # reverse the list
         prev = None
         while slow:
-            next_node = slow.next
+            temp = slow.next
             slow.next = prev
-
             prev = slow
-            slow = next_node
+            slow = temp
         
-        # find sum
-        max_sum = 0
-
-        while head.next:
-            max_sum = max(prev.val + head.val, max_sum)
-            head = head.next
+        curr = head
+        while prev:
+            max_sum = max(prev.val + curr.val, max_sum)
+            curr = curr.next
             prev = prev.next
-        
         return max_sum
-        
