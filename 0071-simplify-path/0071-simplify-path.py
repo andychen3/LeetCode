31c38ead1e33f12c8db302a54fab7f8fff4d8d73
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-16 17:47:24 -0400

    Time: 40 ms (60.08%), Space: 16.2 MB (94.6%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -4,13 +4,11 @@ class Solution:
         stack = []
 
         for c in abs_path:
-            if c == "" or c == ".":
+            if c == "." or c == "":
                 continue
             elif c == "..":
                 if stack:
                     stack.pop()
             else:
                 stack.append(c)
-
         return "/" + "/".join(stack)
-            
