12b452e92139efdce703ec91ce538efb3ce4841f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-16 17:45:08 -0400

    Time: 46 ms (23.69%), Space: 16.1 MB (94.6%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,7 +1,6 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         abs_path = path.split("/")
-        print(abs_path)
         stack = []
 
         for c in abs_path:
@@ -12,7 +11,6 @@ class Solution:
                     stack.pop()
             else:
                 stack.append(c)
-        print("/".join(stack))
 
         return "/" + "/".join(stack)
             
