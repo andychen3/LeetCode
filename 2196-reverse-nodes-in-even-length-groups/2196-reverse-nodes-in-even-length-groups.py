c2a49b5794575531957c24308b609350633d0dba
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-16 16:25:00 -0400

    Time: 1523 ms (76.77%), Space: 55.8 MB (89.37%) - LeetHub

--- 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
+++ 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
@@ -5,46 +5,35 @@
 #         self.next = next
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        group = 2
-        tail = head
+        dummy = prev = ListNode(next=head)
+        groups = 1
+        curr = head
+        connect = None
+        length = 1
+        def reverse(node, n):
+            new_curr = node.next
+            prev = node
+            node_to_start = new_curr
 
-        while tail and tail.next:
-            count = 1
-            curr = tail.next
-
-            # find number of nodes within group
-            # It is curr.next because you have to find out if the next element exists
-            while curr.next and count < group:
-                count += 1
-                curr = curr.next
-
-            # Set variables to begin reversal
-            prev, curr = tail, tail.next
-
-            if count % 2 == 0:
-                # cookie cutter traversal
-                while count and curr:
-                    next_node = curr.next
-                    curr.next = prev
-
-                    prev = curr
-                    curr = next_node
-                    count -= 1
-                # Find start of current group
-                first = tail.next
-                # Set the current group's first node to connect to next group's first node
-                first.next = curr
-                # Set last group's end node to the current group's end node
-                tail.next = prev
-                # Move tail to end node of current group
-                tail = first
-            else:
-                # No reversal
-                while count and curr:
-                    prev = curr
-                    curr = curr.next
-                    count -= 1
-                tail = prev
+            for _ in range(n):
+                temp = new_curr.next
+                new_curr.next = prev
+                prev = new_curr
+                new_curr = temp
+            node.next = prev
+            node_to_start.next = new_curr
+            return node_to_start
             
-            group += 1
-        return head
+
+        while curr:
+            if groups == length or not curr.next:
+                if length % 2 == 0:
+                    curr = reverse(connect, length)
+                connect = curr
+                groups += 1
+                length = 0
+            length += 1
+            curr = curr.next
+        return dummy.next
+
+        
