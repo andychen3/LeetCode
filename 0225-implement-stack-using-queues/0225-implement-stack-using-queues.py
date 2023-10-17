4ab1b4aee793228afea727fe08b1e87ce14cf96e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-17 16:21:44 -0400

    Time: 33 ms (84.7%), Space: 16.3 MB (63.5%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -3,6 +3,7 @@ class MyStack:
 
     def __init__(self):
         self.q = deque()
+        
 
     def push(self, x: int) -> None:
         self.q.append(x)
@@ -14,6 +15,7 @@ class MyStack:
 
     def top(self) -> int:
         return self.q[0]
+        
 
     def empty(self) -> bool:
         return not self.q
