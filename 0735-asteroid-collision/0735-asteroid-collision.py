acc3c3ca4c99136c1cd01036d818849889953535
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-17 09:03:45 -0400

    Time: 93 ms (61.84%), Space: 17.3 MB (85.12%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,19 +3,13 @@ class Solution:
         stack = []
 
         for rock in asteroids:
-            # Check if there are asteroids on stack and if the top is positive and next rock is negative
             while stack and stack[-1] > 0 and rock < 0:
-                # if both are same size break both and move on
-                if stack[-1] + rock == 0:
+                if stack[-1] < abs(rock):
                     stack.pop()
-                    break
-                # if top rock is greater than coming rock move on
-                elif stack[-1] + rock > 0:
-                    break
-                # if rock is bigger than top rock break top rock and keep checking stack
-                elif stack[-1] + rock < 0:
+                    continue
+                elif stack[-1] == abs(rock):
                     stack.pop()
-            # If stack is empty or positive rock add to stack.
+                break
             else:
                 stack.append(rock)
-        return stack
+        return stack        
