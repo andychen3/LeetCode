798803ac3816669936f1bf1eda2a53260f33f54d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-17 15:33:06 -0400

    Time: 33 ms (97.29%), Space: 17.3 MB (55.04%) - LeetHub

--- 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.py
+++ 0850-insert-into-a-sorted-circular-linked-list/0850-insert-into-a-sorted-circular-linked-list.py
@@ -12,18 +12,16 @@ class Solution:
             new_node = ListNode(insertVal)
             new_node.next = new_node
             return new_node
-
+        
         curr = head
-
         while curr.next != head:
             if curr.val <= insertVal <= curr.next.val:
                 break
             elif curr.val > curr.next.val and (insertVal > curr.val or insertVal < curr.next.val):
                 break
-
             curr = curr.next
 
-
-        node = ListNode(insertVal, next=curr.next)
+        node = ListNode(insertVal, next=curr.next) 
         curr.next = node
-        return head
+        return head   
+        
