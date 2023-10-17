81b5a1010cc377072f7ffd2116a927681a58ba2c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-17 07:30:01 -0400

    Time: 168 ms (95.83%), Space: 17.9 MB (52.82%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.py
@@ -3,9 +3,8 @@ class Solution:
         stack = []
 
         for c in s:
-            if stack and c == "*":
+            if c == "*":
                 stack.pop()
             else:
                 stack.append(c)
-        
         return "".join(stack)
