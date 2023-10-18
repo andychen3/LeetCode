8ab701e4ce3b5adaea3908e4fdf2e23fcd838e53
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-18 06:50:04 -0400

    Time: 63 ms (87.67%), Space: 17.1 MB (76.73%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -6,13 +6,11 @@ class Solution:
         zeros = arr.count(0)
         n = len(arr)
 
-        for i in range(n-1,-1,-1):
+        for i in range(n - 1, -1, -1):
             if i + zeros < n:
-                arr[i+zeros] = arr[i]
+                arr[i + zeros] = arr[i]
             if arr[i] == 0:
                 zeros -= 1
-                # We do this second if statement because we are making space for the zeros before it
-                # Since the place where we found this element could not be a zero but is suppose to be a zero
                 if i + zeros < n:
-                    arr[i+zeros] = 0
+                    arr[i + zeros] = 0
         return arr
