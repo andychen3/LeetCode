66b92d70d507a85dc6a65d9b06272123b7f73311
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-19 11:45:54 -0400

    Time: 433 ms (41.19%), Space: 58.1 MB (60.34%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -9,18 +9,12 @@ class Solution:
         def dfs(node):
             if not node:
                 return 0
-
-            # Check for a leaf
-            # if node.left == None and node.right == None:
-            #     return min(node.left, node.right) + 1
             
             left = dfs(node.left)
             right = dfs(node.right)
-
-            if not left or not right:
-                return max(left, right) + 1
-
-            return min(left, right) + 1
-
-        return dfs(root)
+            if node.left and node.right:
+                return min(left, right) + 1
+            
+            return max(left, right) + 1
         
+        return dfs(root)
