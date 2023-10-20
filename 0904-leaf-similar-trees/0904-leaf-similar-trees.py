b4b95bc7fec23775bdf921c40818499c5284d174
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-10-20 17:16:11 -0400

    Time: 47 ms (10.2%), Space: 16.4 MB (41.25%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -12,6 +12,8 @@ class Solution:
 
             if not node.left and not node.right:
                 return [node.val]
-            return dfs(node.left) + dfs(node.right)
+            left = dfs(node.left)
+            right = dfs(node.right)
+            return left + right
 
         return dfs(root1) == dfs(root2)
