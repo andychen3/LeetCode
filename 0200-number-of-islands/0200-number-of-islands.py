7370740c8450a3335f6585d1414b1253bc803293
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-12-29 09:15:28 -0500

    Time: 255 ms (91.61%), Space: 28.7 MB (5.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def valid(r, c):
-            return 0 <= r < rows and 0 <= c < cols and grid[r][c] == '1'
-        
         rows = len(grid)
         cols = len(grid[0])
-        
         seen = set()
-        islands = 0
-        directions = ((0,1), (1,0), (-1,0), (0,-1))
+        ans = 0
         
-        def dfs(r, c):
-            for dx, dy in directions:
-                new_dx, new_dy = dx + r, dy + c
-                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                    seen.add((new_dx, new_dy))
-                    dfs(new_dx, new_dy)
+        def dfs(x, y):
+            if x < 0 or x >= rows or y < 0 or y >= cols or grid[x][y] == '0' or (x, y) in seen:
+                return
+            seen.add((x,y))
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+        
+        
         
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in seen:
-                    seen.add((r,c))
-                    islands += 1
-                    dfs(r, c)
-        return islands
+                if grid[r][c] == '1' and (r,c) not in seen:
+                    dfs(r,c)
+                    ans += 1
+        return ans
