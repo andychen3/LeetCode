a03360d8a14a9480524d4cd97c0d1c24633804cd
Andy Chen <81591593+andychen3@users.noreply.github.com>
2023-12-29 08:22:31 -0500

    Time: 61 ms (6.06%), Space: 17.2 MB (6.85%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,11 +1,11 @@
-from collections import defaultdict
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        hash_map = defaultdict(int)
+        hash_map = {}
         ans = 0
         
         for num in nums:
-            ans += hash_map[num]
-            hash_map[num] += 1
+            if num in hash_map:
+                ans += hash_map[num]
+            hash_map[num] = hash_map.get(num, 0) + 1
         return ans
             
