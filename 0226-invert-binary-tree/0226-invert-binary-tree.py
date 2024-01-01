1e9d68a68a52033d33635405afd96ec76d724270
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-01 17:25:47 -0500

    Time: 76 ms (5.08%), Space: 17.3 MB (13.13%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -7,11 +7,10 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return
+            return 
         
         root.left, root.right = root.right, root.left
-        
-        left = self.invertTree(root.left)
-        right = self.invertTree(root.right)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         
         return root
