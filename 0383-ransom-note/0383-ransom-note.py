b92b7e8feb7c800d636875d61ec6c7339eae79c6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-19 10:40:31 -0500

    Time: 63 ms (48.97%), Space: 16.7 MB (64.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,19 @@
-from collections import Counter
+from collections import defaultdict
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_count = Counter(ransomNote)
-        magazine_count = Counter(magazine)
-
-        for char, count in ransom_count.items():
-            if magazine_count[char] < count:
+        if len(ransomNote) > len(magazine):
+            return False
+        hash_map = defaultdict(int)
+        
+        for char in magazine:
+            hash_map[char] += 1
+        
+        for char in ransomNote:
+            if char in hash_map and hash_map[char] > 0:
+                hash_map[char] -= 1
+                if hash_map[char] == 0:
+                    del hash_map[char]
+            else:
                 return False
         return True
         
