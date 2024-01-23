fdf769fc00b4f288a8a048d9bfd1f10f890f3b2e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-23 17:21:07 -0500

    Time: 34 ms (81.99%), Space: 16.4 MB (74.91%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,8 +1,7 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def remove_backspace(string):
+        def remove(string):
             stack = []
-            
             for char in string:
                 if char == "#":
                     if stack:
@@ -10,5 +9,4 @@ class Solution:
                 else:
                     stack.append(char)
             return "".join(stack)
-        
-        return remove_backspace(s) == remove_backspace(t)
+        return remove(s) == remove(t)
