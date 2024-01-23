8bbf92b8f718c2d7e851be8b153e0d03533e519b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-23 08:17:06 -0500

    Time: 49 ms (61.93%), Space: 16.6 MB (54.86%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,14 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hash_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
-        output = 0
+        roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        n = len(s)
+        ans = 0
         i = 0
-
-        while i < len(s):
-            if i + 1 < len(s) and hash_map[s[i]] < hash_map[s[i+1]]:
-                output += hash_map[s[i+1]] - hash_map[s[i]]
+        
+        while i < n:
+            char = s[i]
+            if i + 1 < n and char == "I" and (s[i + 1] == 'V' or s[i + 1] == "X"):
+                ans += roman_map[s[i + 1]] - roman_map[char]
+                i += 2
+            elif i + 1 < n and char == "X" and (s[i + 1] == 'L' or s[i + 1] == "C"):
+                ans += roman_map[s[i + 1]] - roman_map[char]
+                i += 2
+            elif i + 1 < n and char == "C" and (s[i + 1] == 'D' or s[i + 1] == "M"):
+                ans += roman_map[s[i + 1]] - roman_map[char]
                 i += 2
             else:
-                output += hash_map[s[i]]
+                ans += roman_map[char]
                 i += 1
-        return output
+        return ans
+                
+                
