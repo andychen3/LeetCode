d5ab32b65f204ffb424aa64741cb37b33047cb87
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-24 11:43:56 -0500

    Time: 38 ms (53.35%), Space: 16.6 MB (37.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,15 +6,29 @@ class Solution:
         # 1 house or 2nd house. Max of them both
         # What are the base cases?
         # i = 0 then pick the house that has the most
-        @cache
-        def dp(i):
-            if i == 0:
-                return nums[0]
-            if i == 1:
-                return max(nums[0], nums[1])
+        
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        dp = [0] * n
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[1], nums[0])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        
+        return dp[n-1]
+        
+        
+#         @cache
+#         def dp(i):
+#             if i == 0:
+#                 return nums[0]
+#             if i == 1:
+#                 return max(nums[0], nums[1])
             
-            return max(nums[i] + dp(i-2), dp(i-1))
+#             return max(nums[i] + dp(i-2), dp(i-1))
         
-        
-        
-        return dp(len(nums) - 1)
+#         return dp(len(nums) - 1)
