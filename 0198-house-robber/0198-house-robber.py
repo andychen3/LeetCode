3a5e1ef90317dcd644beaf0eb30d0e4b52e2218e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-24 15:46:01 -0500

    Time: 31 ms (88.67%), Space: 16.5 MB (76.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,28 @@
 from functools import cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
         
-        def dp(i):
-            if i == 0:
-                return nums[0]
-            if i == 1:
-                return max(nums[0], nums[1])
-            if i in memo:
-                return memo[i]
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i-2] + nums[i])
+        return dp[n - 1]
+        
+        
+#         @cache
+#         def dp(i):
+#             if i == 0:
+#                 return nums[0]
+#             if i == 1:
+#                 return max(nums[0], nums[1])
             
-            memo[i] =  max(dp(i - 1), dp(i - 2) + nums[i])
-            return memo[i]
             
-        memo = {}    
-        return dp(len(nums) - 1)
-    
+#             return max(dp(i - 1), dp(i - 2) + nums[i])
+            
+        
+#         return dp(len(nums) - 1)
