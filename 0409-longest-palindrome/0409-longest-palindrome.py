cb5d6d4de4248e60fe0684da48f6b7955628fdcd
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-25 15:50:05 -0500

    Time: 44 ms (27.78%), Space: 16.6 MB (64.04%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,13 +1,14 @@
 from collections import Counter
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        freq = Counter(s)
+        counts = Counter(s)
         odds = 0
         
-        for counts in freq.values():
-            if counts % 2:
+        for freq in counts.values():
+            if freq % 2 == 1:
                 odds += 1
         
         if odds > 1:
             return len(s) - odds + 1
         return len(s)
+        
