693e65ea7eb8fd980de230f47d2dd5a5c7d5b598
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-26 09:27:54 -0500

    Time: 104 ms (95.13%), Space: 17.8 MB (64.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        hash_set = set(nums)
-
-        for i in range(len(nums)+1):
-            if i not in hash_set:
-                return i
+        ans = len(nums)
+        
+        for i, num in enumerate(nums):
+            ans ^= i ^ num
+        return ans
