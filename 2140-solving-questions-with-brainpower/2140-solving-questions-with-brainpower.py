29f70301e10142e8e3245b678debb70bd9c276cf
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-29 09:52:41 -0500

    Time: 1255 ms (86.60%), Space: 62.8 MB (83.13%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,10 +1,19 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        @cache
-        def dp(i):
-            if i >= len(questions):
-                return 0
-            
+        n = len(questions)
+        dp = [0] * (n+1)
+        
+        for i in range(n - 1, -1, -1):
             j = i + questions[i][1] + 1
-            return max(questions[i][0] + dp(j), dp(i + 1))
-        return dp(0)
+            dp[i] = max(dp[i+1], questions[i][0] + dp[min(j, n)])
+        return dp[0]
+            
+            
+#         @cache
+#         def dp(i):
+#             if i >= len(questions):
+#                 return 0
+            
+#             j = i + questions[i][1] + 1
+#             return max(questions[i][0] + dp(j), dp(i + 1))
+#         return dp(0)
