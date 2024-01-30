cd95244ad3f9a31ff893c470873afec868e700e2
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-30 12:36:25 -0500

    Time: 74 ms (79.17%), Space: 19.9 MB (64.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -8,5 +8,6 @@ class Solution:
             elif interval[0] > newInterval[1]:
                 r.append(interval)
             else:
-                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
+                newInterval = [min(interval[0], newInterval[0]),
+                               max(interval[1], newInterval[1])]
         return l + [newInterval] + r
