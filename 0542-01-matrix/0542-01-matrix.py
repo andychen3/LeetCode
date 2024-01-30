25c4d0e2184c7f84aeb4964f7713b7126199ea2e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-01-30 17:06:44 -0500

    Time: 509 ms (54.66%), Space: 19.8 MB (69.79%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,25 +2,27 @@ from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         queue = deque()
-        rows, cols = len(mat), len(mat[0])
         seen = set()
+        directions = ((0,1), (1,0), (0,-1), (-1,0))
+        rows, cols = len(mat), len(mat[0])
         
         for row in range(rows):
             for col in range(cols):
                 if mat[row][col] == 0:
                     queue.append((row, col, 1))
                     seen.add((row, col))
+        
         def valid(r, c):
-            return 0 <= r < rows and 0 <= c < cols and mat[r][c] == 1
+            return 0 <= r < rows and 0 <= c < cols
         
         while queue:
             x, y, steps = queue.popleft()
             
-            for x1, y1 in [(0, 1), (1, 0), (-1,0), (0,-1)]:
-                new_dx, new_dy = x1 + x, y1 + y
-                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                    seen.add((new_dx, new_dy))
-                    queue.append((new_dx, new_dy, steps+1))
-                    mat[new_dx][new_dy] = steps
-        return mat
+            for dx, dy in directions:
+                new_dx, new_dy = dx + x, dy + y
                 
+                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                    mat[new_dx][new_dy] = steps
+                    seen.add((new_dx, new_dy))
+                    queue.append((new_dx, new_dy, steps + 1))
+        return mat
