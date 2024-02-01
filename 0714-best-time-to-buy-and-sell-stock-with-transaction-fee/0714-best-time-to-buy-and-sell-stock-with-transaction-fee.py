3429888a0f706e0872c234f05da90a65803af704
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-01 14:13:00 -0500

    Time: 975 ms (23.16%), Space: 230.3 MB (12.33%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -4,13 +4,12 @@ class Solution:
         def dp(i, holding):
             if i == len(prices):
                 return 0
-            
-            ans = dp(i+1, holding)
+                
+            ans = dp(i + 1, holding)
             if holding:
-                ans = max(prices[i] + dp(i+1, False) - fee, ans)
+                ans = max(ans, prices[i] + dp(i+1, False) - fee)
             else:
-                ans = max(-prices[i] + dp(i+1, True), ans)
-            
+                ans = max(ans, -prices[i] + dp(i+1, True))
             return ans
+        return dp(0, 0)
             
-        return dp(0, False)
