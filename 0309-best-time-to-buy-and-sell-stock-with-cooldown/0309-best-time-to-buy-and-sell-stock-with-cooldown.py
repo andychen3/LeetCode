211200ff781ea400fdde5e53cee6751450c4608e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-01 14:25:03 -0500

    Time: 52 ms (36.02%), Space: 20.5 MB (19.50%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        dp = [[0] * 2 for _ in range(n+2)] # We +2 because of the cooldown which is +2
-
-        for i in range(n - 1, -1 ,-1):
-            for holding in range(2):
-                skip = dp[i+1][holding]
-
-                if holding:
-                    do_something = prices[i] + dp[i+2][0]
-                else:
-                    do_something = -prices[i] + dp[i+1][1]
-                
-                dp[i][holding] = max(skip, do_something)
-        print(dp)
-        return dp[0][0]
+        @cache
+        def dp(i, holding):
+            if i >= len(prices):
+                return 0
+            
+            ans = dp(i + 1, holding)
+            if holding:
+                ans = max(ans, prices[i] + dp(i+2, False))
+            else:
+                ans = max(ans, -prices[i] + dp(i+1, True))
+            return ans
+        return dp(0,0)
+            
