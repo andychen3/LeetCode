a31cd1e3f851453593ce47e016fda619c8421fe3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-02 15:44:30 -0500

    Time: 1085 ms (41.17%), Space: 22 MB (9.78%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = set()
         
-        for index, num in enumerate(nums):
-            # if index > 0 and num == nums[index-1]:
-            #     continue
-            left = index + 1
-            right = len(nums) - 1
-            while left < right:
-                total = num + nums[left] + nums[right]
-                if total == 0:
-                    # res.append([num, nums[left], nums[right]])
-                    res.add((num, nums[left], nums[right]))
-                    left += 1
-                    right -= 1
-                    # while left < right and nums[left] == nums[left - 1]:
-                    #     left += 1
-                elif total > 0:
-                    right -= 1
-                else:
-                    left += 1
-        return res
+        seen = set()
+        ans = []
+        
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            hash_map = {}
+            for j in range(i + 1, len(nums)):
+                diff = (num + nums[j]) * -1
+                if diff in hash_map:
+                    triplets = tuple(sorted([num, nums[j], diff]))
+                    if triplets not in seen:
+                        seen.add(triplets)
+                        ans.append(list(triplets))
+                hash_map[nums[j]] = nums[j]
+        return ans
