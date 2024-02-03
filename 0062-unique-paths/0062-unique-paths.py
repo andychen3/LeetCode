7d02fb811594d0ea8cfa86fa24f5e5e015ec304f
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-03 09:35:18 -0500

    Time: 29 ms (94.29%), Space: 16.5 MB (78.90%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,14 +4,13 @@ class Solution:
         dp[0][0] = 1
         
         for row in range(m):
-            
             for col in range(n):
-                ans = 0    
+                
                 if row > 0:
-                    ans += dp[row - 1][col]
+                    dp[row][col] += dp[row - 1][col]
                 if col > 0:
-                    ans += dp[row][col - 1]
-                dp[row][col] += ans
+                    dp[row][col] += dp[row][col - 1]
+                
         return dp[m - 1][n - 1]
         
 #         @cache
