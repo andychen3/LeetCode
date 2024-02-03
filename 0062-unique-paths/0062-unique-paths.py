9ffefb31f6a6a212fe241e7ec7328943225e4cf2
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-03 09:26:38 -0500

    Time: 34 ms (78.50%), Space: 16.9 MB (55.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # This is a dp problem because its asking for num of possible unique paths and 
-        # The types of movements are constrained to only down and right
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = 1
-
-        for row in range(m):
-            for col in range(n):
-                # We are iterating forward and making sure that row is not negative because
-                # we start from 0,0
-                if row > 0:
-                    dp[row][col] += dp[row-1][col]
-                if col > 0:
-                    dp[row][col] += dp[row][col-1]
-
-        return dp[m-1][n-1]
+        @cache
+        def dp(row, col):
+            if row + col == 0:
+                return 1
+            
+            ans = 0
+            if row > 0:
+                ans += dp(row - 1, col)
+            if col > 0:
+                ans += dp(row, col - 1)
+            return ans
+        return dp(m - 1, n - 1)
