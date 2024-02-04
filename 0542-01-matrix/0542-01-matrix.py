d71bbf45ba8265d51444bc36514f9976cc4dd7e1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-04 11:40:42 -0500

    Time: 520 ms (47.98%), Space: 19.9 MB (68.79%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,27 +2,26 @@ from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         queue = deque()
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
         seen = set()
-        directions = ((0,1), (1,0), (0,-1), (-1,0))
-        rows, cols = len(mat), len(mat[0])
+        row, col = len(mat), len(mat[0])
         
-        for row in range(rows):
-            for col in range(cols):
-                if mat[row][col] == 0:
-                    queue.append((row, col, 1))
-                    seen.add((row, col))
+        for r in range(row):
+            for c in range(col):
+                if mat[r][c] == 0:
+                    seen.add((r,c))
+                    queue.append((r, c, 1))
         
         def valid(r, c):
-            return 0 <= r < rows and 0 <= c < cols
+            return 0 <= r < row and 0 <= c < col
         
         while queue:
             x, y, steps = queue.popleft()
-            
             for dx, dy in directions:
                 new_dx, new_dy = dx + x, dy + y
-                
                 if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                    mat[new_dx][new_dy] = steps
                     seen.add((new_dx, new_dy))
+                    mat[new_dx][new_dy] = steps
                     queue.append((new_dx, new_dy, steps + 1))
         return mat
+        
