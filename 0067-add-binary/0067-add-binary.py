a064852e5bbe06aa087f8a6b4ec20c905182a219
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-04 17:15:59 -0500

    Time: 33 ms (87.60%), Space: 16.5 MB (82.03%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,7 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         x, y = int(a, 2), int(b, 2)
-        
         while y:
             answer = x ^ y
             carry = (x & y) << 1
