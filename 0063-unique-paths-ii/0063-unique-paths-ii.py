89120012646dca22c34c763fc9ac76fd4ef02efd
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-04 10:05:00 -0500

    Time: 47 ms (53.70%), Space: 17.1 MB (53.59%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,25 +1,18 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        if obstacleGrid[0][0] == 1:
-            return 0
-        
+        @cache
+        def dp(row, col):
+            if obstacleGrid[row][col] == 1:
+                return 0
+            
+            if row + col == 0:
+                return 1
+            
+            ans = 0
+            if row > 0:
+                ans += dp(row - 1, col)
+            if col > 0:
+                ans += dp(row, col -1)
+            return ans
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = 1
-
-        # We go forward because we are trying to reach the bottom right corner
-        for row in range(m):
-            for col in range(n):
-                if obstacleGrid[row][col] == 1:
-                    dp[row][col] = 0
-                    continue
-                # Check if we aren't negative
-                if row > 0:
-                    # check how many steps were from the left
-                    dp[row][col] += dp[row-1][col]
-                if col > 0:
-                    # check how many steps were from the up
-                    dp[row][col] += dp[row][col-1]
-
-        return dp[m-1][n-1]
-        
+        return dp(m - 1, n - 1)
