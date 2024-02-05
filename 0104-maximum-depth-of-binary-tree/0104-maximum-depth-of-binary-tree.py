a7a59199bc306352836d4c4a603c43af1c410ece
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-05 11:38:32 -0500

    Time: 38 ms (80.49%), Space: 17.6 MB (95.18%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -9,10 +9,10 @@ class Solution:
         def dfs(node):
             if not node:
                 return 0
+            
             left = dfs(node.left)
             right = dfs(node.right)
+            
             return max(left, right) + 1
-        
+            
         return dfs(root)
-        
-        
