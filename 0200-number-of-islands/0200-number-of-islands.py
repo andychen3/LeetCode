e1c97dc3f708eaf58a5593d80fdf45c2d1a0d436
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-05 08:19:07 -0500

    Time: 261 ms (56.81%), Space: 28 MB (10.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+        row, col = len(grid), len(grid[0])
         seen = set()
         ans = 0
         
-        def valid(r, c):
-            return 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1"
-        
         
         def dfs(x, y):
-            for dx, dy in ((0,1), (1,0), (-1,0), (0,-1)):
-                new_dx, new_dy = dx + x, dy + y
-                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                    seen.add((new_dx, new_dy))
-                    dfs(new_dx, new_dy)
+            if x >= row or x < 0 or y >= col or y < 0 or grid[x][y] == "0" or (x, y) in seen:
+                return
+            # grid[x][y] = "0"
+            seen.add((x, y))
+            dfs(x - 1, y)
+            dfs(x + 1, y)
+            dfs(x, y - 1)
+            dfs(x, y + 1)
         
-        
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == "1" and (row, col) not in seen:
-                    seen.add((row, col))
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == "1" and (r,c) not in seen:
                     ans += 1
-                    dfs(row, col)
+                    dfs(r, c)
         return ans
