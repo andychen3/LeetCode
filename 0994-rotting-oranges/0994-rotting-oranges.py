da15b895708afa24eeee9be3727d9c39672072da
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-06 09:02:09 -0500

    Time: 42 ms (96.09%), Space: 16.5 MB (84.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,29 +1,41 @@
+from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh_oranges = 0
-        rows, cols = len(grid), len(grid[0])
-        d = collections.deque()
-        time = 0
+        seen = set()
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        fresh = 0
+        queue = deque()
+        row, col = len(grid), len(grid[0])
+        minutes = -1
         
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 2:
-                    d.append((row,col))
-                elif grid[row][col] == 1:
-                    fresh_oranges += 1
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                    seen.add((r,c))
+                else:
+                    if grid[r][c] == 1:
+                        fresh += 1
         
-        while d and fresh_oranges > 0:
-            
-            for i in range(len(d)):
-                r,c = d.popleft()
-                for new_x, new_y in [(r+1,c), (r-1,c), (r,c+1), (r,c-1)]:
-                    if 0 <= new_x < rows and 0 <= new_y < cols and grid[new_x][new_y] == 1:
-                        grid[new_x][new_y] = 2
-                        fresh_oranges -= 1
-                        d.append((new_x,new_y))
-            time += 1
-                
-        if fresh_oranges > 0:
-            return -1
-        return time
+        if fresh == 0:
+            return 0
         
+        def valid(x, y):
+            return 0 <= x < row and 0 <= y < col and grid[x][y] == 1
+        
+        
+        while queue:
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+
+                for dx, dy in directions:
+                    new_dx, new_dy = dx + x, dy + y
+                    if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                        grid[new_dx][new_dy] = 2
+                        seen.add((new_dx, new_dy))
+                        fresh -= 1
+                        queue.append((new_dx, new_dy))
+            minutes += 1
+
+        
+        return minutes if fresh == 0 else -1 
