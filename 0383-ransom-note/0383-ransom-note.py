42ab9f7c67f4eceed1b47ddff23d7e1cd8a3668b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-06 16:25:07 -0500

    Time: 58 ms (58.34%), Space: 16.9 MB (58.43%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,13 +3,14 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         if len(ransomNote) > len(magazine):
             return False
-        mag_counts = Counter(magazine)
+        
+        magazine_count = Counter(magazine)
         
         for char in ransomNote:
-            if char in mag_counts:
-                mag_counts[char] -= 1
-                if mag_counts[char] == 0:
-                    del mag_counts[char]
+            if char in magazine_count:
+                magazine_count[char] -= 1
+                if magazine_count[char] == 0:
+                    del magazine_count[char]
             else:
                 return False
         return True
