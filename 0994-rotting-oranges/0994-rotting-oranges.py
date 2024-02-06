fc15ebae3529d53231e1e851d478377cacf03e1e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-06 09:04:32 -0500

    Time: 59 ms (28.72%), Space: 16.5 MB (73.01%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,18 +1,16 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        seen = set()
         directions = [(0,1), (1,0), (-1,0), (0,-1)]
         fresh = 0
         queue = deque()
         row, col = len(grid), len(grid[0])
-        minutes = -1
+        minutes = 0
         
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
                     queue.append((r, c))
-                    seen.add((r,c))
                 else:
                     if grid[r][c] == 1:
                         fresh += 1
@@ -24,15 +22,14 @@ class Solution:
             return 0 <= x < row and 0 <= y < col and grid[x][y] == 1
         
         
-        while queue:
+        while queue and fresh > 0:
             for _ in range(len(queue)):
                 x, y = queue.popleft()
 
                 for dx, dy in directions:
                     new_dx, new_dy = dx + x, dy + y
-                    if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                    if valid(new_dx, new_dy):
                         grid[new_dx][new_dy] = 2
-                        seen.add((new_dx, new_dy))
                         fresh -= 1
                         queue.append((new_dx, new_dy))
             minutes += 1
