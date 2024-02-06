f7cba40536063981cb335e73f88277211f200d1b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-06 16:26:49 -0500

    Time: 64 ms (67.88%), Space: 23.1 MB (83.64%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,7 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         dp = [0] * (n+1)
-        for i in range(1, n+1):
-            # i >> 1 is dividing by 2 and then adding i mod 2 which gives the significant bit
+        
+        for i in range(1, n + 1):
             dp[i] = dp[i >> 1] + (i & 1)
         return dp
-            
