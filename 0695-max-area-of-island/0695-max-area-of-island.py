d33b711f2b60b5d14054bdfa0b360a68952be7d8
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-07 19:35:52 -0500

    Time: 106 ms (78.15%), Space: 18.4 MB (39.00%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,28 +1,36 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        def valid(r, c):
-            return 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        seen = set()
-        directions = ((0,1), (1,0), (-1,0), (0,-1))
+        row, col = len(grid), len(grid[0])
+        direction = [(0,1), (1,0), (-1,0), (0,-1)]
         ans = 0
+        seen = set()
         
-        def dfs(x, y):
-            ans = 0
-            for dx, dy in directions:
-                new_dx, new_dy = dx + x, dy + y
-                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                    seen.add((new_dx, new_dy))
-                    ans += dfs(new_dx, new_dy) + 1
-            return ans
+        # def valid(x, y):
+        #     return 0 <= x < row and 0 <= y < col and grid[x][y] == 1
+        
+        def dfs(r, c):
+            if r < 0 or r >= row or c < 0 or c >= col or grid[r][c] == 0 or (r,c) in seen:
+                return 0
+            
+            seen.add((r,c))
+            area = 1
+        
+            area += dfs(r-1, c) 
+            area += dfs(r+1, c)
+            area += dfs(r, c-1)
+            area += dfs(r, c+1)
+            return area
+    
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 1:
+                    ans = max(ans, dfs(r,c))
+        return ans
+        
+            
+            
+            
+            
+            
             
         
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1 and (r,c) not in seen:
-                    seen.add((r,c))
-                    ans = max(ans, dfs(r,c)+1)
-                    
-        return ans
