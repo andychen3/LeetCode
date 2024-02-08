048acaaa58a6e4b0be02706aaa68ca42f15e8624
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-08 16:38:19 -0500

    Time: 49 ms (80.56%), Space: 16.6 MB (83.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,21 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # The reason why we pass a start variable is because we can reuse the same element multiple times
-        def backtrack(start, curr_sum, arr):
-            if curr_sum == target:
+        def backtrack(arr, total, index):
+            if total == target:
                 ans.append(arr[:])
                 return
         
-            for index in range(start, len(candidates)):
-                # We have this check here because we don't want to add any elements if we already
-                # know it will get greater than target
-                if curr_sum + candidates[index] <= target:
-                    arr.append(candidates[index])
-                    # The reason why we don't increment index is because we want to reuse the same element
-                    # The only thing that moves our recursion is increasing curr_sum
-                    backtrack(index, curr_sum + candidates[index], arr)
+            for i in range(index, len(candidates)):
+                num = candidates[i]
+                if total + num <= target:
+                    arr.append(num)
+                    backtrack(arr, total + num, i)
                     arr.pop()
-
+        
         ans = []
-        backtrack(0, 0, [])
+        backtrack([], 0, 0)
         return ans
