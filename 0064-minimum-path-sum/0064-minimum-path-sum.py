7b8eb5a0249d401ab3e0043d406fca5d295d7d62
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-08 15:56:55 -0500

    Time: 107 ms (19.45%), Space: 22.5 MB (10.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,32 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        dp = [[0] * cols for _ in range(rows)]
-        dp[0][0] = grid[0][0]
-        
-        for row in range(rows):
-            for col in range(cols):
-                if row + col == 0:
-                    continue
-                ans = float("inf")
-                if row > 0:
-                    ans = min(ans, dp[row - 1][col])
-                if col > 0:
-                    ans = min(ans, dp[row][col - 1])
-                dp[row][col] = grid[row][col] + ans
-        return dp[rows - 1][cols - 1]
-        
-        
-#         @cache
-#         def dp(row, col):
-#             if row + col == 0:
-#                 return grid[row][col]
+        @cache
+        def dp(row, col):
+            if row + col == 0:
+                return grid[row][col]
+            ans = float("inf")
+            if row > 0:
+                ans = min(ans, dp(row - 1, col))
+            if col > 0:
+                ans = min(ans, dp(row, col - 1))
             
-#             ans = float("inf")
-#             if row > 0:
-#                 ans = min(ans, dp(row - 1, col))
-#             if col > 0:
-#                 ans = min(ans, dp(row, col - 1))
-#             return grid[row][col] + ans
-#         return dp(len(grid) - 1, len(grid[0]) - 1)
+            return grid[row][col] + ans
             
+        m, n = len(grid), len(grid[0])
+        return dp(m - 1, n - 1)
