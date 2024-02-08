61c4a35a1a29992345ce986f7954870fcd106cb1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-08 16:59:41 -0500

    Time: 43 ms (74.30%), Space: 17 MB (53.98%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,5 +1,8 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
+        
         @cache
         def dp(row, col):
             if obstacleGrid[row][col] == 1:
@@ -7,28 +10,11 @@ class Solution:
             if row + col == 0:
                 return 1
             
-            ans = 0
-            if row > 0 :
-                ans += dp(row - 1,col)
+            ways = 0
+            if row > 0:
+                ways += dp(row-1, col)
             if col > 0:
-                ans += dp(row, col- 1)
-            return ans
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        return dp(m - 1, n - 1)
-#         if obstacleGrid[0][0] == 1:
-#             return 0
-        
-#         row, col = len(obstacleGrid), len(obstacleGrid[0])
-#         dp = [[0] * col for _ in range(row)]
-#         dp[0][0] = 1
-        
-#         for r in range(row):
-#             for c in range(col):
-#                 if obstacleGrid[r][c] == 1:
-#                     continue
-#                 if r > 0:
-#                     dp[r][c] += dp[r-1][c]
-#                 if c > 0:
-#                     dp[r][c] += dp[r][c-1]
-#         return dp[row - 1][col -1]
-        
+                ways += dp(row,col - 1)
+            return ways
+        m, n =len(obstacleGrid), len(obstacleGrid[0])
+        return dp(m-1,n-1)
