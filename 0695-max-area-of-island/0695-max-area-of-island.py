177dab99de4565016e1c0859ded589e620de0e8b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-07 19:37:54 -0500

    Time: 116 ms (54.67%), Space: 18.2 MB (42.24%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,21 +5,12 @@ class Solution:
         ans = 0
         seen = set()
         
-        # def valid(x, y):
-        #     return 0 <= x < row and 0 <= y < col and grid[x][y] == 1
-        
         def dfs(r, c):
             if r < 0 or r >= row or c < 0 or c >= col or grid[r][c] == 0 or (r,c) in seen:
                 return 0
             
             seen.add((r,c))
-            area = 1
-        
-            area += dfs(r-1, c) 
-            area += dfs(r+1, c)
-            area += dfs(r, c-1)
-            area += dfs(r, c+1)
-            return area
+            return 1 + dfs(r-1, c) + dfs(r+1, c) + dfs(r, c-1) + dfs(r, c+1)
     
         for r in range(row):
             for c in range(col):
