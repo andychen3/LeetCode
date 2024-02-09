f8211aefcc3b4b1338e8f536747c169a9d767768
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 09:21:04 -0500

    Time: 98 ms (40.97%), Space: 16.7 MB (60.34%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # MY assumptions are that i need row, col, and row // 3 and col //3 which will give me the box
         row_set = set()
         col_set = set()
         box_set = set()
-        row, col = len(board), len(board[0])
-
-        for r in range(row):
-            for c in range(col):
-                if board[r][c] != '.':
-                    if (board[r][c], r) in row_set:
-                        return False
-                    if (board[r][c], c) in col_set:
-                        return False
-                    if (board[r][c], r//3, c//3) in box_set:
+        n = len(board)
+        
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] != ".":
+                    if (r, board[r][c]) in row_set:
                         return False
                     
-                    row_set.add((board[r][c], r))
-                    col_set.add((board[r][c], c))
-                    box_set.add((board[r][c], r//3, c//3))
+                    if (c, board[r][c]) in col_set:
+                        return False
+                    
+                    if (r//3, c//3, board[r][c]) in box_set:
+                        return False
+                    
+                row_set.add((r, board[r][c]))
+                col_set.add((c, board[r][c]))
+                box_set.add((r//3, c//3, board[r][c]))
         return True
+        
