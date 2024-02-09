4a7cb5471a2f58bdda36343cb8f2cd41cee0e7f0
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 16:35:52 -0500

    Time: 511 ms (96.30%), Space: 20.6 MB (79.86%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,21 +6,19 @@ class Solution:
         for i, num in enumerate(nums):
             if num > 0:
                 break
-                
-            if i > 0 and nums[i - 1] == num:
+            if i > 0 and nums[i - 1] == nums[i]:
                 continue
-                
             left, right = i + 1, len(nums) - 1
             while left < right:
-                triplet = num + nums[left] + nums[right]
-                if triplet > 0:
+                triplets = num + nums[left] + nums[right]
+                if triplets > 0:
                     right -= 1
-                elif triplet < 0:
+                elif triplets < 0:
                     left += 1
                 else:
                     ans.append([num, nums[left], nums[right]])
                     left += 1
                     right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
+                    while left < right and nums[left - 1] == nums[left]:
                         left += 1
         return ans
