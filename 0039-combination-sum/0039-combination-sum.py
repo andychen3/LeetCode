3d589583b7142e5d558048ad1e85505df1cf0712
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 16:17:02 -0500

    Time: 49 ms (80.28%), Space: 16.7 MB (67.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
         def backtrack(arr, total, index):
             if total == target:
                 ans.append(arr[:])
@@ -8,8 +7,6 @@ class Solution:
             
             for i in range(index, len(candidates)):
                 num = candidates[i]
-                if i != index and candidates[i-1] == candidates[i]:
-                    continue
                 if total + num <= target:
                     arr.append(num)
                     backtrack(arr, total + num, i)
