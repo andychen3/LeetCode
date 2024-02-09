bdd4d5fe74b04f0c34e1232f527f9521a7e7751a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 15:52:46 -0500

    Time: 567 ms (80.63%), Space: 74.3 MB (66.00%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -8,12 +8,12 @@ class TimeMap:
         self.map[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        res, values = "", self.map[key]
-        left, right = 0, len(values) - 1
+        res, value = "", self.map[key]
+        left, right = 0, len(value) - 1
         while left <= right:
             mid = (left + right) // 2
-            if values[mid][1] <= timestamp:
-                res = values[mid][0]
+            if value[mid][1] <= timestamp:
+                res = value[mid][0]
                 left = mid + 1
             else:
                 right = mid - 1
