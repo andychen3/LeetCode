4ef8ba5093aafc203fddbb292f43c0befb55e9c9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 08:42:51 -0500

    Time: 69 ms (96.03%), Space: 16.6 MB (70.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,15 @@
 from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left = max_freq = 0
-        char_map = defaultdict(int)
-        ans = 0
+        hash_map = defaultdict(int)
+        ans = max_freq = left = 0
         
         for right, char in enumerate(s):
-            char_map[char] += 1
-            max_freq = max(max_freq, char_map[char])
+            hash_map[char] += 1
+            max_freq = max(max_freq, hash_map[char])
             
             while (right - left + 1) - max_freq > k:
-                char_map[s[left]] -= 1
-                
+                hash_map[s[left]] -= 1
                 left += 1
-            
-            ans = max(ans, right - left + 1)
-        return ans
+        
+        return right - left + 1
