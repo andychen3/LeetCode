adfad5395055067042eb89cfeb459c91085a55be
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 07:06:44 -0500

    Time: 220 ms (38.25%), Space: 22.9 MB (34.42%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -4,30 +4,27 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        d = deque()
-        rows = len(rooms)
-        cols = len(rooms[0])
+        queue = deque()
+        row, col = len(rooms), len(rooms[0])
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
         seen = set()
-        directions = ((0,1), (1,0), (-1,0), (0,-1))
         
-        # collect all the gates
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(row):
+            for c in range(col):
                 if rooms[r][c] == 0:
-                    d.append((r, c, 0))
+                    queue.append((r, c, 1))
                     seen.add((r,c))
-        
-        def valid(r,c):
-            return 0 <= r < rows and 0 <= c < cols and rooms[r][c] != -1
-        # Go through deque and expand out
-        
-        while d:
-            x, y, steps = d.popleft()
+            
+        def valid(r, c):
+            return 0 <= r < row and 0 <= c < col and rooms[r][c] != -1
+            
+        while queue:
+            x, y, steps = queue.popleft()
+            
             for dx, dy in directions:
                 new_dx, new_dy = dx + x, dy + y
                 if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                    rooms[new_dx][new_dy] = min(steps, rooms[new_dx][new_dy])
                     seen.add((new_dx, new_dy))
-                    min_val = min(rooms[new_dx][new_dy], steps+1)
-                    rooms[new_dx][new_dy] = min_val
-                    d.append((new_dx, new_dy, steps + 1))
+                    queue.append((new_dx, new_dy, steps + 1))
         return rooms
