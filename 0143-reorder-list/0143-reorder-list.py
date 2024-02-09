8d913fe3154cc14a735c419956a2ce467408b5bc
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 07:40:25 -0500

    Time: 60 ms (49.58%), Space: 24.5 MB (95.44%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,30 +8,28 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return
-        
         slow = fast = head
         
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        curr = slow
+            
         prev = None
+        curr = slow
         
         while curr:
-            curr.next, curr, prev = prev, curr.next, curr
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
         
-        # merge the list
-          
-        first_ptr = head
-        second_ptr = prev
+        curr = head
         
-        while second_ptr.next:
-            first_ptr.next, first_ptr = second_ptr, first_ptr.next
-            second_ptr.next, second_ptr = first_ptr, second_ptr.next
-        
+        while prev.next:
+            temp1 = curr.next
+            temp2 = prev.next
+            curr.next = prev
+            prev.next = temp1
+            curr = temp1
+            prev = temp2
         return head
-        
-            
