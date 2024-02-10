83b36d59542eacf0a8bc3aa6d1ddad408d8fa0d6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-10 17:21:45 -0500

    Time: 51 ms (43.20%), Space: 16.6 MB (76.18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,12 +2,12 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         
-        n = len(s) 
         i = 0
+        n = len(s)
         ans = 0
         
         while i < n:
-            if i + 1 < n and numerals[s[i]] < numerals[s[i+1]]:
+            if i < n - 1 and numerals[s[i]] < numerals[s[i+1]]:
                 ans += numerals[s[i+1]] - numerals[s[i]]
                 i += 2
             else:
