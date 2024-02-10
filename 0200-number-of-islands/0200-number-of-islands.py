fdd7a53074a45964335e52a69c72a82bc56327a9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-09 20:08:31 -0500

    Time: 222 ms (94.28%), Space: 18.9 MB (90.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,17 +5,20 @@ class Solution:
         
         
         def dfs(x, y):
-            if x >= row or x < 0 or y >= col or y < 0 or grid[x][y] == "0":
+            if x < 0 or x >= row or y < 0 or y >= col or grid[x][y] != "1":
                 return
             grid[x][y] = "0"
-            dfs(x - 1, y)
-            dfs(x + 1, y)
-            dfs(x, y - 1)
-            dfs(x, y + 1)
+            
+            dfs(x+1, y)
+            dfs(x-1, y)
+            dfs(x, y+1)
+            dfs(x, y-1)
         
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == "1":
                     ans += 1
                     dfs(r, c)
-        return ans
+        return ans            
+        
+        
