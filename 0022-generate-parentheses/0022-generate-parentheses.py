5407889fdd7230a6842fc2a79cacd5e20e96f484
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-10 20:30:29 -0500

    Time: 27 ms (98.25%), Space: 16.8 MB (68.28%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,26 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def backtrack(left_count, right_count, arr):
-            # We have 2 * n because we want a combination of "(" and ")" so there is 2n.
-            if len(arr) == 2 * n:
-                # We have to join the parantheses into one string
-                ans.append("".join(arr[:])) 
+        def backtrack(stack, opening, closing):
+            if len(stack) == 2 * n:
+                ans.append("".join(stack))
                 return
             
-            # Check for valid opening parentheses up until n
-            if left_count < n:
-                arr.append("(")
-                backtrack(left_count + 1, right_count, arr)
-                arr.pop()
-            
-            # Add closing parantheses only up to the amount of opening parantheses else
-            # it won't be valid
-            if right_count < left_count:
-                arr.append(")")
-                backtrack(left_count, right_count + 1, arr)
-                arr.pop()
+            if opening < n:
+                stack.append("(")
+                backtrack(stack, opening + 1, closing)
+                stack.pop()
+            if closing < opening:
+                stack.append(")")
+                backtrack(stack, opening, closing + 1)
+                stack.pop()
             
         ans = []
-        
-        backtrack(0, 0, [])
+        backtrack([], 0, 0)
         return ans
