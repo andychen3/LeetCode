99b5a0bbabd2b669a73014efcfa08c615108fbe3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-10 19:21:25 -0500

    Time: 43 ms (94.33%), Space: 16.6 MB (76.50%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,26 +5,26 @@ class Solution:
         row, col = len(grid), len(grid[0])
         fresh = 0
         minutes = 0
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
         
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
-                    queue.append((r,c))
+                    queue.append((r, c, 1))
                 elif grid[r][c] == 1:
                     fresh += 1
-        
+
         def valid(x, y):
             return 0 <= x < row and 0 <= y < col and grid[x][y] == 1
         
         while queue and fresh > 0:
             for _ in range(len(queue)):
-                x, y = queue.popleft()
-                for dx, dy in directions:
-                    new_dx, new_dy = dx + x , dy + y
+                x, y, steps = queue.popleft()
+                for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
+                    new_dx, new_dy = dx + x, dy + y
                     if valid(new_dx, new_dy):
                         grid[new_dx][new_dy] = 2
                         fresh -= 1
-                        queue.append((new_dx, new_dy))
+                        queue.append((new_dx, new_dy, steps + 1))
             minutes += 1
         return minutes if fresh == 0 else -1
+                
