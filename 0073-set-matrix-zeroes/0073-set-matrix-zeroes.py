c3c375e6852e218950ec8a297db0e6843558cdd1
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-12 11:22:09 -0500

    Time: 106 ms (52.95%), Space: 17.5 MB (62.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,33 +1,30 @@
-from collections import deque
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        queue = deque()
-        seen = set()
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        for r in range(rows):
-            for c in range(cols):
+        row, col = len(matrix), len(matrix[0])
+        first_row_zero = False
+        
+        for r in range(row):
+            for c in range(col):
                 if matrix[r][c] == 0:
-                    queue.append((r,c))
-                    seen.add((r,c))
+                    matrix[0][c] = 0
+                
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        first_row_zero = True
         
+        for r in range(1, row):
+            for c in range(1, col):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
         
-
-        while queue:
-            x, y = queue.popleft()
-
-            # Change all rows into zero
-            for i in range(rows):
-                matrix[i][y] = 0
-                seen.add((i, y))
-            
-            # Change all cols into zero:
-            for i in range(cols):
-                matrix[x][i] = 0
-                seen.add((x, i))
-        return matrix
-
+        if matrix[0][0] == 0:
+            for r in range(row):
+                matrix[r][0] = 0
+        
+        if first_row_zero:
+            for c in range(col):
+                matrix[0][c] = 0
