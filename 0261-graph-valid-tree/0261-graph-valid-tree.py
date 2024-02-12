47da4684d22eecca7bbb95723cd6406fc51f49bc
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-12 12:50:03 -0500

    Time: 90 ms (50.39%), Space: 18.5 MB (70.60%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,22 +1,24 @@
 from collections import defaultdict
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n-1 != len(edges):
+        if len(edges) != n - 1:
             return False
         
         graph = defaultdict(list)
+        
         for x, y in edges:
             graph[x].append(y)
             graph[y].append(x)
             
-            
         def dfs(node):
+            if node in seen:
+                return False
+            seen.add(node)
+            
             for neighbor in graph[node]:
-                if neighbor not in seen:
-                    seen.add(neighbor)
-                    dfs(neighbor)
+                dfs(neighbor)
+                    
+        seen = set()
         
-        seen = {0}
         dfs(0)
         return len(seen) == n
-        
