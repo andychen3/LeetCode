510c19d1fc56c5715d0e4e2e597699c1937ba232
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-13 16:00:00 -0500

    Time: 35 ms (61.80%), Space: 16.5 MB (68.07%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,9 +1,13 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        carry = 0
         mask = 0xffffffff
-        while b & mask != 0:
-            carry = (a & b) << 1
-            a ^= b
-            b = carry
-        return a & mask if b > mask else a
+        
+        while b != 0:
+            temp = (a&b) << 1 # finds the carry
+            a = (a^b) & mask # a^b is the addition without carry and we AND to mask to prevetn overflow
+            b = temp & mask # same here we AND it to prevent overflow
+            
+        if a > mask // 2:
+            return ~(a^mask)
+        else:
+            return a
