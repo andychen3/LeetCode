b691e2ea47e1d591c0ed73f4f2529c812a998677
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-13 17:23:05 -0500

    Time: 48 ms (27.65%), Space: 16.5 MB (97.02%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,20 +1,25 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # False because we are trying to return true or false
         dp = [False] * len(s)
-
+        
         for i in range(len(s)):
             for word in wordDict:
-                # We first chceck if length of s is greater than a length of a word
-                # WE have the condition to check if i is equal to length of a word in wordDict
-                # This is to check for if the first word is built from 0 to i and there is no previous word
-                # because dp[i-len(word)] would be negative and that is out of bounds
-                # After we have found a word dp[i-len(word)] would check if the previous word was true
-                if i >= len(word) - 1 and (i == len(word) - 1 or dp[i - len(word)]):
-                    if s[i - len(word) + 1 : i + 1] == word:
+                if i < len(word) - 1:
+                    continue
+                    
+                if i == len(word) - 1 or dp[i - len(word)]:
+                    if s[i - len(word) + 1: i + 1] == word:
                         dp[i] = True
                         break
-
         return dp[-1]
-
         
+#         @cache
+#         def dp(i):
+#             if i < 0 :
+#                 return True
+            
+#             for word in wordDict:
+#                 if s[i - len(word) + 1: i + 1] == word and dp(i - len(word)):
+#                     return True
+#             return False
+#         return dp(len(s)-1)
