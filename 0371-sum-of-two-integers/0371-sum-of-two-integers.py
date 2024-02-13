fcb05c0976571b091e4f6236577f15271d99c463
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-13 16:04:41 -0500

    Time: 41 ms (25.82%), Space: 16.6 MB (68.07%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -3,11 +3,10 @@ class Solution:
         mask = 0xffffffff
         
         while b != 0:
-            temp = (a&b) << 1 # finds the carry
-            a = (a^b) & mask # a^b is the addition without carry and we AND to mask to prevetn overflow
-            b = temp & mask # same here we AND it to prevent overflow
-            
+            temp = (a &b) << 1
+            a = (a ^b) & mask
+            b = temp & mask
+        
         if a > mask // 2:
             return ~(a^mask)
-        else:
-            return a
+        return a
