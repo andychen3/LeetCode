1fee76fb76f7e2a98f5c74021313012a4c0c51e6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-15 15:42:12 -0500

    Time: 49 ms (93.85%), Space: 18.1 MB (93.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,25 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import defaultdict
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         def build(left, right):
-            nonlocal preorder_idx
+            nonlocal pre_idx
             if left > right:
                 return None
             
-            root = TreeNode(preorder[preorder_idx])
+            root = TreeNode(preorder[pre_idx])
             
-            preorder_idx += 1
+            pre_idx += 1
             
             root.left = build(left, inorder_map[root.val] - 1)
-            root.right = build(inorder_map[root.val]+1, right)
+            root.right = build(inorder_map[root.val] + 1, right)
             return root
         
-        preorder_idx = 0
-        inorder_map = defaultdict(int)
+        pre_idx = 0
+        inorder_map = {}
         for idx, node in enumerate(inorder):
             inorder_map[node] = idx
-        
         return build(0, len(preorder) - 1)
