517a53e130b83998235c2941d98e4afa7a145aa0
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-16 14:50:01 -0500

    Time: 551 ms (27.28%), Space: 29.6 MB (79.97%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,10 +4,9 @@ class Solution:
         ans = 0
         
         while left < right:
-            min_height = min(height[left], height[right])
-            ans = max(ans, min_height*(right-left))
+            ans = max(ans, min(height[left], height[right]) * (right - left))
             
-            if height[left] <= height[right]:
+            if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
