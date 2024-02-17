dc34943f8402f1f90eea0b888b28b957707b6e0a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-17 16:34:39 -0500

    Time: 45 ms (67.12%), Space: 17.2 MB (54.80%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,19 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix), len(matrix[0])
+        n, m = len(matrix), len(matrix[0])
         left, right = 0, (m * n) - 1
         
         while left <= right:
             mid = (left + right) // 2
-            row = mid // n # n is the column
-            col = mid % n
+            row = mid // m 
+            col = mid % m
             num = matrix[row][col]
             
             if num == target:
                 return True
             
-            if num > target:
+            elif num > target:
                 right = mid - 1
             else:
                 left = mid + 1
         return False
+                
