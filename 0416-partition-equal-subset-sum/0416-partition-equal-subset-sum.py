4f2d85938674916fcd9a92da25568b2d8d6e913c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-18 09:30:30 -0500

    Time: 1666 ms (25.60%), Space: 32.7 MB (42.68%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,18 +1,35 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        
-        if total % 2 == 1:
+        if total % 2:
             return False
+        n = len(nums)
+        curr_sum = total // 2
+        dp = [[False] * (n + 1) for _ in range(curr_sum + 1)]
+        dp[curr_sum][n] = True
         
-        @cache
-        def dp(remain, i):
-            if remain == curr_sum:
-                return True
-            if i == len(nums) or remain > curr_sum:
-                return False
+        
+        for remain in range(curr_sum - 1, -1, -1):
+            for i in range(n - 1, -1, -1):
+                dp[remain][i] = dp[remain][i+1]
+                if remain + nums[i] <= curr_sum:
+                    dp[remain][i] = dp[remain][i] or dp[remain + nums[i]][i+1]
+        return dp[0][0]
+        
+        
+#         total = sum(nums)
+        
+#         if total % 2 == 1:
+#             return False
+        
+#         @cache
+#         def dp(remain, i):
+#             if remain == curr_sum:
+#                 return True
+#             if i == len(nums) or remain > curr_sum:
+#                 return False
             
-            return dp(remain, i+1) or dp(remain + nums[i], i + 1)
-        curr_sum = total // 2                                
-        return dp(0, 0)
+#             return dp(remain, i+1) or dp(remain + nums[i], i + 1)
+#         curr_sum = total // 2                                
+#         return dp(0, 0)
             
