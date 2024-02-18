09513f795bdd0ceea868aa9f4259d0f2685f0f19
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-18 09:40:12 -0500

    Time: 508 ms (73.61%), Space: 16.7 MB (93.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,35 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        if total % 2:
+                # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
             return False
-        n = len(nums)
-        curr_sum = total // 2
-        dp = [[False] * (n + 1) for _ in range(curr_sum + 1)]
-        dp[curr_sum][n] = True
-        
-        
-        for remain in range(curr_sum - 1, -1, -1):
-            for i in range(n - 1, -1, -1):
-                dp[remain][i] = dp[remain][i+1]
-                if remain + nums[i] <= curr_sum:
-                    dp[remain][i] = dp[remain][i] or dp[remain + nums[i]][i+1]
-        return dp[0][0]
-        
-        
-#         total = sum(nums)
-        
-#         if total % 2 == 1:
-#             return False
-        
-#         @cache
-#         def dp(remain, i):
-#             if remain == curr_sum:
-#                 return True
-#             if i == len(nums) or remain > curr_sum:
-#                 return False
-            
-#             return dp(remain, i+1) or dp(remain + nums[i], i + 1)
-#         curr_sum = total // 2                                
-#         return dp(0, 0)
-            
+        subset_sum = total_sum // 2
+
+        # construct a dp table of size (subset_sum + 1)
+        dp = [False] * (subset_sum + 1)
+        dp[0] = True
+        for curr in nums:
+            for j in range(subset_sum, curr - 1, -1):
+                dp[j] = dp[j] or dp[j - curr]
+
+        return dp[subset_sum]
