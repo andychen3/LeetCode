08849fac99192b13b72e0338aed8fd8327b3f476
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-18 08:45:12 -0500

    Time: 734 ms (13.92%), Space: 291.9 MB (5.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+#         n = len(coins)
+#         dp = [[0] * (amount + 1) for _ in range(n + 1)]
         
-        for i in range(n+1):
-            dp[i][0] = 1
+#         for i in range(n+1):
+#             dp[i][0] = 1
             
-        for i in range(n - 1, -1, -1):
-            for remain in range(amount + 1):
-                if remain - coins[i] >= 0:
-                    dp[i][remain] = dp[i][remain-coins[i]] + dp[i+1][remain]
-                else:
-                    dp[i][remain] = dp[i+1][remain]
-        return dp[0][amount]
+#         for i in range(n - 1, -1, -1):
+#             for remain in range(amount + 1):
+#                 if remain - coins[i] >= 0:
+#                     dp[i][remain] = dp[i][remain-coins[i]] + dp[i+1][remain]
+#                 else:
+#                     dp[i][remain] = dp[i+1][remain]
+#         return dp[0][amount]
         
         
-#         @cache
-#         def dp(i, remaining):
-#             if i == len(coins):
-#                 return 0
-#             if remaining == 0:
-#                 return 1
-#             if remaining < 0:
-#                 return 0
+        @cache
+        def dp(remaining, i):
+            if i == len(coins) or remaining < 0:
+                return 0
+            if remaining == 0:
+                return 1
+            return dp(remaining - coins[i], i) + dp(remaining, i + 1)
+        return dp(amount, 0)
             
-#             return dp(i + 1, remaining) + dp(i, remaining - coins[i])
-            
-#         return dp(0, amount)
