d7278c36f8d9b3b7a0f4505575aef6213a50d716
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-19 08:53:53 -0500

    Time: 293 ms (75.91%), Space: 17.6 MB (90.06%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,17 +1,21 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         row, col = len(matrix), len(matrix[0])
+        memo = [[0] * col for _ in range(row)]
         
-        @cache
         def dp(r, c, pre):
             if r < 0 or r >= row or c < 0 or c >= col or matrix[r][c] <= pre:
                 return 0
+            
+            if memo[r][c]:
+                return memo[r][c]
             
             ans = 1
             ans = max(ans, 1 + dp(r + 1, c, matrix[r][c]))
             ans = max(ans, 1 + dp(r , c - 1, matrix[r][c]))
             ans = max(ans, 1 + dp(r - 1, c, matrix[r][c]))
             ans = max(ans, 1 + dp(r , c + 1, matrix[r][c]))
+            memo[r][c] = ans
             return ans
         
         ans = 0
@@ -19,4 +23,24 @@ class Solution:
             for y in range(col):
                 ans = max(ans, dp(x, y, -1)) 
         return ans
+        
+#         row, col = len(matrix), len(matrix[0])
+        
+#         @cache
+#         def dp(r, c, pre):
+#             if r < 0 or r >= row or c < 0 or c >= col or matrix[r][c] <= pre:
+#                 return 0
+            
+#             ans = 1
+#             ans = max(ans, 1 + dp(r + 1, c, matrix[r][c]))
+#             ans = max(ans, 1 + dp(r , c - 1, matrix[r][c]))
+#             ans = max(ans, 1 + dp(r - 1, c, matrix[r][c]))
+#             ans = max(ans, 1 + dp(r , c + 1, matrix[r][c]))
+#             return ans
+        
+#         ans = 0
+#         for x in range(row):
+#             for y in range(col):
+#                 ans = max(ans, dp(x, y, -1)) 
+#         return ans
         
