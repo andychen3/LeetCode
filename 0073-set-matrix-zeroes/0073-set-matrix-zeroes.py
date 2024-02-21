aafec6392e003551f0444fde004d66be254ae085
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-21 12:11:51 -0500

    Time: 106 ms (46.29%), Space: 17.5 MB (88.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -10,15 +10,17 @@ class Solution:
             for c in range(col):
                 if matrix[r][c] == 0:
                     matrix[0][c] = 0
-                
+                    
                     if r > 0:
                         matrix[r][0] = 0
                     else:
                         first_row_zero = True
+                    
         for r in range(1, row):
             for c in range(1, col):
                 if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
+                    
                     
         if matrix[0][0] == 0:
             for r in range(row):
@@ -27,5 +29,3 @@ class Solution:
         if first_row_zero:
             for c in range(col):
                 matrix[0][c] = 0
-                        
-                
