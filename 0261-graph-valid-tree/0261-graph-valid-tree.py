ca30d9f67b8a418a20dc82cdc92fc1d56098caab
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-21 16:24:03 -0500

    Time: 93 ms (31.75%), Space: 18.3 MB (89.33%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,38 +1,40 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = [node for node in range(n)]
-        self.rank = [1] * n
-        
-    def find(self, node):
-        while node != self.parent[node]:
-            self.parent[node] = self.parent[self.parent[node]]
-            node = self.parent[node]
-        return node
-    
-    def union(self, node1, node2):
-        root1 = self.find(node1)
-        root2 = self.find(node2)
-        
-        if root1 == root2:
-            return False
-        
-        if self.rank[root1] < self.rank[root2]:
-            self.parent[root1] = root2
-            self.rank[root2] += self.rank[root1]
-        else:
-            self.parent[root2] = root1
-            self.rank[root1] += self.rank[root2] 
-    
-        return True
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n-1:
+        if len(edges) != n - 1:
             return False
         
-        unionFind = UnionFind(n)
+        parent = [node for node in range(n)]
+        rank = [1] * n
+        
+        def find(node):
+            while node != parent[node]:
+                parent[node] = parent[parent[node]]
+                node = parent[node]
+            return node
+            
+        def union(node1, node2):
+            root1, root2 = find(node1), find(node2)
+            
+            if root1 == root2:
+                return False
+            
+            if rank[root1] < rank[root2]:
+                parent[root1] = root2
+                rank[root2] += rank[root1]
+            else:
+                parent[root2] = root1
+                rank[root1] += rank[root2]
+            
+            return True
+            
         
         for x, y in edges:
-            if not unionFind.union(x, y):
+            if not union(x, y):
                 return False
+            
         return True
+            
+            
+            
+        
+        
