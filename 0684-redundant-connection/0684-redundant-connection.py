3355bff12ea9cf47523f2ab6c1bfe410351dc9a5
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-21 16:44:47 -0500

    Time: 59 ms (44.14%), Space: 17.1 MB (75.18%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,16 +1,29 @@
-class Solution(object):
-    def findRedundantConnection(self, edges):
-        graph = collections.defaultdict(set)
-
-        def dfs(source, target):
-            if source not in seen:
-                seen.add(source)
-                if source == target: return True
-                return any(dfs(nei, target) for nei in graph[source])
-
-        for u, v in edges:
-            seen = set()
-            if u in graph and v in graph and dfs(u, v):
-                return u, v
-            graph[u].add(v)
-            graph[v].add(u)
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        parent = [node for node in range(n + 1)]
+        rank = [1] * (n + 1)
+        
+        def find(node):
+            while node != parent[node]:
+                parent[node] = parent[parent[node]]
+                node = parent[node]
+            return node
+        
+        def union(node1, node2):
+            root1, root2 = find(node1), find(node2)
+            
+            if root1 == root2:
+                return False
+            
+            if rank[root1] > rank[root2]:
+                parent[root2] = root1
+                rank[root1] += rank[root2]
+            else:
+                parent[root1] = root2
+                rank[root2] += rank[root1]
+            return True
+            
+        for x, y in edges:
+            if not union(x, y):
+                return[x,y]
