6c82b50ee73c17698956d9ec6ac188555a5c61ea
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-27 17:36:45 -0500

    Time: 123 ms (78.43%), Space: 18.1 MB (56.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero_ptr = 0
+        zeros = 0
         
         for i, num in enumerate(nums):
             if num != 0:
-                nums[i], nums[zero_ptr] = nums[zero_ptr], nums[i]
-                zero_ptr += 1
+                nums[i], nums[zeros] = nums[zeros], nums[i]
+                zeros += 1
         return nums
