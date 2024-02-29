92f375922f8d2fd84455b9912289fafc646d91a4
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-02-28 21:18:04 -0500

    Time: 30 ms (88.77%), Space: 16.6 MB (47.84%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,13 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        if n < 0:
-            x = 1 / x
-            n = -n
-        
-        result = self.myPow(x, n // 2)
-        if n % 2 == 0:
-            return result * result
-        else:
-            return x * result * result
+        def helper(x, n):
+            if x == 0:
+                return 0
+            if n == 0: 
+                return 1
+            res = helper(x*x, n//2)
+            # This is to find out if the exponent is odd for the cases of 2^5 we need 2^2 * 2^2 * 2.
+            return x * res if n % 2 else res
+            
+            
+            
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1 / res
