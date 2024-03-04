3034502012ac06f54a1ac116ec0807845df69d45
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-04 07:49:20 -0500

    Time: 50 ms (36.01%), Space: 16.8 MB (44.61%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,15 +2,15 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         @cache
         def dp(i, j):
-            if j == len(p):
-                ans = i == len(s)
+            if i >= len(s) and j >= len(p):
+                return True
+            elif j >= len(p):
+                return False
             else:
-                first_match = i < len(s) and (s[i] == p[j] or p[j] == ".")
-                if j+1 < len(p) and p[j+1] == '*':
-                    ans = dp(i, j+2) or first_match and dp(i+1, j)
+                match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+                if j + 1 < len(p) and p[j + 1] == "*":
+                    return dp(i, j + 2) or match and dp(i + 1, j)
                 else:
-                    ans = first_match and dp(i+1, j+1)
-
-            return ans
-
-        return dp(0, 0)
+                    return match and dp(i+1, j + 1)
+            return False
+        return dp(0,0)
