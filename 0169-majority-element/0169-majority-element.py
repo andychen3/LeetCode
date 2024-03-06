c5bdca629823418cea3d0d9f67ac4c236c2eb8bf
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-06 13:32:58 -0500

    Time: 169 ms (18.26%), Space: 18.1 MB (43.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,10 +4,9 @@ class Solution:
         counts = 0
         
         for num in nums:
-            if majority != num and counts == 0:
+            if counts == 0:
                 majority = num
-                counts = 0
-            elif num != majority:
+            if num != majority:
                 counts -= 1
             else:
                 counts += 1
