11d8d776977204d94d552924652dedae3647e9e5
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-06 17:36:48 -0500

    Time: 38 ms (98.05%), Space: 16.5 MB (73.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,12 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        string = str(x)
-        left, right = 0, len(string) -1
+        string = [s for s in str(x)]
+        left, right = 0, len(string) - 1
         
-        while left <= right:
-            if string[left] != string[right]:
+        while left < right:
+            left_char = string[left]
+            right_char = string[right]
+            if left_char != right_char:
                 return False
-            if string[left] == string[right]:
-                left += 1
-                right -= 1
+            left += 1
+            right -= 1
         return True
