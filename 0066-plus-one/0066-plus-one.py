c5d9fcba7d9669c63e19034586c6910fe9705f08
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-06 17:09:07 -0500

    Time: 40 ms (31.74%), Space: 16.4 MB (87.55%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,7 +2,6 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for i in range(len(digits)-1, -1, -1):
             if digits[i] == 9:
-                carry = 1
                 digits[i] = 0
             else:
                 digits[i] += 1
