4ce0b94fb50b5af1b231573ab262d7cb431c8b2d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-08 12:56:42 -0500

    Time: 48 ms (40.35%), Space: 17.2 MB (64.72%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,17 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @cache
-        def dp(i, holding):
-            if i >= len(prices):
-                return 0
-            
-            ans = dp(i+1, holding)
-            if holding:
-                ans = max(ans, prices[i] + dp(i+2, False))
-            else:
-                ans = max(ans, -prices[i] + dp(i+1, True))
-                
-            return ans
+        n = len(prices)
+        dp = [[0] * (2) for _ in range(n+1)]
         
-        return dp(0, False)
+        for i in range(n-1, -1, -1):
+            for holding in range(2):
+                dp[i][holding] = dp[i+1][holding]
+                if holding:
+                    dp[i][holding] = max(dp[i][holding], prices[i] + dp[min(i + 2, n)][False])
+                else:
+                    dp[i][holding] = max(dp[i][holding], -prices[i] + dp[i + 1][True])
+        return dp[0][False]
+        
+#         @cache
+#         def dp(i, holding):
+#             if i >= len(prices):
+#                 return 0
+            
+#             ans = dp(i+1, holding)
+#             if holding:
+#                 ans = max(ans, prices[i] + dp(i+2, False))
+#             else:
+#                 ans = max(ans, -prices[i] + dp(i+1, True))
+                
+#             return ans
+        
+#         return dp(0, False)
         
