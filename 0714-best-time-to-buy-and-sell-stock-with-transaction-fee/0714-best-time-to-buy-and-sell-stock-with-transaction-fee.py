6f968f302a79fe8853adf0fcba7c86ae912919e2
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-08 13:03:09 -0500

    Time: 981 ms (12.48%), Space: 231.7 MB (7.68%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,27 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        n = len(prices)
-        dp = [[0,0] for _ in range(n+1)]
-        
-        for i in range(n-1, -1, -1):
-            for holding in range(2):
-                ans = dp[i+1][holding]
-                if holding:
-                    ans = max(ans, prices[i] + dp[i+1][0] - fee)
-                else:
-                    ans = max(ans, -prices[i] + dp[i+1][1])
-                dp[i][holding] = ans
-        return dp[0][0]
-        
-#         @cache
-#         def dp(i, holding):
-#             if i >= len(prices):
-#                 return 0
+        @cache
+        def dp(i, holding):
+            if i >= len(prices):
+                return 0
             
-#             ans = dp(i+1, holding)
-#             if holding:
-#                 ans = max(ans, prices[i] + dp(i+1, False) - fee)
-#             else:
-#                 ans = max(ans, -prices[i] + dp(i+1, True))
-#             return ans
-#         return dp(0,0)
+            ans = dp(i+1, holding)
+            if holding:
+                ans = max(ans, prices[i] + dp(i+1, False) - fee)
+            else:
+                ans = max(ans, -prices[i] + dp(i+1, True))
+            return ans
+        return dp(0,False)
