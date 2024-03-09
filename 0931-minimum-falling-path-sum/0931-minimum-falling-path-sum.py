55274e811dd617b52e16a9e45617998d14882b86
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-09 08:21:42 -0500

    Time: 124 ms (51.32%), Space: 17.2 MB (99.79%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,17 +1,31 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
-        @cache
-        def dp(row, col):
-            if row < 0 or row >= n or col < 0 or col >= n:
-                return inf
+        dp = [[inf] * (n + 1) for _ in range(n +1)]
+        
+        for i in range(n):
+            dp[n-1][i] = matrix[n-1][i]
             
-            if row == n - 1:
-                return matrix[row][col]
+        for row in range(n-2, -1, -1):
+            for col in range(n):
+                dp[row][col] = matrix[row][col] + min(
+                dp[row+1][col - 1] if col - 1 >= 0 else inf,
+                dp[row+1][col],
+                   dp[row+1][col+1] if col + 1 < n else inf)
+                   
+        return min(dp[0])
+        
+#         @cache
+#         def dp(row, col):
+#             if row < 0 or row >= n or col < 0 or col >= n:
+#                 return inf
             
-            return matrix[row][col] + min(
-            dp(row + 1, col -1),
-            dp(row+1, col),
-            dp(row+1, col+1))
-        return min(dp(0, col) for col in range(n))
+#             if row == n - 1:
+#                 return matrix[row][col]
+            
+#             return matrix[row][col] + min(
+#             dp(row + 1, col -1),
+#             dp(row+1, col),
+#             dp(row+1, col+1))
+#         return min(dp(0, col) for col in range(n))
             
