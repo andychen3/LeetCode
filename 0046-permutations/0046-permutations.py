39a97517428b147426ec5e56041b62f70bd8035d
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-09 08:55:32 -0500

    Time: 44 ms (33.55%), Space: 16.8 MB (48.39%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -4,12 +4,13 @@ class Solution:
             if len(arr) == len(nums):
                 ans.append(arr[:])
                 return
-            
+        
             for num in nums:
                 if num not in arr:
                     arr.append(num)
                     backtrack(arr)
                     arr.pop()
+            
         ans = []
         backtrack([])
         return ans
