9612189e9aaeb9d5e9050eb05bc4e3aa03659e0c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-10 10:23:14 -0400

    Time: 42 ms (79.53%), Space: 16.5 MB (86.46%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -5,20 +5,19 @@ class Solution:
                 return 1
             
             result = 0
-            
             for col in range(n):
                 if col in col_set or (row - col) in diag_set or (row + col) in anti_diag_set:
                     continue
                     
                 col_set.add(col)
-                diag_set.add(row-col)
-                anti_diag_set.add(row+col)
+                diag_set.add(row - col)
+                anti_diag_set.add(row + col)
                 
                 result += backtrack(row + 1, col_set, diag_set, anti_diag_set)
-                col_set.remove(col)
-                diag_set.remove(row-col)
-                anti_diag_set.remove(row+col)
-            return result
-        
-        return backtrack(0, set(), set(), set())
                 
+                col_set.remove(col)
+                diag_set.remove(row - col)
+                anti_diag_set.remove(row + col)
+                
+            return result
+        return backtrack(0, set(), set(), set())
