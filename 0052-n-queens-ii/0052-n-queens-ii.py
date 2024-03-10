da5bfd2040ccaff3c36cd8afff8e8ca49039e460
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-10 10:08:03 -0400

    Time: 40 ms (85.45%), Space: 16.4 MB (98.93%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -2,23 +2,23 @@ class Solution:
     def totalNQueens(self, n: int) -> int:
         def backtrack(row, col_set, diag_set, anti_diag_set):
             if row == n:
-                nonlocal ans
-                ans += 1
-                return
+                return 1
+            
+            result = 0
             
             for col in range(n):
-                if col in col_set or (row + col) in diag_set or (row - col) in anti_diag_set:
+                if col in col_set or (row - col) in diag_set or (row + col) in anti_diag_set:
                     continue
                     
                 col_set.add(col)
-                diag_set.add(row+col)
-                anti_diag_set.add(row-col)
+                diag_set.add(row-col)
+                anti_diag_set.add(row+col)
                 
-                backtrack(row + 1, col_set, diag_set, anti_diag_set)
+                result += backtrack(row + 1, col_set, diag_set, anti_diag_set)
                 col_set.remove(col)
-                diag_set.remove(row+col)
-                anti_diag_set.remove(row-col)
-        ans = 0
-        backtrack(0, set(), set(), set())
-        return ans
+                diag_set.remove(row-col)
+                anti_diag_set.remove(row+col)
+            return result
+        
+        return backtrack(0, set(), set(), set())
                 
