e09223bf30aea8e279ff38a75ac72b8718a853b8
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-10 09:28:13 -0400

    Time: 175 ms (75.69%), Space: 44.1 MB (6.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,14 +2,10 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         @cache
         def dp(i, total):
-            if i == len(nums) and total == target:
+            if i >= len(nums) and total == target:
                 return 1
-            
             if i >= len(nums):
                 return 0
-        
             
-            return dp(i+1, total + nums[i]) + dp(i + 1, total - nums[i])
-            
-        
+            return dp(i+1, nums[i] + total) + dp(i+1, total - nums[i] )
         return dp(0,0)
