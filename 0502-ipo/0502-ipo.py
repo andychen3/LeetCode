3e8e7f0ec4907e2335f1f5f1fe9458c802089cdf
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-11 16:59:58 -0400

    Time: 720 ms (68.57%), Space: 41.3 MB (64.01%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,24 +1,18 @@
 import heapq
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        # What do i know about this problem?
-        # Could be a heap problem since it says at most k distinct projects
-        # Also could be greedy and use of heap since you are trying to maximize your final capital 
-        # So everytime you get a project the next project if same cost you want the highest profit.
-        choices = sorted(zip(capital, profits))
+        projects = sorted(zip(capital, profits))
+        i = 0
         heap = []
-        i = 0 # Iterate through the sorted choices
-        n = len(choices)
-
-        for _ in range(k): # Iterate up to k times
-            while i < n and choices[i][0] <= w: # Check which projects you can afford
-                heapq.heappush(heap, -choices[i][1]) # Add the projects onto the max heap
+        n = len(projects)
+        
+        for _ in range(k):
+            while i < n and projects[i][0] <= w:
+                heappush(heap, -projects[i][1])
                 i += 1
-            # If no more available projects return profits
+                
             if len(heap) == 0:
                 return w
-
-            # Pop from max heap and add to profits
-            w -= heapq.heappop(heap)
-        
+            
+            w -= heappop(heap)
         return w
