07320561f429c4a53f3e81daa4fde3ba0d05ba67
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-11 17:17:41 -0400

    Time: 247 ms (47.14%), Space: 26.8 MB (51.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,12 +1,26 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def dp(remain, i):
-            if i >= len(coins) or remain < 0:
-                return 0
+        n = len(coins)
+        dp = [[0] * (n+1) for _ in range(amount + 1)]
+        
+        for i in range(n + 1):
+            dp[0][i] = 1
             
-            if remain == 0:
-                return 1
+        for remain in range(amount + 1):
+            for i in range(n - 1, -1, -1):
+                dp[remain][i] = dp[remain][i+1]
+                if remain - coins[i] >= 0:
+                    dp[remain][i] += dp[remain - coins[i]][i]
+        return dp[amount][0]
+        
+        
+#         @cache
+#         def dp(remain, i):
+#             if i >= len(coins) or remain < 0:
+#                 return 0
             
-            return dp(remain - coins[i], i) + dp(remain, i + 1)
-        return dp(amount, 0)
+#             if remain == 0:
+#                 return 1
+            
+#             return dp(remain - coins[i], i) + dp(remain, i + 1)
+#         return dp(amount, 0)
