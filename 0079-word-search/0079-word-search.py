3d8c5db4b718872e187e3b92b28e670ca5235384
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-11 08:33:40 -0400

    Time: 5050 ms (27.93%), Space: 16.7 MB (21.12%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,11 +1,11 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row, col = len(board), len(board[0])
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         
         def valid(x, y):
             return 0 <= x < row and 0 <= y < col
-            
+        
         def backtrack(x, y, index, seen):
             if index == len(word):
                 return True
@@ -19,11 +19,9 @@ class Solution:
                             return True
                         seen.remove((new_dx, new_dy))
             return False
-            
+        
         for r in range(row):
             for c in range(col):
-                if board[r][c] == word[0] and backtrack(r, c, 1, {(r,c)}):
+                if board[r][c] == word[0] and backtrack(r, c, 1, {(r, c)}):
                     return True
         return False
-                
-                
