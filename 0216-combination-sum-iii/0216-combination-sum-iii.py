07d15616e64f010de1365f77194c56817a6b4047
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-11 09:47:18 -0400

    Time: 40 ms (30.33%), Space: 16.5 MB (81.54%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,20 +1,17 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def backtrack(arr, curr, i):
-            if len(arr) == k and curr == n:
+        def backtrack(arr, i, total):
+            if len(arr) == k and total == n:
                 ans.append(arr[:])
                 return
             
             for j in range(i, 10):
-                # This is to make sure that when i add the sum to the next number it's still
-                # within n else i skip it.
-                if curr + j <= n:
+                if total + j <= n:
                     arr.append(j)
-                    # I do j + 1 because they say that all numbers must be used at most once
-                    # By doing this i skip the previous number
-                    backtrack(arr, curr + j, j + 1)
+                    backtrack(arr, j + 1, total + j)
                     arr.pop()
-
+                
+                
         ans = []
-        backtrack([], 0, 1)
+        backtrack([], 1, 0)
         return ans
