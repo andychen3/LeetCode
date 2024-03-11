17879317ae973aca9c46ad788a33bde8fcda7946
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-11 12:22:17 -0400

    Time: 44 ms (86.96%), Space: 17.3 MB (87.46%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,16 +1,12 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # This binary search and left returns the position because left will always stop shy of
-        # where the value would be inserted
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            # We always do if because this is technically the beginning of the conditions
-            # The earlier if is to exit early if target is found
-            if nums[mid] > target:
-                right = mid - 1
-            else:
+            elif nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
         return left
