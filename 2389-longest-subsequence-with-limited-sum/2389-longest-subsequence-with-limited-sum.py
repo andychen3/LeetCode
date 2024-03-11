e04e8954c58b1380ba06d52b1015b901e9db64b4
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-11 13:53:37 -0400

    Time: 94 ms (65.75%), Space: 17 MB (53.55%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,27 +1,23 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        # def binary_search(arr, target):
-        #     left = 0
-        #     right = len(arr)-1
-        #     while left <= right:
-        #         mid = (left+right) // 2
-        #         if arr[mid] == target:
-        #             return mid + 1
-        #         if arr[mid] > target:
-        #             right = mid - 1
-        #         else:
-        #             left = mid + 1
-        #     return left
-        
-        
-        temp = sorted(nums)
-        prefix = [temp[0]]
-        ans = []
-        
-        for i in range(len(temp)-1):
-            prefix.append(prefix[-1] + temp[i+1])
-        
-        for num in queries:
-            ans.append(bisect.bisect_right(prefix, num))
+        def binary_search(arr, target):
+            left, right = 0, len(arr) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if arr[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return left
             
+            
+        nums.sort()
+        ans = []
+        prefix = [nums[0]]
+        for num in nums[1:]:
+            prefix.append(prefix[-1] + num)
+        
+        for query in queries:
+            ans.append(binary_search(prefix, query))
         return ans
+        
