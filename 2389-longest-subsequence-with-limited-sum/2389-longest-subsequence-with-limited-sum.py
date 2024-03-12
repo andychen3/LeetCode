afeb2896e6b45c7385871047843cd20f93c28be6
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-12 19:53:53 -0400

    Time: 99 ms (52.02%), Space: 16.9 MB (88.63%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -4,20 +4,20 @@ class Solution:
             left, right = 0, len(arr) - 1
             while left <= right:
                 mid = (left + right) // 2
-                if arr[mid] > target:
-                    right = mid - 1
-                else:
+                if arr[mid] <= target:
                     left = mid + 1
+                else:
+                    right = mid - 1
             return left
-            
-            
+        
         nums.sort()
-        ans = []
         prefix = [nums[0]]
         for num in nums[1:]:
             prefix.append(prefix[-1] + num)
+            
+        ans = []
         
         for query in queries:
             ans.append(binary_search(prefix, query))
         return ans
-        
+            
