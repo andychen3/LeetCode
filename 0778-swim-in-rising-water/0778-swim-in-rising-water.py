cad75552b0e43bf8aa772eda544b71eb47f0dbee
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-13 14:53:56 -0400

    Time: 84 ms (75.99%), Space: 17.3 MB (73.91%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,33 +1,20 @@
+import heapq
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        def valid(x, y):
-            return 0 <= x < len(grid) and 0 <= y < len(grid)
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        heap = [(grid[0][0], 0, 0)]
+        seen = {(0,0)}
+        n = len(grid)
         
-        def check(k):
-            directions = [(0,1), (1,0), (-1,0), (0,-1)]
-            stack = [(0,0)]
-            seen = {(0,0)}
-            n = len(grid)
+        while heap:
+            time, row, col = heappop(heap)
             
-            while stack:
-                x, y = stack.pop()
-                
-                if x == n - 1 and y == n - 1:
-                    return True
-                
-                for dx, dy in directions:
-                    new_dx, new_dy = dx + x, dy + y
-                    if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                        if k >= grid[new_dx][new_dy]:
-                            seen.add((new_dx, new_dy))
-                            stack.append((new_dx, new_dy))
-            return False
+            if row == n-1 and col == n - 1:
+                return time
             
-        left, right = grid[0][0], max(max(row) for row in grid)
-        while left <= right:
-            mid = (left + right) // 2
-            if check(mid):
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left
+            for dx, dy in directions:
+                new_dx, new_dy = dx + row, dy + col
+                if 0 <= new_dx < n and 0 <= new_dy < n and (new_dx, new_dy) not in seen:
+                    seen.add((new_dx, new_dy))
+                    heappush(heap, (max(time, grid[new_dx][new_dy]), new_dx, new_dy))
+        
