ac72f5a36fc7b38f520be454b8b06c88d88a9c82
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-15 16:10:39 -0400

    Time: 197 ms (94.85%), Space: 18.1 MB (57.98%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,23 +1,23 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pac, atl = set(), set()
         row, col = len(heights), len(heights[0])
+        atl, pac = set(), set()
         
         def dfs(x, y, seen, prev):
-            if x < 0 or x >= row or y < 0 or y >= col or heights[x][y] < prev or (x,y) in seen:
+            if x < 0 or x >= row or y < 0 or y >= col or (x, y) in seen or heights[x][y] < prev:
                 return
             seen.add((x, y))
             dfs(x+1, y, seen, heights[x][y])
             dfs(x-1, y, seen, heights[x][y])
             dfs(x, y+1, seen, heights[x][y])
             dfs(x, y-1, seen, heights[x][y])
+        
+        for i in range(col):
+            dfs(0, i, pac, -1)
+            dfs(row - 1, i, atl, -1)
             
-        for r in range(row):
-            dfs(r, 0, pac, -1)
-            dfs(r, col - 1, atl, -1)
-            
-        for c in range(col):
-            dfs(0, c, pac, -1)
-            dfs(row - 1, c, atl, -1)
-            
-        return list(pac.intersection(atl))
+        for j in range(row):
+            dfs(j, 0, pac, -1)
+            dfs(j, col - 1, atl, -1)
+        
+        return list(atl & pac)
