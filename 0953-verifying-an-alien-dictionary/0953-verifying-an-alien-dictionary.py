9fef3c522b73c13fce5fa46115f3d9637be82105
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-15 16:03:05 -0400

    Time: 38 ms (74.62%), Space: 16.8 MB (12.91%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -2,9 +2,9 @@ class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         mapping = {char: i for i, char in enumerate(order)}
         
-        for i in range(1, len(words)):
-            w1 = words[i-1]
-            w2 = words[i]
+        for i in range(len(words) - 1):
+            w1 = words[i]
+            w2 = words[i+1]
             for j in range(len(w1)):
                 if j == len(w2):
                     return False
