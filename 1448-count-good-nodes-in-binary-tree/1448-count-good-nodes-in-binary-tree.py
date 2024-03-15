fb8c672ad42c4b4f3dd0c04c3c616d6ddb904648
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-15 16:53:18 -0400

    Time: 155 ms (11.81%), Space: 31.1 MB (64.08%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -9,13 +9,10 @@ class Solution:
         def dfs(node, curr_max):
             if not node:
                 return 0
-            curr_max = max(node.val, curr_max)
-            left = dfs(node.left, curr_max)
-            right = dfs(node.right, curr_max)
+            left = dfs(node.left, max(node.val, curr_max))
+            right = dfs(node.right, max(node.val, curr_max))
             ans = left + right
             if node.val >= curr_max:
                 ans += 1
             return ans
-            
-            
         return dfs(root, root.val)
