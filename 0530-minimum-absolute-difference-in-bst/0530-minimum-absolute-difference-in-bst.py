ce61a4f09ee0efa4aaa3676602ba1f14d725f7da
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-15 21:50:41 -0400

    Time: 49 ms (48.58%), Space: 18.2 MB (73.28%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -14,5 +14,5 @@ class Solution:
         arr = inorder(root)
         ans = inf
         for i in range(1, len(arr)):
-            ans = min(ans, abs(arr[i] - arr[i-1]))
+            ans = min(ans, arr[i] - arr[i-1])
         return ans
