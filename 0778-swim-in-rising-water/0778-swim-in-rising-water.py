5a3e365f2d197fe5de8c335480e1b32e466c8039
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-16 07:17:57 -0400

    Time: 89 ms (60.86%), Space: 17.4 MB (46.10%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -2,21 +2,18 @@ import heapq
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         heap = [(grid[0][0], 0, 0)]
+        seen = {(0,0)}
         n = len(grid)
         directions = [(0,1), (1,0), (-1,0), (0,-1)]
-        seen = {(0,0)}
         
         while heap:
             time, x, y = heappop(heap)
             
-            if x == n - 1 and y == n - 1:
+            if (x, y) == (n-1, n-1):
                 return time
             
             for dx, dy in directions:
                 new_dx, new_dy = dx + x, dy + y
                 if 0 <= new_dx < n and 0 <= new_dy < n and (new_dx, new_dy) not in seen:
                     seen.add((new_dx, new_dy))
-                    heappush(heap, (max(time, grid[new_dx][new_dy]), new_dx, new_dy))
-        
-            
-            
+                    heappush(heap, (max(grid[new_dx][new_dy], time), new_dx, new_dy))
