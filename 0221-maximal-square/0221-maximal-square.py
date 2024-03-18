40f90b7a0d6962d512a1f5f0bcde2adc2f4e9751
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-18 10:21:07 -0400

    Time: 665 ms (21.37%), Space: 63.7 MB (5.96%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,31 +1,22 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        max_len = 0
+        self.ans = 0
         
-        
-        for row in range(m -1, -1, -1):
-            for col in range(n -1, -1, -1):
-                if matrix[row][col] == "1":
-                    dp[row][col] = min(dp[row+1][col], dp[row][col+1], dp[row+1][col+1]) + 1
-                    max_len = max(max_len, dp[row][col])
-        return max_len ** 2
-#         self.ans = 0
-#         @cache
-#         def dp(row, col):
-#             if row >= m or col >= n:
-#                 return 0
+        @cache
+        def dp(row, col):
+            if row >= m or col >= n:
+                return 0
             
-#             length = 0
-#             if matrix[row][col] == "1":
-#                 length = min(dp(row+1, col), dp(row, col+1), dp(row+1, col+1)) + 1
-#                 self.ans = max(self.ans, length)
-#             else:
-#                 dp(row+1, col)
-#                 dp(row, col+1)
-#                 dp(row+1, col+1)
-#             return length
-        
-#         dp(0,0)
-#         return self.ans ** 2
+            length = 0
+            if matrix[row][col] == "1":
+                length = min(dp(row+1, col), dp(row, col+1), dp(row+1, col+1)) + 1
+                self.ans = max(self.ans, length)
+            else:
+                dp(row+1, col)
+                dp(row, col+1)
+                dp(row+1, col+1)
+            return length
+        dp(0,0)
+        return self.ans ** 2
+            
