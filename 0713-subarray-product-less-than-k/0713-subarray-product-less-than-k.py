af568a79f739392b25be5996adbedde1c5997c88
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-27 10:55:58 -0400

    Time: 475 ms (98.30%), Space: 19.3 MB (16.13%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,19 +1,17 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # We have k <= 1 and not k <= 1 because we start our curr at 1
-        # Any answer in nums will exceed k if k == 1. Thus we have to have a condition to return 0
         if k <= 1:
             return 0
-
-        left = ans = 0
-        curr = 1
-
+        ans = left = 0
+        total = 1
+        
         for right, num in enumerate(nums):
-            curr *= num
-
-            while curr >= k:
-                curr //= nums[left]
+            total *= num
+            
+            while total >= k:
+                total //= nums[left]
                 left += 1
             
             ans += right - left + 1
         return ans
+        
