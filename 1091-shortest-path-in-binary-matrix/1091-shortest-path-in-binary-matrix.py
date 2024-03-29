18158ead0c0b0041ca5ae54250e29e8b66616b88
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-29 12:52:47 -0400

    Time: 539 ms (21.22%), Space: 18.1 MB (55.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -3,31 +3,24 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1:
             return -1
-        
         queue = deque([(0, 0, 1)])
-        directions = ((0,1), (1,0), (-1,0), (0,-1), (1,1), (-1,-1), (-1,1), (1,-1))
-        row = len(grid)
-        col = len(grid[0])
-        seen = {(0,0)}
+        directions = [(0, 1), (1,0), (1,1), (0,-1), (-1,0), (-1,-1), (-1,1), (1,-1)]
+        row, col = len(grid), len(grid[0])
+        seen = {(0, 0)}
         
-        def valid(r, c):
-            return 0 <= r < row and 0 <= c < col and grid[r][c] == 0
-        
+        def valid(x, y):
+            return 0 <= x < row and 0 <= y < col and grid[x][y] == 0
         
         while queue:
-            queue_len = len(queue)
+            x, y, steps = queue.popleft()
             
-            for _ in range(queue_len):
-                x, y, steps = queue.popleft()
-                
-                if (x,y) == (row-1, col-1):
-                    return steps
-                
-                for dx, dy in directions:
-                    new_x, new_y = dx + x, dy + y
-                    if valid(new_x, new_y) and (new_x, new_y) not in seen:
-                        seen.add((new_x, new_y))
-                        queue.append((new_x, new_y, steps + 1))
-                        
+            if (x, y) == (row-1, col - 1) and grid[x][y] == 0:
+                return steps
+            
+            for dx, dy in directions:
+                new_dx, new_dy = dx + x, dy + y
+                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                    seen.add((new_dx, new_dy))
+                    queue.append((new_dx, new_dy, steps + 1))
         return -1
-                
+            
