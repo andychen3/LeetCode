763a093c18291591438155b10161b8b4b3cee202
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-29 11:57:44 -0400

    Time: 1326 ms (19.91%), Space: 86.1 MB (13.82%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.py
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 class Solution:
     def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
         graph = defaultdict(list)
+        restrict = set(restricted)
         
         for x, y in edges:
             graph[x].append(y)
@@ -9,12 +10,9 @@ class Solution:
             
         def dfs(node):
             for neighbors in graph[node]:
-                if neighbors not in seen and neighbors not in res:
+                if neighbors not in seen and neighbors not in restrict:
                     seen.add(neighbors)
                     dfs(neighbors)
-        
         seen = {0}
-        res = set(restricted)
         dfs(0)
-        
         return len(seen)
