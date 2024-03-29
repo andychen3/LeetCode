93aa6a6c0f893571097f5a3394c2b263bc4a47d9
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-29 08:55:46 -0400

    Time: 186 ms (53.84%), Space: 17.6 MB (27.48%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,24 +4,24 @@ class Solution:
         graph = defaultdict(list)
         n = len(isConnected)
         
-        
         def dfs(node):
             if node in seen:
                 return
-            
             seen.add(node)
             for neighbors in graph[node]:
                 dfs(neighbors)
-        
+            
         
         for i in range(n):
-            for j in range(i+1, n):
-                if isConnected[i][j] == 1:
+            for j in range(i + 1, n):
+                if isConnected[i][j]:
                     graph[i].append(j)
                     graph[j].append(i)
-                    
+        
+        
         seen = set()
         ans = 0
+        
         for i in range(n):
             if i not in seen:
                 ans += 1
