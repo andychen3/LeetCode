20c32a58788f447b79798b1707f7fd8eb255e6e4
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-29 17:21:05 -0400

    Time: 529 ms (26.30%), Space: 19.8 MB (40.70%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,22 +1,23 @@
 from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        queue = deque()
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
-        seen = set()
         row, col = len(mat), len(mat[0])
+        queue = deque()
+        seen = set()
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
         
         for r in range(row):
             for c in range(col):
                 if mat[r][c] == 0:
-                    seen.add((r,c))
                     queue.append((r, c, 1))
+                    seen.add((r,c))
         
-        def valid(r, c):
-            return 0 <= r < row and 0 <= c < col
-        
+        def valid(x, y):
+            return 0 <= x < row and 0 <= y < col and mat[x][y] == 1
+            
         while queue:
             x, y, steps = queue.popleft()
+            
             for dx, dy in directions:
                 new_dx, new_dy = dx + x, dy + y
                 if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
@@ -24,4 +25,3 @@ class Solution:
                     mat[new_dx][new_dy] = steps
                     queue.append((new_dx, new_dy, steps + 1))
         return mat
-        
