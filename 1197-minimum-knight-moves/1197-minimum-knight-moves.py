8cf3b14360fa62344ee3096fa3045a22c9c15cd5
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-29 20:36:42 -0400

    Time: 3287 ms (49.83%), Space: 78.6 MB (16.25%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,15 +1,14 @@
 from collections import deque
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        queue = deque([(0, 0, 0)])
-        # directions = [(-1, -2), (-2,-1), (1,-2), (2,-1), (-2,1), (-1,2), (1,2), (2,1)]
-        directions = [(-1,-2), (1,-2), (-1,2), (1, 2), (-2,-1), (-2,1), (2,-1), (2,1)]
+        queue = deque([(0,0,0)])
+        directions = [(1,2), (-1,-2),(-1,2), (1,-2), (2,1), (-2,-1), (2,-1), (-2,1)]
         seen = {(0,0)}
         
         while queue:
             r, c, steps = queue.popleft()
             
-            if (r, c) == (x, y):
+            if (r, c) == (x,y):
                 return steps
             
             for dx, dy in directions:
@@ -17,3 +16,6 @@ class Solution:
                 if (new_dx, new_dy) not in seen:
                     seen.add((new_dx, new_dy))
                     queue.append((new_dx, new_dy, steps + 1))
+                
+        
+        
