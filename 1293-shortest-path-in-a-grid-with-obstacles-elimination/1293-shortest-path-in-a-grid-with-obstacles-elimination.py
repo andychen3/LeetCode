26392b46d90655f8fd90dc6e4bdf4cead1958e0e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-03-30 13:39:03 -0400

    Time: 523 ms (17.73%), Space: 24.5 MB (26.29%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,7 +1,7 @@
 from collections import deque
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        queue = deque([(0, 0, 0,k)])
+        queue = deque([(0,0,0,k)])
         seen = {(0,0,k)}
         directions = [(0,1), (1,0), (-1,0), (0,-1)]
         row, col = len(grid), len(grid[0])
@@ -12,16 +12,17 @@ class Solution:
         while queue:
             x, y, steps, remain = queue.popleft()
             
-            if (x, y) == (row-1, col-1):
+            if (x, y) == (row-1,col-1):
                 return steps
             
             for dx, dy in directions:
                 new_dx, new_dy = dx + x, dy + y
                 if valid(new_dx, new_dy):
-                    if grid[new_dx][new_dy] == 1 and remain > 0 and (new_dx, new_dy, remain - 1) not in seen:
-                        seen.add((new_dx, new_dy, remain - 1))
-                        queue.append((new_dx, new_dy, steps + 1, remain - 1))
-                    elif grid[new_dx][new_dy] == 0 and (new_dx, new_dy, remain) not in seen:
+                    if grid[new_dx][new_dy] == 0 and (new_dx, new_dy, remain) not in seen:
                         seen.add((new_dx, new_dy, remain))
                         queue.append((new_dx, new_dy, steps + 1, remain))
-        return -1        
+                    elif grid[new_dx][new_dy] and remain > 0 and (new_dx, new_dy, remain-1) not in seen:
+                        seen.add((new_dx, new_dy, remain - 1))
+                        queue.append((new_dx, new_dy, steps + 1, remain-1))
+        return -1
+                        
