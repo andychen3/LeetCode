81a47be5a38ef75722cba21db3b79b117d45b8fd
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-04-01 14:35:51 -0400

    Time: 34 ms (65.23%), Space: 16.6 MB (52.53%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,18 +1,25 @@
-from collections import deque
+from collections import defaultdict, deque
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        q = deque([(startGene, 0)])
-        seen ={startGene}
+        queue = deque([(startGene, 0)])
+        seen = {(startGene)}
+        moves = set(bank)
         
-        while q:
-            node, steps = q.popleft()
+        def bfs(node):
+            ans = []
+            for i in range(8):
+                for char in "ACGT":
+                    ans.append(node[:i] + char + node[i+1:])
+            return ans
+        
+        while queue:
+            node, steps = queue.popleft()
+            
             if node == endGene:
                 return steps
             
-            for c in "ACGT":
-                for i in range(len(node)):
-                    neighbor = node[:i] + c + node[i+1:]
-                    if neighbor not in seen and neighbor in bank:
-                        seen.add(neighbor)
-                        q.append((neighbor, steps+1))
+            for neighbor in bfs(node):
+                if neighbor in bank and neighbor not in seen:
+                    seen.add(neighbor)
+                    queue.append((neighbor, steps + 1))
         return -1
