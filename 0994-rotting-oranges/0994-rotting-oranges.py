e0014151cce888beba7b212290334b863b0dd80c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-04-02 10:42:50 -0400

    Time: 54 ms (35.85%), Space: 16.4 MB (99.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,35 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue = deque()
         row, col = len(grid), len(grid[0])
-        fresh = 0
-        minutes = 0
+        fresh_oranges = 0
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        queue = deque()
+        seen = set()
         
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 2:
                     queue.append((r, c))
-                elif grid[r][c] == 1:
-                    fresh += 1
+                    seen.add((r,c))
+                if grid[r][c] == 1:
+                    fresh_oranges += 1
 
         def valid(x, y):
             return 0 <= x < row and 0 <= y < col and grid[x][y] == 1
         
-        while queue and fresh > 0:
+        time = 0
+        while queue and fresh_oranges > 0:
             for _ in range(len(queue)):
                 x, y = queue.popleft()
-                for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
-                    new_dx, new_dy = dx + x, dy + y
-                    if valid(new_dx, new_dy):
-                        grid[new_dx][new_dy] = 2
-                        fresh -= 1
-                        queue.append((new_dx, new_dy))
-            minutes += 1
-        return minutes if fresh == 0 else -1
                 
+                for dx, dy in directions:
+                    new_dx, new_dy = dx + x, dy + y
+                    if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                        fresh_oranges -= 1
+                        seen.add((new_dx, new_dy))
+                        queue.append((new_dx, new_dy))
+            
+            time += 1
+        
+        return time if fresh_oranges == 0 else -1
