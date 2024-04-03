05a63fe9b6c9309d862d836a400527ffd5e82d4b
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-04-03 15:01:36 -0400

    Time: 47 ms (67.34%), Space: 16.8 MB (45.24%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,11 +1,12 @@
+from collections import defaultdict
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hash_map = {}
+        hash_map = defaultdict(int)
         stack = []
-
+        
         for num in nums2:
-          while stack and stack[-1] < num:
-            hash_map[stack.pop()] = num
-          stack.append(num)
-        return [hash_map.get(num, -1) for num in nums1]
-          
+            while stack and stack[-1] < num:
+                hash_map[stack.pop()] = num
+            stack.append(num)
+        
+        return [hash_map[num] if hash_map[num] != 0 else -1 for num in nums1]
