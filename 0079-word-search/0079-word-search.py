f80b6cd1f18c3e870f6d090ce3844772e14f2b19
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-04-03 07:28:27 -0400

    Time: 5042 ms (27.83%), Space: 16.6 MB (71.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,27 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row, col = len(board), len(board[0])
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
         
         def valid(x, y):
             return 0 <= x < row and 0 <= y < col
         
-        def backtrack(x, y, index, seen):
-            if index == len(word):
+        def backtrack(x, y, idx, seen):
+            if idx == len(word):
                 return True
             
             for dx, dy in directions:
                 new_dx, new_dy = dx + x, dy + y
                 if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
-                    if board[new_dx][new_dy] == word[index]:
+                    if board[new_dx][new_dy] == word[idx]:
                         seen.add((new_dx, new_dy))
-                        if backtrack(new_dx, new_dy, index + 1, seen):
+                        if backtrack(new_dx, new_dy, idx + 1, seen):
                             return True
+                        
                         seen.remove((new_dx, new_dy))
             return False
         
         for r in range(row):
             for c in range(col):
-                if board[r][c] == word[0] and backtrack(r, c, 1, {(r, c)}):
+                if board[r][c] == word[0] and backtrack(r, c, 1, {(r,c)}):
                     return True
         return False
