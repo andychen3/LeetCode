d5d0210d951bf29909eba73851a3406c5b9a14b3
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-09-12 08:57:51 -0400

    Time: 80 ms (41.27%), Space: 16.7 MB (12.52%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -1,9 +1,14 @@
+from collections import defaultdict
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m, n = len(matrix), len(matrix[0])
-
-        for r in range(1, m):
-            for c in range(1, n):
-                if matrix[r][c] != matrix[r-1][c-1]:
+        row, col = len(matrix), len(matrix[0])
+        diag_dict = defaultdict(int)
+        
+        for r in range(row):
+            for c in range(col):
+                if (r - c) not in diag_dict:
+                    diag_dict[r-c] = matrix[r][c]
+                elif matrix[r][c] != diag_dict[r - c]:
                     return False
+                
         return True
