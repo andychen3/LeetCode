166d93f2116be11396cbad2639b0249a95a8ae41
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-09-18 13:29:16 -0400

    Time: 159 ms (15.43%), Space: 17.3 MB (69.14%) - LeetHub

---
+++ 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def numSplits(self, s: str) -> int:
+        first_split = Counter(s)
+        second_split = Counter()
+        good_splits = 0
+
+        for idx, char in enumerate(s):
+            first_split[char] -= 1
+            if first_split[char] == 0:
+                del first_split[char]
+            second_split[char] += 1
+
+            if len(first_split) == len(second_split):
+                good_splits += 1
+        return good_splits
