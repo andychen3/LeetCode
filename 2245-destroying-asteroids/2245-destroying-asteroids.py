72a656f961c2072530f7737555b924290e8da269
Andy Chen <81591593+andychen3@users.noreply.github.com>
2024-10-19 15:14:29 -0400

    Time: 81 ms (100%), Space: 30.5 MB (21.35%) - LeetHub

--- 2245-destroying-asteroids/2245-destroying-asteroids.py
+++ 2245-destroying-asteroids/2245-destroying-asteroids.py
@@ -1,16 +1,10 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        # How can you tell if this is a greedy problem?
-        # Because you can rearrange the asteroid in any order and you only want to collide
-        # with asteroids that are smaller than the mass so you can gain it to destory the next ones
-        # Reasoning is if you can't destroy an asteroid once its sorted decreasing order than all 
-        # asteroids after will destory your planet
-
         asteroids.sort()
-        for rock in asteroids:
-            if rock <= mass:
-                mass += rock
+
+        for asteroid in asteroids:
+            if mass >= asteroid:
+                mass += asteroid
             else:
                 return False
         return True
-        
