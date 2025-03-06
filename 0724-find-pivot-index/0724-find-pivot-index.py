71bdd7615ed30e8b37727e566b26cb73752ada7a
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-03-06 09:38:50 -0500

    Time: 3 ms (87.5%), Space: 18.8 MB (39.05%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,11 +4,13 @@ class Solution:
         right_sum = sum(nums)
 
         for i, num in enumerate(nums):
-            left_sum += num
+            right_sum -= num
 
             if left_sum == right_sum:
                 return i
 
-            right_sum -= num
+            left_sum += num
+
+            
         
         return -1
