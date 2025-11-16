df0b24beb188fee92204de2929b4c671169dbb8e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-16 09:52:25 -0500

    Time: 0 ms (100%), Space: 22.9 MB (82.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,14 +3,4 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        # what are the base cases?
-        #when the string is empty what do you do?
-        # What is the recurrence relationship?
-        def reverse(word, left, right):
-            if left >= right:
-                return word
-            word[left], word[right] = word[right], word[left]
-            return reverse(word, left + 1, right - 1)
-
-        return reverse(s, 0, len(s)-1)
-        
+        return s.reverse()
