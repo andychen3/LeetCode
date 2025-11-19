98ddf6d53254b554869153877b1fad9718f61d2e
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-19 13:54:20 -0500

    Time: 109 ms (51.45%), Space: 26.8 MB (9.56%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,12 +1,14 @@
 from collections import defaultdict
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        ans = odds = 0
         counts = defaultdict(int)
         counts[0] = 1
-        total = ans = 0
 
         for num in nums:
-            total += num % 2
-            ans += counts[total - k]
-            counts[total] += 1
+            if num % 2 == 1:
+                odds += 1
+            ans += counts[odds - k]
+            counts[odds] += 1
+        
         return ans
