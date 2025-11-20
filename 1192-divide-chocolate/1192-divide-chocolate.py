412dbd8c0a482ecb6cd6f609939677082eb643cf
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-20 15:29:46 -0500

    Time: 71 ms (27.38%), Space: 18.8 MB (54.16%) - LeetHub

--- 1192-divide-chocolate/1192-divide-chocolate.py
+++ 1192-divide-chocolate/1192-divide-chocolate.py
@@ -1,31 +1,28 @@
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
-        # The difference between a heap problem and a binary search problem
-        # If there is k is what is the k asking?
-        # This k is asking for if divided among the k people what is the min total sweetness you get
-        # So there is a threshold. While the other problems are asking kth element or kth operations.
-        # We also know this is a greedy problem because of min and max.
-        def check(sweetness_level):
-            ans = 0
+
+        def check(max_sweetness):
             groups = 0
+            chunks = 0
 
-            for sweet in sweetness:
-                groups += sweet
-                if groups >= sweetness_level:
-                    ans += 1
-                    groups = 0
-            # When do you reduce the sweetness level?
-            # When ans is greater than the amount of groups. Which tells us that
-            # The sweetness level is not big enough so we have to increase our left range
-            return ans >= k + 1 
+            for s in sweetness:
+                chunks += s
+                if chunks >= mid:
+                    groups += 1
+                    chunks = 0
+            
+            return groups >= k + 1
 
-        # This is the binary search template for finding a maximum
-        left = 1
-        right = sum(sweetness)
+        left, right = 1, sum(sweetness)
+        res = 0
+
         while left <= right:
             mid = (left + right) // 2
+
             if check(mid):
+                res = mid
                 left = mid + 1
             else:
                 right = mid - 1
-        return right
+
+        return res
