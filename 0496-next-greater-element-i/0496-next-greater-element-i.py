3e8ee29af00b9da9f9533ab15d8fdb752341f785
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-20 14:29:46 -0500

    Time: 0 ms (100%), Space: 18.1 MB (39.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,9 +2,9 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hash_map = {}
         stack = []
-        
+
         for num in nums2:
-            while stack and stack[-1] < num:
+            while stack and num > stack[-1]:
                 hash_map[stack.pop()] = num
             stack.append(num)
         
