0c690c4bef512e26bba7f6b483087c395077aab8
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-21 09:30:19 -0500

    Time: 687 ms (21.31%), Space: 41.9 MB (84.78%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -1,27 +1,26 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        # How do i know to use binary search? Maybe its the at least key word?
-        # Saying that if i was to sort input then all the ones after will be greater than successful
-        potions.sort()
-        ans = []
-        n = len(potions)
+        def binary_search(spell):
+            left = 0
+            right = len(sorted_potions) - 1
+            idx = len(sorted_potions)
 
-        # This keeps the basic BS format
-        def binary_search(arr, target):
-            left, right = 0, len(arr) - 1
             while left <= right:
                 mid = (left + right) // 2
-                # We combine the equal and don't return early if we found mid equal target
-                # because if we have duplicates in the array we want to find the index where 
-                # the element just became successful
-                # This will be in the left most index
-                if arr[mid] >= target:
+
+                if sorted_potions[mid] * spell >= success:
+                    idx = mid
                     right = mid - 1
                 else:
                     left = mid + 1
-            return n - left
+            return idx
+            
+        sorted_potions = sorted(potions)
+        res = []
+        m = len(sorted_potions)
 
-        for spell in spells:
-            ans.append(binary_search(potions, success / spell))
-
-        return ans
+        for s in spells:
+            length = binary_search(s)
+            res.append(m - length)
+        
+        return res
