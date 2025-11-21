972d7ca9e316f1be88549c395a95cf2cb89f392c
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-21 09:41:14 -0500

    Time: 11 ms (71.62%), Space: 18.1 MB (68.74%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -1,34 +1,30 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        # Many different ways to solve. But we will consider binary search because
-        # Subsequence does not matter. We are also given a target from queries
-        # We can find the solution space
-        nums.sort()
+        sorted_nums = sorted(nums)
+        prefix = [sorted_nums[0]]
 
-        # build prefix sum since we are trying to find the sum less than queries
-        # to make our life easier we can create a prefix
-        prefix = [nums[0]]
+        for i in range(1, len(sorted_nums)):
+            prefix.append(sorted_nums[i] + prefix[-1])
 
-        for i in range(1, len(nums)):
-            prefix.append(prefix[-1] + nums[i])
+        def binary_search(query):
+            left = 0
+            right = len(sorted_nums) - 1
+            res = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if prefix[mid] <= query:
+                    res = mid
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return res
         
         ans = []
 
-        def binary_search(arr, target):
-            left, right = 0, len(arr) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                # So the reason why we don't want to return if we find target is because
-                # it is asking us for a sum that is less than or equal. So if we returned when we found
-                # target we could be missing a duplicate.
-                if arr[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            # We consider left because left will have the leftmost index to insert the query
-            return left
-
-        for query in queries:
-            ans.append(binary_search(prefix, query))
+        for q in queries:
+            idx = binary_search(q)
+            ans.append(idx + 1)
         
         return ans
