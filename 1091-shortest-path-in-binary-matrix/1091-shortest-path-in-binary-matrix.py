5c5dd25ba98fbf4dc7d484bbc2e63537f79fc334
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-26 15:35:01 -0500

    Time: 207 ms (47.1%), Space: 19.4 MB (41.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,26 +1,29 @@
 from collections import deque
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1:
             return -1
-        queue = deque([(0, 0, 1)])
-        directions = [(0, 1), (1,0), (1,1), (0,-1), (-1,0), (-1,-1), (-1,1), (1,-1)]
-        row, col = len(grid), len(grid[0])
+
+        def valid(r, c):
+            return 0 <= r < n and 0 <= c < n and grid[r][c] == 0
+        
+        queue = deque([[0, 0, 1]])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
         seen = {(0, 0)}
-        
-        def valid(x, y):
-            return 0 <= x < row and 0 <= y < col and grid[x][y] == 0
-        
+        n = len(grid)
+
         while queue:
-            x, y, steps = queue.popleft()
-            
-            if (x, y) == (row-1, col - 1) and grid[x][y] == 0:
+            row, col, steps = queue.popleft()
+
+            if row == n - 1 and col == n - 1:
                 return steps
-            
+
             for dx, dy in directions:
-                new_dx, new_dy = dx + x, dy + y
-                if valid(new_dx, new_dy) and (new_dx, new_dy) not in seen:
+                new_dx, new_dy = row + dx, col + dy
+
+                if (new_dx, new_dy) not in seen and valid(new_dx, new_dy):
                     seen.add((new_dx, new_dy))
                     queue.append((new_dx, new_dy, steps + 1))
+        
         return -1
-            
