6e56a7593253de5acdca90b30ca68ef28d528ee8
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-26 11:53:07 -0500

    Time: 185 ms (65.09%), Space: 52 MB (33.02%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,24 +1,26 @@
 from collections import defaultdict
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = defaultdict(list)
         roads = set()
-        
+        graph = defaultdict(list)
+
         for x, y in connections:
-            roads.add((x, y))
             graph[x].append(y)
             graph[y].append(x)
-            
+            roads.add((x, y))
+
         def dfs(node):
-            ans = 0
-            for neighbors in graph[node]:
-                if neighbors not in seen:
-                    if (node, neighbors) in roads:
+            nonlocal ans
+            
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    if (node, neighbor) in roads:
                         ans += 1
                     
-                    seen.add(neighbors)
-                    ans += dfs(neighbors)
-            return ans
-        
+                    seen.add(neighbor)
+                    dfs(neighbor)
+
         seen = {0}
-        return dfs(0)
+        ans = 0
+        dfs(0)
+        return ans
