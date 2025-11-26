3a208c86f2da2a027b5ae7a78c9b8f6e36a31b25
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-11-26 10:48:01 -0500

    Time: 258 ms (41.28%), Space: 28.2 MB (13.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row, col = len(grid), len(grid[0])
-        
-        def dfs(x, y):
-            if x < 0 or x >= row or y < 0 or y >= col or grid[x][y] != "1" or (x,y) in seen:
-                return
-            seen.add((x, y))
-            dfs(x+1, y)
-            dfs(x-1, y)
-            dfs(x, y+1)
-            dfs(x, y-1)
-        
-        
-        ans = 0
         seen = set()
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == "1" and (r, c) not in seen:
-                    ans += 1
-                    dfs(r, c)
-        return ans
-                    
-                
+        res = 0
+
+        def valid(dx, dy):
+            return dx < row and dy < col and dx >= 0 and dy >= 0 and grid[dx][dy] == '1' 
+
+        def dfs(r, c):
+            seen.add((r, c))
+
+            for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                new_dx, new_dy = dx + r, dy + c
+                if (new_dx, new_dy) not in seen and valid(new_dx, new_dy):
+                    dfs(new_dx, new_dy)
+
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == '1' and (i, j) not in seen:
+                    res += 1
+                    dfs(i, j)
+
+        return res
