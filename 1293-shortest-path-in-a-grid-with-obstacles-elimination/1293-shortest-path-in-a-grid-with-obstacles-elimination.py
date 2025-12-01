852c9b33dc5ffb08064bf7df7272a07b08a90eec
Andy Chen <81591593+andychen3@users.noreply.github.com>
2025-12-01 15:14:20 -0500

    Time: 518 ms (16.11%), Space: 25.5 MB (33.57%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,28 +1,29 @@
 from collections import deque
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        queue = deque([(0,0,0,k)])
-        seen = {(0,0,k)}
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
-        row, col = len(grid), len(grid[0])
-        
-        def valid(x, y):
-            return 0 <= x < row and 0 <= y < col
-        
+        m, n = len(grid), len(grid[0])
+        queue = deque([(0, 0, 0, k)])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        seen = {(0, 0, k)}
+
+        def valid(r, c):
+            return 0 <= r < m and 0 <= c < n
+
         while queue:
-            x, y, steps, remain = queue.popleft()
-            
-            if (x, y) == (row-1,col-1):
+            row, col, steps, removals = queue.popleft()
+
+            if (row, col) == (m - 1, n - 1) and removals >= 0:
                 return steps
-            
+
             for dx, dy in directions:
-                new_dx, new_dy = dx + x, dy + y
-                if valid(new_dx, new_dy):
-                    if grid[new_dx][new_dy] == 0 and (new_dx, new_dy, remain) not in seen:
-                        seen.add((new_dx, new_dy, remain))
-                        queue.append((new_dx, new_dy, steps + 1, remain))
-                    elif grid[new_dx][new_dy] and remain > 0 and (new_dx, new_dy, remain-1) not in seen:
-                        seen.add((new_dx, new_dy, remain - 1))
-                        queue.append((new_dx, new_dy, steps + 1, remain-1))
+                new_dx, new_dy = row + dx, col + dy
+
+                if valid(new_dx, new_dy): 
+                    if grid[new_dx][new_dy] == 1 and removals - 1 >= 0 and (new_dx, new_dy, removals - 1) not in seen:
+                        seen.add((new_dx, new_dy, removals - 1))
+                        queue.append((new_dx, new_dy, steps + 1, removals - 1))
+                    elif grid[new_dx][new_dy] == 0 and (new_dx, new_dy, removals) not in seen:
+                        seen.add((new_dx, new_dy, removals))
+                        queue.append((new_dx, new_dy, steps + 1, removals))
+                    
         return -1
-                        
